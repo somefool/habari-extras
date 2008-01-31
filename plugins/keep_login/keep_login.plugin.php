dfa8ee05ace97a5acc252dcb00ada4fea6db0d8f
ringmaster <ringmaster@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-01-31 01:22:31 +0000

    Added keep_login plugin to plugins.
    git-svn-id: http://svn.habariproject.org/habari-extras@4 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/keep_login/keep_login.plugin.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Keep Login plugin
+ *
+ **/
+
+class KeepLogin extends Plugin
+{
+	/**
+	 * function info
+	 * Returns information about this plugin
+	 * @return array Plugin info array
+	 **/
+	function info()
+	{
+		return array (
+			'name' => 'Keep Login',
+			'url' => 'http://habariproject.org/',
+			'author' => 'Habari Community',
+			'authorurl' => 'http://habariproject.org/',
+			'version' => '1.0',
+			'description' => 'Uses ajax to keep a session logged in in the admin.',
+			'license' => 'Apache License 2.0',
+		);
+	}
+
+	public function action_admin_header( $admintheme )
+	{
+		$ajaxurl = URL::get('auth_ajax', array('context'=>'keep_session'));
+		echo <<< HEADER_JS
+<script type="text/javascript">
+$(document).ready(function(){
+	window.setInterval(
+		function(){
+			$.post('$ajaxurl');
+		},
+		1000 * 60 * 5 // 5 minutes
+	);
+});
+</script>
+HEADER_JS;
+	}
+
+	public function action_auth_ajax_keep_session()
+	{
+		echo time();
+	}
+
+}
+
+?>
