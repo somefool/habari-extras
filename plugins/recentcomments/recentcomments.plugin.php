41293c77cb0fa6eabc564fff1d40f773bf8c9c09
dmondark <dmondark@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-02-16 00:55:10 +0000

    Added update notification support, method comments and also minor code cleanup
    git-svn-id: http://svn.habariproject.org/habari-extras@12 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/recentcomments/recentcomments.plugin.php
+++ plugins/recentcomments/recentcomments.plugin.php
@@ -1,74 +1,122 @@
 <?php
+
 Class RecentComments extends Plugin
 {
 	private $recent_comments;
-		
+	
+	/**
+	 * Provides Plugin information.
+	 **/	
 	public function info()
 	{
 		return array(
 		'name'=>'Recent Comments',
-		'version'=>'1.0',
+		'version'=>'1.1',
 		'url'=>'http://habariproject.org/',
-		'author'=>'Ali B.',
-		'authorurl'=>'http://awhitebox.com',
+		'author'=>'Habari Community',
+		'authorurl'=>'http://habariproject.org/',
 		'license'=>'Apache License 2.0',
-		'description'=>'Displays the most recent comments in your Habari blog'	
+		'description'=>'Displays the most recent comments in your Habari blog'
 		);
 	}
-
-public function action_init()
-	{
-		//initalize: nothing needed here, so far
+	
+	/**
+	 * Implement the update notification feature
+	 */
+  	public function action_update_check()
+  	{
+    	Update::add( 'RecentComments', '6d49a362-db63-11dc-95ff-0800200c9a66',  $this->info->version );
+  	}
+	
+  	/**
+  	 * Initializes the plugin's output variable when this plugin initializes 
+  	 */
+	public function action_init()
+	{	
+		$recent_comments='';
 	}
 	
-	public function filter_plugin_config( $actions, $plugin_id)
+	/**
+	 * Adds a Configure action to the plugin
+	 * 
+	 * @param array $actions An array of actions that apply to this plugin
+	 * @param string $plugin_id The id of a plugin
+	 * @return array The array of actions
+	 */
+	public function filter_plugin_config( $actions, $plugin_id )
 	{
 		
-		if ( $this->plugin_id()==$plugin_id ){
-			$actions[]='Configure';		
+		if ( $this->plugin_id() == $plugin_id ){
+			$actions[]= 'Configure';		
 		}
 		return $actions;
 	}
 	
+	/**
+	 * Creates a UI form to handle the plguin configurations
+	 *
+	 * @param string $plugin_id The id of a plugin
+	 * @param array $actions An array of actions that apply to this plugin
+	 */
 	public function action_plugin_ui( $plugin_id, $action )
 	{
-		if ( $this->plugin_id()==$plugin_id && $action=='Configure'){
-			$form=new FormUI(strtolower(get_class($this)));
-			$format=$form->add('text','format','List item format (use [[user]], [[post]] and/or [[date]]):','[[user]] on [[post]]');
-			$format->add_validator('validate_required');
-			$dateformat=$form->add('text','dateformat','Date fomrat <i>(if [[date]] is used)</i>:','Mj n:ia');
-			$count=$form->add('text','count','Number of comments to display:','5');
-			$count->add_validator('validate_required');
-			$withul=$form->add('checkbox','withul','Wrap the list with 	&lt;ul&gt;&lt;/ul&gt;',true);
+		if ( $this->plugin_id()==$plugin_id && $action=='Configure' ){
+			$form= new FormUI( strtolower(get_class( $this ) ) );
+			$format= $form->add( 'text','format','List item format (use [[user]], [[post]] and/or [[date]]):','[[user]] on [[post]]' );
+			$format->add_validator( 'validate_required' );
+			$dateformat= $form->add( 'text','dateformat','Date fomrat <i>(if [[date]] is used)</i>:','Mj n:ia' );
+			$count= $form->add( 'text','count','Number of comments to display:','5' );
+			$count->add_validator( 'validate_required' );
+			$withul= $form->add('checkbox','withul','Wrap the list with &lt;ul&gt;&lt;/ul&gt;',true );
 			$form->on_success( array( $this, 'saved_config' ) );
 			$form->out();
 			}
 	}
 	
-	public function saved_config($form)
+	/**
+	 * Invoked when the before the plugin configurations are saved
+	 *
+	 * @param FormUI $form The configuration form being saved
+	 * @return true
+	 */
+	public function saved_config( $form )
 	{   
 		return true;
 	}
 	
+	/**
+	 * Makes the variable holding the recent comments list available to the theme
+	 *
+	 * @param Theme $theme The theme to add the variable to
+	 */
 	public function action_add_template_vars( $theme ) {
 		$theme->assign ('recent_comments',$this->get_recent_comments());
 	}
 	
+	/**
+	 * Compiles and formats the recent comments list
+	 *
+	 * @return string An HTML unorderd list of the recent comments
+	 */
 	public function get_recent_comments(){
-
-		$limit=Options::get(strtolower(get_class($this)) . ':count');
-		$format=Options::get(strtolower(get_class($this)) . ':format');
-		$dateformat=Options::get(strtolower(get_class($this)) . ':dateformat');
-		$withul=Options::get(strtolower(get_class($this)) . ':withul');
-		if ($withul==false) $ul=array('','');
+		//Get the plugin options
+		$limit= Options::get(strtolower(get_class($this)) . ':count' );
+		$format= Options::get( strtolower(get_class( $this ) ) . ':format' );
+		$dateformat=Options::get(strtolower(get_class($this)) . ':dateformat' );
+		$withul=Options::get(strtolower(get_class($this)) . ':withul' );
+		
+		//Assign default values if options not set
+		if (!$withul) $ul=array('','');
 		else $ul=array("<ul>\n","</ul>\n");
 		if (empty($limit)) $limit='5';
 		if (empty($format)) $format='[[user]] on [[post]]';
 		if (empty($dateformat)) $dateformat='Mj n:ia';
+		
 		$status=Comment::STATUS_APPROVED;
 		$commentarray=array('limit'=>$limit, 'status'=>$status, 'type'=>Comment::COMMENT, 'orderby'=>'date DESC');
 		$comments=Comments::get($commentarray);
 		$list=$ul[0];
+		
 		foreach ($comments as $comment){
 			$name='<a href="'.$comment->url.'">'.$comment->name.'</a>';
 			$post='<a href="'.$comment->post->permalink.'" rel="external">'.$comment->post->title.'</a>';
@@ -76,8 +124,11 @@ public function action_init()
 			$date=date($dateformat,mktime($datearray['hour'],$datearray['minute'],0,$datearray['month'],$datearray['day'],$datearray['year']));
 			$list.="<li>".str_replace('[[user]]',$name, str_replace('[[post]]',$post,str_replace('[[date]]',$date,$format)))."</li>\n";
 		}
+		
 		$list.=$ul[1];
+		
 		return $list;
 	}
+	
 }
 ?>
