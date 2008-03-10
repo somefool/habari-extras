f3dcf7ebf96ac48b4ef2cdf0ea001b86693b3df2
freakerz <freakerz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-03-10 23:04:26 +0000

    Committed the wrong version earlier.
    This is the good one.
    
    git-svn-id: http://svn.habariproject.org/habari-extras@65 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/feedburner/feedburner.plugin.php
+++ plugins/feedburner/feedburner.plugin.php
@@ -1,8 +1,9 @@
 <?php
 class FeedBurner extends Plugin {
-	
-	/* FeedBurner URL List
-	 * You MUST change this to your Feed Address.
+
+	/**
+	 * FeedBurner URL List
+	 * You MUST change this to your Feed Address
 	 *
 	 * introspection - Overall Collection Feed
 	 * collection - Overall Collection Feed
@@ -10,7 +11,7 @@ class FeedBurner extends Plugin {
 	 *
 	 * URL Example: http://feeds.feedburner.com/HabariProject
 	 *
-	 * Actions you do not define will not be filtered.
+	 * Actions you do not define will not be filtered
 	 */
 	private static $feedburner_feeds= array(
 		'introspection' => 'http://feeds.feedburner.com/HabariProject',
@@ -18,11 +19,25 @@ class FeedBurner extends Plugin {
 		'comments' => 'http://feeds.feedburner.com/HabariProject/comments',
 	);
 
-	/* Required Plugin Informations */
+	/**
+	 * Feed groups used in the dashboard statistics module
+	 * The key is the title of the statistic,
+	 * the value is an array of Feedburner feeds based on the array above ($feedburner_feeds)
+	 *
+	 * You shouldn't have to edit this
+	 */
+	private static $feed_groups= array(
+		'entries' => array( 'introspection', 'collection' ),
+		'comments' => array( 'comments' ),
+	);
+
+	/**
+	 * Required Plugin Informations
+	 */
 	public function info() {
 		return array(
 			'name' => 'FeedBurner',
-			'version' => '1.2',
+			'version' => '1.3',
 			'url' => 'http://habariproject.org/',
 			'author' =>	'Habari Community',
 			'authorurl' => 'http://habariproject.org/',
@@ -31,7 +46,11 @@ class FeedBurner extends Plugin {
 			'copyright' => '2007'
 		);
 	}
-	
+
+	/**
+	 * When the AtomHandler is created, check what action called it
+	 * If the action is set in our URL list, intercept and redirect to Feedburner
+	 */
 	public function action_init_atom()
 	{
 		/* List of exclusions - You SHOULD NOT edit this! */
@@ -55,28 +74,37 @@ class FeedBurner extends Plugin {
 			}
 		}
 	}
-	
+
+	/**
+	 * Add the FeedBurner statistics to the admin dashboard
+	 *
+	 * @param array $stats Statistic summary array used in the dashboard
+	 * @return array Statistic summary array to which we appended Feedburner statistics
+	 */
 	public function filter_statistics_summary( $stats ) {
-		foreach ( self::$feed_url as $type => $url ) {
-			if ( $url != '' ) {
-				$awareness_api= 'http://api.feedburner.com/awareness/1.0/GetFeedData?uri=' . $url;
-				$request= new RemoteRequest( $awareness_api );
-				if ( !$request->execute() ) {
-					return;
-				}
-				$xml= simplexml_load_string( $request->get_response_body() );
-				if ( $xml['stat'] == 'fail' ) {
-					$stat_str= "{$xml->err['msg']} ({$type})";
-					$stats[$stat_str]= '';
-				}
-				else {
-					$readers= ( string ) $xml->feed->entry['circulation'];
-					$reach= ( string ) $xml->feed->entry['reach'];
-					
-					$reader_str= "FeedBurner Readers ({$type})";
-					$reach_str= "FeedBurner Reach ({$type})";
-					$stats[$reader_str]= $readers;
-					$stats[$reach_str]= $reach;
+		foreach ( self::$feed_groups as $type => $feeds ) {
+			$readers= array();
+			$reach= array();
+			$reader_str= "FeedBurner Readers ({$type})";
+			$reach_str= "FeedBurner Reach ({$type})";
+			foreach ( $feeds as $feed ) {
+				if ( self::$feedburner_feeds[$feed] != '' ) {
+					$awareness_api= 'http://api.feedburner.com/awareness/1.0/GetFeedData?uri=' . self::$feedburner_feeds[$feed];
+					$request= new RemoteRequest( $awareness_api );
+					if ( !$request->execute() ) {
+						return;
+					}
+					$xml= simplexml_load_string( $request->get_response_body() );
+					if ( $xml['stat'] == 'fail' ) {
+						$stat_str= "{$xml->err['msg']} ({$type})";
+						$stats[$stat_str]= '';
+					}
+					else {
+						$readers[]= (string) $xml->feed->entry['circulation'];
+						$reach[]= (string) $xml->feed->entry['reach'];
+						$stats[$reader_str]= array_sum($readers);
+						$stats[$reach_str]= array_sum($reach);
+					}
 				}
 			}
 		}
