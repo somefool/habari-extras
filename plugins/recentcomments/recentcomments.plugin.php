bdebcc02031efe7eb59fe576f2430b968ad78438
dmondark <dmondark@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-03-14 01:27:06 +0000

    plugin:recentcomments Moved the plugin output to a template (recentcomments.php). The plugin can be called now using $theme->show_recentcommentsl()
    git-svn-id: http://svn.habariproject.org/habari-extras@77 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/recentcomments/recentcomments.plugin.php
+++ plugins/recentcomments/recentcomments.plugin.php
@@ -1,9 +1,13 @@
 <?php
+/*
+ * Recent Comments Plugin
+ * Usage: <?php $theme->show_recentcomments(); ?> 
+ * A sample recentcomments.php template is included with the plugin.  This can be copied to your 
+ * active theme and modified to fit your preference.
+ */
 
 Class RecentComments extends Plugin
-{
-	private $recent_comments;
-	
+{	
 	/**
 	 * Provides Plugin information.
 	 **/	
@@ -11,12 +15,12 @@ Class RecentComments extends Plugin
 	{
 		return array(
 		'name'=>'Recent Comments',
-		'version'=>'1.1',
+		'version'=>'1.2',
 		'url'=>'http://habariproject.org/',
 		'author'=>'Habari Community',
 		'authorurl'=>'http://habariproject.org/',
 		'license'=>'Apache License 2.0',
-		'description'=>'Displays the most recent comments in your Habari blog'
+		'description'=>'Displays the most recent comments in your blog sidebar'
 		);
 	}
 	
@@ -27,15 +31,7 @@ Class RecentComments extends Plugin
   	{
     	Update::add( 'RecentComments', '6d49a362-db63-11dc-95ff-0800200c9a66',  $this->info->version );
   	}
-	
-  	/**
-  	 * Initializes the plugin's output variable when this plugin initializes 
-  	 */
-	public function action_init()
-	{	
-		$recent_comments='';
-	}
-	
+		
 	/**
 	 * Adds a Configure action to the plugin
 	 * 
@@ -45,7 +41,6 @@ Class RecentComments extends Plugin
 	 */
 	public function filter_plugin_config( $actions, $plugin_id )
 	{
-		
 		if ( $this->plugin_id() == $plugin_id ){
 			$actions[]= 'Configure';		
 		}
@@ -60,14 +55,15 @@ Class RecentComments extends Plugin
 	 */
 	public function action_plugin_ui( $plugin_id, $action )
 	{
-		if ( $this->plugin_id()==$plugin_id && $action=='Configure' ){
+		if ( $this->plugin_id()==$plugin_id && $action=='Configure' )
+			{
 			$form= new FormUI( strtolower(get_class( $this ) ) );
+			$title= $form->add( 'text', 'title', 'Title:', 'Recent Comments');
 			$format= $form->add( 'text','format','List item format (use [[user]], [[post]] and/or [[date]]):','[[user]] on [[post]]' );
 			$format->add_validator( 'validate_required' );
 			$dateformat= $form->add( 'text','dateformat','Date fomrat <i>(if [[date]] is used)</i>:','Mj n:ia' );
 			$count= $form->add( 'text','count','Number of comments to display:','5' );
 			$count->add_validator( 'validate_required' );
-			$withul= $form->add('checkbox','withul','Wrap the list with &lt;ul&gt;&lt;/ul&gt;',true );
 			$form->on_success( array( $this, 'saved_config' ) );
 			$form->out();
 			}
@@ -85,29 +81,17 @@ Class RecentComments extends Plugin
 	}
 	
 	/**
-	 * Makes the variable holding the recent comments list available to the theme
-	 *
-	 * @param Theme $theme The theme to add the variable to
-	 */
-	public function action_add_template_vars( $theme ) {
-		$theme->assign ('recent_comments',$this->get_recent_comments());
-	}
-	
-	/**
 	 * Compiles and formats the recent comments list
 	 *
 	 * @return string An HTML unorderd list of the recent comments
 	 */
-	public function get_recent_comments(){
+	public function theme_show_recentcomments( $theme ){
 		//Get the plugin options
 		$limit= Options::get(strtolower(get_class($this)) . ':count' );
 		$format= Options::get( strtolower(get_class( $this ) ) . ':format' );
 		$dateformat=Options::get(strtolower(get_class($this)) . ':dateformat' );
-		$withul=Options::get(strtolower(get_class($this)) . ':withul' );
-		
+		$theme->recentcomments_title= Options::get(strtolower(get_class($this)) . ':title' );
 		//Assign default values if options not set
-		if (!$withul) $ul=array('','');
-		else $ul=array("<ul>\n","</ul>\n");
 		if (empty($limit)) $limit='5';
 		if (empty($format)) $format='[[user]] on [[post]]';
 		if (empty($dateformat)) $dateformat='Mj n:ia';
@@ -115,20 +99,26 @@ Class RecentComments extends Plugin
 		$status=Comment::STATUS_APPROVED;
 		$commentarray=array('limit'=>$limit, 'status'=>$status, 'type'=>Comment::COMMENT, 'orderby'=>'date DESC');
 		$comments=Comments::get($commentarray);
-		$list=$ul[0];
 		
+		$list= array();
 		foreach ($comments as $comment){
 			$name='<a href="'.$comment->url.'">'.$comment->name.'</a>';
 			$post='<a href="'.$comment->post->permalink.'" rel="external">'.$comment->post->title.'</a>';
 			$datearray=date_parse($comment->date);
 			$date=date($dateformat,mktime($datearray['hour'],$datearray['minute'],0,$datearray['month'],$datearray['day'],$datearray['year']));
-			$list.="<li>".str_replace('[[user]]',$name, str_replace('[[post]]',$post,str_replace('[[date]]',$date,$format)))."</li>\n";
+			$list[]="<li>".str_replace('[[user]]',$name, str_replace('[[post]]',$post,str_replace('[[date]]',$date,$format)))."</li>\n";
 		}
-		
-		$list.=$ul[1];
-		
-		return $list;
+		$theme->recentcomments_links= $list;
+		return $theme->fetch( 'recentcomments' );
+
+	}
+
+	public function action_init()
+	{
+		$this->add_template('recentcomments', dirname(__FILE__) . '/recentcomments.php');
 	}
 	
+
+
 }
 ?>
