1b17b20d65b6703aa8b983b523b05a4ace6032df
dmondark <dmondark@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-03-21 14:47:38 +0000

    plugin:sitemaps Added caching functionaly to cache sitemap.xml
    git-svn-id: http://svn.habariproject.org/habari-extras@84 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/sitemaps/sitemaps.plugin.php
+++ plugins/sitemaps/sitemaps.plugin.php
@@ -41,34 +41,54 @@ class Sitemaps extends Plugin {
 	 */
 	public function Sitemap()
 	{
-	
-		$xml= '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>';
-
-		$xml= new SimpleXMLElement( $xml );
-				
-		/* Retreive all published posts and pages from the database */
-		$content['posts']= Posts::get( array( 'content_type' => 'entry', 'status' => Post::status( 'published' ), 'nolimit' => 1 ) );
-		$content['pages']= Posts::get( array( 'content_type' => 'page', 'status' => Post::status( 'published' ), 'nolimit' => 1 ) );
-		
-		/* Add the index page first */
-		$url= $xml->addChild( 'url' );
-		$url_loc= $url->addChild( 'loc', Site::get_url( 'habari' ) );
-		
-		/* Generate the `<url>`, `<loc>`, `<lastmod>` markup for each post and page. */
-		foreach ( $content as $entries ) {
-			foreach ( $entries as $entry ) {
-				$url= $xml->addChild( 'url' );
-				$url_loc= $url->addChild( 'loc', $entry->permalink );
-				$url_lastmod= $url->addChild( 'lastmod', Utils::atomtime( $entry->updated ) );
-			}
+		//return cached sitemap if exsist
+		if ( Cache::has( 'sitemap' ) ){
+			$xml= Cache::get( 'sitemap' );
 		}
-		
-		$xml= $xml->asXML();
+		else {
+			//..or generate a new one
+			$xml= '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>';
+	
+			$xml= new SimpleXMLElement( $xml );
+					
+			/* Retreive all published posts and pages from the database */
+			$content['posts']= Posts::get( array( 'content_type' => 'entry', 'status' => Post::status( 'published' ), 'nolimit' => 1 ) );
+			$content['pages']= Posts::get( array( 'content_type' => 'page', 'status' => Post::status( 'published' ), 'nolimit' => 1 ) );
+			
+			/* Add the index page first */
+			$url= $xml->addChild( 'url' );
+			$url_loc= $url->addChild( 'loc', Site::get_url( 'habari' ) );
+			
+			/* Generate the `<url>`, `<loc>`, `<lastmod>` markup for each post and page. */
+			foreach ( $content as $entries ) {
+				foreach ( $entries as $entry ) {
+					$url= $xml->addChild( 'url' );
+					$url_loc= $url->addChild( 'loc', $entry->permalink );
+					$url_lastmod= $url->addChild( 'lastmod', Utils::atomtime( $entry->updated ) );
+				}
+			}
+			$xml= $xml->asXML();
+			Cache::set('sitemap', $xml);
+		}
 		
 		/* Clean the output buffer, so we can output from the header/scratch. */
 		ob_clean();
 		header( 'Content-Type: application/xml' );
 		print $xml;
+	}
+	
+	/**
+	 * post_update_status action called when the post status field is updated.
+	 * Expires the cached sitemap if the status has been changed.
+	 * 
+	 * @param Post $post The post being updated
+	 * @param string $new_status the status feild new value
+	 */
+	public function action_post_update_status( $post, $new_status )
+	{	
+		if ($post->status != $new_status){
+			Cache::expire('sitemap');
+		}
 	}
 
 }
