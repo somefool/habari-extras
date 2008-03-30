ebda4671b513031687e6cefd33237e76b24452c9
michaeltwofish <michaeltwofish@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-03-30 08:56:25 +0000

    Plugin:LiveHelp Only add nick to theme if we're on the Live Help page in admin
    git-svn-id: http://svn.habariproject.org/habari-extras@101 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/livehelp/livehelp.plugin.php
+++ plugins/livehelp/livehelp.plugin.php
@@ -47,10 +47,12 @@ class LiveHelp extends Plugin
 
 	public function action_add_template_vars( $theme )
 	{
-		$user= User::identify();
-		$nick= $user->username;
-		$nick= $nick == 'admin' ? substr($user->email, 0, strpos($user->email, '@')) : $nick;
-		$theme->assign('nick', $nick);
+		if ($theme->admin_page == 'livehelp') {
+			$user= User::identify();
+			$nick= $user->username;
+			$nick= $nick == 'admin' ? substr($user->email, 0, strpos($user->email, '@')) : $nick;
+			$theme->assign('nick', $nick);
+		}
 	}
 
 	public function action_admin_header( $theme ) {
