b64b0fae8d9ae685dfd89973c502f15ef9ab59bb
chrismeller <chrismeller@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-06-03 15:46:31 +0000

    Removing charcoal, since it's included in core now.
    git-svn-id: http://svn.habariproject.org/habari-extras@497 f4a90942-1786-4aa1-b092-57c31935ddc3

--- themes/charcoal/trunk/scripts/fixpngs.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-	$(".post, blockquote, .post-tags, #prev-posts, #archives, #tags, .post-date, .post-comments-link a, .nav-prev, .nav-next, #search-btn, .feedlink a, img[@src$=pwrd_habari.png]").pngfix({sizingMethod: "crop"});	
-});
