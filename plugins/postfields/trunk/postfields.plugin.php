51bab9c85324a313de1e04a113bacbb8bb9dc67e
ringmaster <ringmaster@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-06-12 14:00:50 +0000

    Updated for publish page FormUI revisions.
    git-svn-id: http://svn.habariproject.org/habari-extras@571 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/postfields/trunk/postfields.plugin.php
+++ plugins/postfields/trunk/postfields.plugin.php
@@ -61,55 +61,40 @@ class postfields extends Plugin
 	/**
 	* Add additional controls to the publish page tab
 	*
-	* @param array $controls An associative array of HTML that will appear in the tabs on the publish page
+	* @param FormUI $form The form that is used on the publish page
 	* @param Post $post The post being edited
-	* @return array The modified $controls array
 	**/
-	public function filter_publish_controls($controls, $post)
+	public function action_form_publish($form, $post)
 	{
 		$fields = Options::get('postfields__fields');
 		$output = '';
 		$control_id = 0;
+		$postfields = $form->publish_controls->append('fieldset', 'postfields', 'Additional Fields');
 		foreach($fields as $field) {
-			$control_id++;
-			$value = isset($post->info->{$field}) ? $post->info->{$field} : '';
-			$output .= <<< FIELD_OUT
-				<div class="container">
-					<p class="column span-5"><label for="postfield_{$control_id}">{$field}</field></p>
-					<p class="column span-14 last"><input type="text" id="postfield_{$control_id}" name="postfield[{$field}]" value="{$value}"></p>
-				</div>
-FIELD_OUT;
+			$control_id = md5($field);
+			$fieldname = "postfield_{$control_id}";
+			$customfield = $postfields->append('text', $fieldname, 'null:null', $field);
+			$customfield->value = isset($post->info->{$field}) ? $post->info->{$field} : '';
+			$customfield->template = 'tabcontrol_text';
 		}
-
-		$controls['Additional Fields'] = $output;
-		return $controls;
 	}
+	
 
 	/**
 	* Modify a post before it is updated
 	*
 	* @param Post $post The post being saved, by reference
+	* @param FormUI $form The form that was submitted on the publish page
 	*/
-	public function action_post_update_before($post)
+	public function action_publish_post($post, $form)
 	{
-		if(isset($_POST['postfield'])) {
-			$fields = Options::get('postfields__fields');
-			foreach($fields as $field) {
-				$post->info->{$field} = isset($_POST['postfield'][$field]) ? $_POST['postfield'][$field] : '';
-			}
+		$fields = Options::get('postfields__fields');
+		foreach($fields as $field) {
+			$control_id = md5($field);
+			$fieldname = "postfield_{$control_id}";
+			$customfield = $form->$fieldname;
+			$post->info->{$field} = $customfield->value;
 		}
 	}
-
-	/**
-	* Modify a post before it is updated
-	*
-	* @param Post $post The post being saved, by reference
-	*/
-	public function action_post_insert_before($post)
-	{
-		$this->action_post_update_before($post);
-	}
-
 }
-
 ?>
