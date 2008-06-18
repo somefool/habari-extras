c51fdc64b387e32b9fcaa77ee1b5cc881cadcd53
arthus <arthus@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-06-18 06:23:22 +0000

    VERY preliminary work on a GUI for rewrite rules
    git-svn-id: http://svn.habariproject.org/habari-extras@617 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/rewriter/trunk/rewriter.plugin.php
+++ plugins/rewriter/trunk/rewriter.plugin.php
@@ -13,9 +13,9 @@ class Rewriter extends Plugin
 			'url' => 'http://habariproject.org',
 			'version' => '0.1',
 			'license' => 'Apache License 2.0'
-			);
+		);
 	}
-
+	
 	public function action_init()
 	{
 		$this->add_template( 'rules', dirname(__FILE__) . '/rules.php' );
@@ -27,13 +27,52 @@ class Rewriter extends Plugin
 		return $rules;
 	}
 	
-	function action_add_template_vars( $theme ) {
-		$theme->rules= self::get_rules();
+	function add_rule( $name, $params ) {
+		$rule = RewriteRules::by_name( $name );
+		if(count($rule) == 1) {
+			$rule = $rule[0];
+			
+			foreach($params as $key => $param) {
+				$rule->$key = $param;
+			}
+
+			$rule->update();
+		} else {
+			$rule = new RewriteRule($params);
+			$rule->insert();
+		}	
 		
 	}
 	
+	function action_add_template_vars( $theme ) {
+		$theme->simple = Options::get('rewriter:simpleEntry');
+		
+		$theme->rules= self::get_rules();	
+	}
+	
 	function action_admin_theme_get_rules( $handler, $theme )
-	{
+	{	
+		$handler_vars = $handler->handler_vars;
+		
+		if(isset($handler_vars['names'])) {
+			
+			foreach($handler_vars['names'] as $key => $name) {
+				$changes = array(
+					'name' => $name,
+					'parse_regex' => $handler_vars['regexes'][$key],
+					'action' => $handler_vars['actions'][$key],
+					'priority' => $handler_vars['priorities'][$key],
+					'description' => $handler_vars['descriptions'][$key]
+				);
+				
+				self::add_rule( $key, $changes);
+				
+			}
+			
+			Session::notice(_t('Rewrite rules updated.'));
+			
+			Utils::redirect();
+		}
 		
 		$theme->display( 'rules' );
 		exit;
@@ -41,7 +80,7 @@ class Rewriter extends Plugin
 
 	function action_admin_theme_post_rules( $handler, $theme )
 	{
-		$this->action_admin_theme_get_spam( $handler, $theme );
+		$this->action_admin_theme_get_rules( $handler, $theme );
 	}
 
 	public function filter_adminhandler_post_loadplugins_main_menu( $menu )
--- plugins/rewriter/trunk/rules.php
+++ plugins/rewriter/trunk/rules.php
@@ -6,23 +6,42 @@
 		<div class="head clear">
 
 		<span class="name pct20"><?php _e('Name'); ?></span>
-		<span class="regex pct30"><?php _e('Regular Expression'); ?></span>
-		<span class="action pct10"><?php _e('Action'); ?></span>
+		<span class="regex pct25"><?php _e('Regular Expression'); ?></span>
+		<span class="action pct15"><?php _e('Action'); ?></span>
 		<span class="priority pct10"><?php _e('Priority'); ?></span>
 		<span class="description pct20"><?php _e('Description'); ?></span>
 		
 		</div>
 	</div>
 	
+	<form name="rules" id="rules" method="post" action="<?php URL::get( 'admin', 'page=rules' ); ?>">
 	<?php foreach( $rules as $rule) { ?>
-	<div class="item rule clear" style="padding-top:3px; padding-bottom:2px">
+	<div class="item rule view clear" style="padding-top:3px; padding-bottom:2px">
 		<span class="name pct20"><?php echo $rule->name; ?></span>
-		<span class="regex pct30"><?php echo $rule->parse_regex; ?></span>
-		<span class="action pct10"><?php echo $rule->action; ?></span>
+		<span class="regex pct25"><?php echo $rule->parse_regex; ?></span>
+		<span class="action pct15"><?php echo $rule->action; ?></span>
 		<span class="priority pct10"><?php echo $rule->priority; ?></span>
 		<span class="description pct20 minor"><span><?php echo $rule->description; ?></span></span>
 	</div>
-	<?php	} ?>
+	<div class="item rule change clear" style="padding-top:3px; padding-bottom:2px">
+		<span class="name pct20"><input name="names[<?php echo $rule->name; ?>]" type="text" value="<?php echo $rule->name; ?>"></span>
+		<span class="regex pct25"><input name="regexes[<?php echo $rule->name; ?>]" type="text" value="<?php echo $rule->parse_regex; ?>"></span>
+		<span class="action pct15"><input name="actions[<?php echo $rule->name; ?>]" type="text" value="<?php echo $rule->action; ?>"></span>
+		<span class="priority pct10"><input name="priorities[<?php echo $rule->name; ?>]" type="text" value="<?php echo $rule->priority; ?>"></span>
+		<span class="description pct20 minor"><input name="descriptions[<?php echo $rule->name; ?>]" type="text" value="<?php echo $rule->description; ?>"></span></span>
+	</div>
+	<?php } ?>
+	
+	<div class="item">
+		<div class="foot clear">
+		
+		<input type="submit" name="submit" id="submit" class="apply button" value="<?php _e('Apply'); ?>">
+		
+		</div>
+	</div>
+	
+	</form>
+	
 </div>
 
 <?php include( HABARI_PATH . '/system/admin/footer.php'); ?>
