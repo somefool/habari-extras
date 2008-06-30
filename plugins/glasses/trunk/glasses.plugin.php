7c52cf4e67f2951bf94d334560e7beae962e079f
arthus <arthus@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-06-30 16:43:30 +0000

    Initial commit of glasses plugin
    git-svn-id: http://svn.habariproject.org/habari-extras@660 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/glasses/trunk/glasses.plugin.php
@@ -0,0 +1,16 @@
+<?php
+class Glasses extends Plugin
+{
+	public function info()
+	{
+		return array(
+			'name' => 'Glasses',
+			'author' => 'Habari Community',
+			'description' => 'Allows posts to be displayed in a continuous feed instead of being paged.',
+			'url' => 'http://habariproject.org',
+			'version' => '0.5',
+			'license' => 'Apache License 2.0'
+			);
+	}
+}
+?>
