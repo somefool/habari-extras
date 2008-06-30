dad6877802b0f1149607fb0ddf4c17779d53d1fa
arthus <arthus@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-06-30 18:06:22 +0000

    Made speed improvements on client side animation
    git-svn-id: http://svn.habariproject.org/habari-extras@661 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/loupable/trunk/loupable.js
+++ plugins/loupable/trunk/loupable.js
@@ -2,19 +2,13 @@ $(document).ready(function() {
 	var loupe = $('#timeline');
 	loupe.hover(
 		function() {
-			$('#timeline, #timeline .years, #timeline a.item').animate({ 
-				height: "50"
-			}, 500 );
-			$('#timeline .years .months div span').animate({ 
-				top: "33"
+			$('#timeline').animate({ 
+				top: "0"
 			}, 500 );
 		},
 		function() {
-			$('#timeline, #timeline .years, #timeline a.item').animate({ 
-				height: "15"
-			}, 500 );
-			$('#timeline .years .months div span').animate({ 
-				top: "-2"
+			$('#timeline').animate({ 
+				top: "-35"
 			}, 500 );
 		}
 	);
