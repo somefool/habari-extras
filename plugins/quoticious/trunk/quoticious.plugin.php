1cddb81235086fbbd49ef7c74f26900e4bb47f56
arthus <arthus@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-07-03 20:29:22 +0000

    Initial commit of quoticious plugin
    git-svn-id: http://svn.habariproject.org/habari-extras@663 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/quoticious/trunk/quoticious.plugin.php
@@ -0,0 +1,58 @@
+<?php
+class Quoticious extends Plugin {
+	function info() {
+		return array(
+			'name' => 'Quoticious',
+			'version' => '1.0',
+			'url' => 'http://habariproject.org',
+			'author' => 'Habari Community',
+			'authorurl' => 'http://habariproject.org',
+			'license' => 'Apache License 2.0',
+			'description' => 'Allows quotes to be collected'
+		);
+	}
+	
+	/**
+	* Registers this plugin for updates against the beacon
+	*/
+	public function action_update_check()
+	{
+		Update::add('Quoticious', 'cffb07c5-ab53-42e7-b42d-08133097244c', $this->info->version);
+	}
+	
+	public function action_init() {		
+		Post::add_new_type('quote');
+	}
+	
+	public function action_form_publish ($form, $post) {
+		if($post->content_type == Post::type('quote')) {
+			$quote = $form->publish_controls->append('fieldset', 'quote', _t('Quote'));
+		
+			$quote->append('text', 'quote_author', 'null:null', _t('Author'), 'tabcontrol_text');
+			$quote->quote_author->value = $post->info->quote_author;
+
+			$quote->append('text', 'quote_url', 'null:null', _t('URL'), 'tabcontrol_text');
+			$quote->quote_url->value = $post->info->quote_url;
+			
+			return $form;
+		}
+	}
+	
+	public function action_publish_post ( $post, $form ) {
+		
+		if($post->content_type == Post::type('quote')) {
+		
+			if( strlen( $form->quote->quote_author->value ) ) {
+				$post->info->quote_author= $form->quote->quote_author->value;
+			}
+		
+			if( strlen( $form->quote->quote_url->value ) ) {
+				$post->info->quote_url= $form->quote->quote_url->value;
+			}
+		
+		}
+		
+	}
+
+}
+?>
