d730468e39382d753f95fadc332edf5ba12a61e0
michaeltwofish <michaeltwofish@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-07-24 06:56:26 +0000

    Log exceptions. Closes #48. I don't think the OpenID plugin should throw exceptions in so many places. It's entirely ungraceful at the moment.
    git-svn-id: http://svn.habariproject.org/habari-extras@692 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/openid/trunk/openid.plugin.php
+++ plugins/openid/trunk/openid.plugin.php
@@ -1,7 +1,8 @@
 <?php
 class OpenID extends Plugin
 {
-	public function info() {
+	public function info()
+	{
 		return array(
 			'name' => 'OpenID',
 			'version' => '1.1.1',
@@ -13,9 +14,10 @@ class OpenID extends Plugin
 			'copyright' => '2007'
 			);
 	}
-	
-	public function filter_rewrite_rules( $db_rules ) {
-		$db_rules[]= new RewriteRule( array(
+
+	public function filter_rewrite_rules( $db_rules )
+	{
+		$db_rules[] = new RewriteRule( array(
 			'name' => 'openid',
 			'parse_regex' => '%^openid/?(?P<user>[^/]*)/?$%i', // For Server, if previous matched, don't look.
 			'build_str' => 'openid/({$user})',
@@ -26,18 +28,20 @@ class OpenID extends Plugin
 			'rule_class' => RewriteRule::RULE_CUSTOM,
 			'description' => 'OpenID Authentification'
 			) );
-		
+
 		return $db_rules;
 	}
 
-	public function act( $action ) {
+	public function act( $action )
+	{
 		if ( isset( $_GET['openid_mode'] ) ) {
 			switch ( $_GET['openid_mode'] ) {
 				case 'id_res':
 					self::openid_end();
 					break;
 				case 'cancel':
-					throw new Exception( 'Authorization failed: user cancelled the authorization.' );
+					EventLog::log( 'Authorization failed: User cancelled authorization.', 'info', 'authentication', 'OpenID' );
+					throw new Exception( 'Authorization failed: User cancelled authorization.' );
 					break;
 			}
 		}
@@ -45,26 +49,31 @@ class OpenID extends Plugin
 			self::openid_start();
 		}
 		else {
+			EventLog::log( 'Authorization failed: unknown error.', 'err', 'authentication', 'OpenID' );
 			throw new Exception( 'Authorization failed: unknown error.' );
 		}
 	}
 
-	public function action_plugin_activation( $file ) {		
+	public function action_plugin_activation( $file )
+	{
 		if ( realpath( $file ) == __FILE__ ) {
 			if ( !extension_loaded('curl') && !@dl('curl') ) {
-				throw new Exception( 'Could not load CURL, you need CURL for OpenID to work.' );
+				EventLog::log( 'Could not load CURL, which is needed for OpenID to work.', 'err', 'authentication', 'OpenID' );
+				throw new Exception( 'Could not load CURL, which is needed for OpenID to work.' );
 			}
 			EventLog::register_type( 'authentification', 'OpenID' );
 		}
 	}
 
-	public function action_plugin_deactivation( $file ) {
+	public function action_plugin_deactivation( $file )
+	{
 		if ( realpath( $file ) == __FILE__ ) {
 			EventLog::unregister_type( 'OpenID' );
 		}
 	}
 
-	public function action_init() {
+	public function action_init()
+	{
 		if ( session_id() == '' ) {
 			session_start();
 		}
@@ -72,13 +81,15 @@ class OpenID extends Plugin
 		Stack::add( 'template_stylesheet', array( Site::get_url('user').'/plugins/openid/openid.css', 'screen' ), 'openid_style' );
 	}
 
-	public function action_theme_loginform_before() {
+	public function action_theme_loginform_before()
+	{
 		if ( isset( $_GET['openid_url'] ) ) {
 			echo '<hr><div class="alert"><strong>If you have an existing account</strong>, sign in so we can assign your OpenID identifer to it.</div>';
 		}
 	}
-	
-	public function action_theme_loginform_after() {
+
+	public function action_theme_loginform_after()
+	{
 	 // @todo Remove the !isset( $_GET['openid_url'] ) once registration works in Habari.
 		if ( ( Controller::get_action() != 'register' ) && !isset( $_GET['openid_url'] ) ) {
 			echo '
@@ -95,92 +106,107 @@ class OpenID extends Plugin
 		}
 	}
 
-	public function action_theme_loginform_controls() {
+	public function action_theme_loginform_controls()
+	{
 		if ( isset( $_GET['openid_url'] ) ) {
 			echo '<input type="hidden" value="'.$_GET['openid_url'].'" name="habari_openid_url">';
 		}
 	}
-	
+
 	/* Uncomment once registration is supported by Habari.
-	public function action_theme_registerform_controls() {
+	public function action_theme_registerform_controls()
+	{
 		if ( isset( $_GET['openid_url'] ) ) {
 			echo '<input type="hidden" value="'.$_GET['openid_url'].'" name="habari_openid_url">';
 		}
 	} */
 
-	public function action_theme_admin_user( $user ) {
-		$openid_url= isset( $user->info->openid_url ) ? $user->info->openid_url : '';
+	public function action_theme_admin_user( $user )
+	{
+		$openid_url = isset( $user->info->openid_url ) ? $user->info->openid_url : '';
 		echo '<p><label for="openid_url">OpenID Identifier:</label></p>';
 		echo '<p><input type="text" value="'.$openid_url.'" name="habari_openid_url" disabled></p>';
 	}
-	
-	public function action_user_identify() {
+
+	public function action_user_identify()
+	{
 		if ( ( Controller::get_action() == 'login' ) && !empty( $_POST['openid_url'] ) ) {
 			self::openid_start();
 		}
 	}
 
 	// TODO: Add more security against form hijacking (for instance, check against server sent data)
-	public function action_user_authenticate_successful( $user ) {
+	public function action_user_authenticate_successful( $user )
+	{
 		if ( !empty( $_POST['habari_openid_url'] ) ) {
-			$user->info->openid_url= $_POST['habari_openid_url'];
+			$user->info->openid_url = $_POST['habari_openid_url'];
 		}
 	}
 
-	function getOpenIDURL() {
+	function getOpenIDURL()
+	{
 		if ( empty( $_POST['openid_url'] ) ) {
+			EventLog::log( 'Expected an OpenID URL.', 'err', 'authentication', 'OpenID' );
 			throw new Exception( 'Expected an OpenID URL.' );
 		}
-		
+
 		return $_POST['openid_url'];
 	}
-	
-	function getReturnTo() {
+
+	function getReturnTo()
+	{
 		return URL::get('openid');
 	}
-	
-	function getTrustRoot() {
+
+	function getTrustRoot()
+	{
 		return Site::get_url('habari');
 	}
-	
-	function getStore() {
-		$store_path= "/tmp/_php_consumer_test";
-		
+
+	function getStore()
+	{
+		$store_path = "/tmp/_php_consumer_test";
+
 		if ( !file_exists( $store_path ) && !mkdir( $store_path ) ) {
+			EventLog::log( 'Could not create the FileStore directory: ' . $store_path, 'err', 'authentication', 'OpenID' );
 			throw new Exception( 'Could not create the FileStore directory: ' . $store_path . '. Please check the effective permissions.' );
 		}
-		
+
 		return new Auth_OpenID_FileStore( $store_path );
 	}
-	
-	function getConsumer() {
+
+	function getConsumer()
+	{
 		require_once "Auth/OpenID/Consumer.php";
 		require_once "Auth/OpenID/FileStore.php";
 		require_once "Auth/OpenID/SReg.php";
-		$store= self::getStore();
+		$store = self::getStore();
 		return new Auth_OpenID_Consumer( $store );
 	}
-	
-	function openid_start() {
-		$openid= self::getOpenIDURL();
-		$consumer= self::getConsumer();
-		
-		$auth_request= $consumer->begin( $openid );
-		
+
+	function openid_start()
+	{
+		$openid = self::getOpenIDURL();
+		$consumer = self::getConsumer();
+
+		$auth_request = $consumer->begin( $openid );
+
 		if ( !$auth_request ) {
-			throw new Exception( 'Authentication error; not a valid OpenID.' );
+			EventLog::log( 'Authentication error: Not a valid OpenID.', 'err', 'authentication', 'OpenID' );
+			throw new Exception( 'Authentication error: Not a valid OpenID.' );
 		}
-		
-		$sreg_request= Auth_OpenID_SRegRequest::build( array( 'nickname' ), array( 'fullname', 'email' ) );
-		
+
+		$sreg_request = Auth_OpenID_SRegRequest::build( array( 'nickname' ), array( 'fullname', 'email' ) );
+
 		if ( $sreg_request ) {
 			$auth_request->addExtension( $sreg_request );
 		}
-		
+
 		if ( $auth_request->shouldSendRedirect() ) {
-			$redirect_url= $auth_request->redirectURL( self::getTrustRoot(), self::getReturnTo() );
-			
+			$redirect_url = $auth_request->redirectURL( self::getTrustRoot(), self::getReturnTo() );
+
 			if ( Auth_OpenID::isFailure( $redirect_url ) ) {
+				EventLog::log( 'Could not redirect to server: ' . $redirect_url->message, 'err', 'authentication', 'OpenID' );
 				throw new Exception( 'Could not redirect to server: ' . $redirect_url->message );
 			}
 			else {
@@ -188,10 +214,11 @@ class OpenID extends Plugin
 			}
 		}
 		else {
-			$form_id= 'openid_message';
-			$form_html= $auth_request->formMarkup( self::getTrustRoot(), self::getReturnTo(), false, array( 'id' => $form_id ) );
-			
+			$form_id = 'openid_message';
+			$form_html = $auth_request->formMarkup( self::getTrustRoot(), self::getReturnTo(), false, array( 'id' => $form_id ) );
+
 			if ( Auth_OpenID::isFailure( $form_html ) ) {
+				EventLog::log( 'Could not prepare redirection form: ' . $form_html->message, 'err', 'authentication', 'OpenID' );
 				throw new Exception( 'Could not prepare redirection form: ' . $form_html->message );
 			}
 			else {
@@ -208,31 +235,35 @@ class OpenID extends Plugin
 			}
 		}
 	}
-	
-	function openid_end() {
-		$consumer= self::getConsumer();
-		$return_to= self::getReturnTo();
-		$response= $consumer->complete( $return_to );
-		
+
+	function openid_end()
+	{
+		$consumer = self::getConsumer();
+		$return_to = self::getReturnTo();
+		$response = $consumer->complete( $return_to );
+
 		switch( $response->status ) {
 			case Auth_OpenID_CANCEL:
+				EventLog::log( 'Verification cancelled.', 'err', 'authentication', 'OpenID' );
 				throw new Exception( 'Verification cancelled.' );
 				break;
 			case Auth_OpenID_FAILURE:
+				EventLog::log( 'OpenID authentication failed: ' . $response->message, 'err', 'authentication', 'OpenID' );
 				throw new Exception( 'OpenID authentication failed: ' . $response->message );
 				break;
 			case Auth_OpenID_SUCCESS:
-				$openid= $response->getDisplayIdentifier();
-				$esc_identity= htmlspecialchars( $openid, ENT_QUOTES );
-				
-				$user= Users::get_by_info( 'openid_url', $openid );
+				$openid = $response->getDisplayIdentifier();
+				$esc_identity = htmlspecialchars( $openid, ENT_QUOTES );
+
+				$user = Users::get_by_info( 'openid_url', $openid );
 				if ( count( $user ) != 0 ) {
 					if ( count( $user ) > 1 ) {
-						throw new Exception( 'Authentication error: more than one user have this OpenID.' );
+						EventLog::log( 'Authentication error: More than one user has this OpenID.', 'err', 'authentication', 'OpenID' );
+						throw new Exception( 'Authentication error: More than one user has this OpenID.' );
 					}
 					$user[0]->remember();
 					EventLog::log( 'Successful login for ' . $user[0]->username, 'info', 'authentication', 'OpenID' );
-					
+
 					header( "HTTP/1.1 100 Continue" );
 					header( "Location: " . Site::get_url( 'admin' ) );
 					header( "Connection: close" );
