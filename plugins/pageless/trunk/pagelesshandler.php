e6f801014cac5112876391ddbbad6a9cc68cc47f
arthus <arthus@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-08-02 05:29:58 +0000

    Adding pageless plugin, with permission from author
    git-svn-id: http://svn.habariproject.org/habari-extras@730 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/pageless/trunk/pagelesshandler.php
@@ -0,0 +1,63 @@
+<?php
+class PagelessHandler extends ActionHandler
+{
+	private $theme = null;
+	private $default_fields = array(
+		'slug' => ''
+	);
+
+	/**
+	 * Constructor for the default theme handler.  Here, we
+	 * automatically load the active theme for the installation,
+	 * and create a new Theme instance.
+	 */
+	public function __construct()
+	{
+		$this->theme = Themes::create();
+	}
+
+	public function act_display_pageless()
+	{
+		$this->handler_vars = array_merge($this->default_fields, $this->handler_vars);
+		$post = Post::get( array( 'slug' => $this->handler_vars['slug'] ) );
+		if ( $post instanceof Post ) {
+			// Default params
+			$params = array(
+				'before' => $post->pubdate,
+				'content_type' => $post->content_type,
+				'limit' => Options::get( 'pageless__num_item' ),
+				'orderby' => 'pubdate DESC'
+				);
+
+			// Additional filters, in other word, handling act_display
+			if ( array_key_exists('type', $this->handler_vars) ) {
+				if ( $this->handler_vars['type'] === 'tag' ) {
+					$params['tag_slug'] = $this->handler_vars['param'];
+				} else
+				if ( $this->handler_vars['type'] === 'date' ) {
+					$date = explode('/', $this->handler_vars['param']);
+					$params_count = count($date);
+					switch ( $params_count ) {
+						case 3:
+							$params['day'] = $date[2];
+						case 2:
+							$params['month'] = $date[1];
+						case 1:
+							$params['year'] = $date[0];
+						default:
+							break;
+					}
+				} else
+				if ( $this->handler_vars['type'] === 'search' ) {
+					$params['criteria'] = $this->handler_vars['param'];
+				}
+			}
+
+			// Get $posts -> Assign $posts to theme -> Display template
+			$posts = Posts::get( $params );
+			$this->theme->assign( 'posts', $posts );
+			$this->theme->display( 'pageless' );
+		}
+	}
+}
+?>
