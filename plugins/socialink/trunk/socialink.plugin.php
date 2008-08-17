f750df48f4adf23ba7e6d58e25f186057601ef45
ayunyan <ayunyan@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-08-17 08:45:28 +0000

    plugin:socialink Added goo Bookmark (http://bookmark.goo.ne.jp/)
    git-svn-id: http://svn.habariproject.org/habari-extras@759 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/socialink/trunk/socialink.plugin.php
+++ plugins/socialink/trunk/socialink.plugin.php
@@ -1,12 +1,12 @@
 <?php
-/*
-
-  Socialink
-
-  Revision: $Id$
-  Head URL: $URL$
-
-*/
+/**
+ * Socialink
+ *
+ * @version $Id$
+ * @author ayunyan
+ * @author rickc (@535)
+ * @author dmondark (@582)
+ */
 class Socialink extends Plugin
 {
 	var $services= array(
@@ -15,25 +15,26 @@ class Socialink extends Plugin
 		'digg' => array('name' => 'Digg', 'url' => 'http://digg.com/submit?phase=2&url=%PERMALINK%'),
 		'delicious' => array('name' => 'del.icio.us', 'url' => 'http://del.icio.us/post?url=%PERMALINK%'),
 		'technorati' => array('name' => 'Technorati', 'url' => 'http://technorati.com/faves?add=%PERMALINK%'),
-		'google' => array('name' => 'Google', 'url' => "javascript:(function(){var a=window,b=document,c=encodeURIComponent,d=a.open('http://www.google.com/bookmarks/mark?op=edit&output=popup&bkmk=%PERMALINK%&title=%TITLE%','bkmk_popup','left='+((a.screenX||a.screenLeft)+10)+',top='+((a.screenY||a.screenTop)+10)+',height=420px,width=550px,resizable=1,alwaysRaised=1');a.setTimeout(function(){d.focus()},300)})();"),
-		'yahoo' => array('name' => 'Yahoo! My Web 2.0', 'url' => 'http://myweb2.search.yahoo.com/myresults/bookmarklet?u=%PERMALINK%&t=%TITLE%'),
+		'google' => array('name' => 'Google', 'url' => "javascript:(function(){var a=window,b=document,c=encodeURIComponent,d=a.open('http://www.google.com/bookmarks/mark?op=edit&amp;output=popup&amp;bkmk=%PERMALINK%&amp;title=%TITLE%','bkmk_popup','left='+((a.screenX||a.screenLeft)+10)+',top='+((a.screenY||a.screenTop)+10)+',height=420px,width=550px,resizable=1,alwaysRaised=1');a.setTimeout(function(){d.focus()},300)})();"),
+		'yahoo' => array('name' => 'Yahoo! My Web 2.0', 'url' => 'http://myweb2.search.yahoo.com/myresults/bookmarklet?u=%PERMALINK%&amp;t=%TITLE%'),
 		'furl' => array('name' => 'furl', 'url' => 'http://www.furl.net/storeIt.jsp?u=%PERMALINK%'),
-		'reddit' => array('name' => 'Reddit', 'url' => 'http://reddit.com/submit?url=%PERMALINK%&title=%PERMALINK%'),
-		'magnolia' => array('name' => 'Ma.gnolia', 'url' => 'http://ma.gnolia.com/bookmarklet/add?url=%PERMALINK%&title=%TITLE%'),
-		'faves' => array('name' => 'Faves', 'url' => 'http://faves.com/Authoring.aspx?u=%PERMALINK%&t=%TITLE%'),
-		'blinklist' => array('name' => 'blinklist', 'url' => 'http://www.blinklist.com/?Action=Blink/addblink.php&Description=&Url=%PERMALINK%&Title=%TITLE%'),
-		'stumbleupon' => array( 'name' => 'StumbleUpon', 'url'=> 'http://www.stumbleupon.com/submit?url=%PERMALINK%&title=%TITLE%'),
-		'diigo' => array( 'name' => 'Diigo', 'url'=> 'hhttp://www.diigo.com/post?url=%PERMALINK%&title=%TITLE%'),
+		'reddit' => array('name' => 'Reddit', 'url' => 'http://reddit.com/submit?url=%PERMALINK%&amp;title=%PERMALINK%'),
+		'magnolia' => array('name' => 'Ma.gnolia', 'url' => 'http://ma.gnolia.com/bookmarklet/add?url=%PERMALINK%&amp;title=%TITLE%'),
+		'faves' => array('name' => 'Faves', 'url' => 'http://faves.com/Authoring.aspx?u=%PERMALINK%&amp;t=%TITLE%'),
+		'blinklist' => array('name' => 'blinklist', 'url' => 'http://www.blinklist.com/?Action=Blink/addblink.php&amp;Description=&amp;Url=%PERMALINK%&amp;Title=%TITLE%'),
+		'stumbleupon' => array( 'name' => 'StumbleUpon', 'url'=> 'http://www.stumbleupon.com/submit?url=%PERMALINK%&amp;title=%TITLE%'),
+		'diigo' => array( 'name' => 'Diigo', 'url'=> 'hhttp://www.diigo.com/post?url=%PERMALINK%&amp;title=%TITLE%'),
 
 		// Japan
-		'hatena' => array('name' => 'Hatena Bookmark', 'url' => "javascript:(function(){window.open('http://b.hatena.ne.jp/add?mode=confirm&is_bm=1&title=%TITLE%&url=%PERMALINK%','socialink','width=550,height=600,resizable=1,scrollbars=1');})();"),
+		'hatena' => array('name' => 'Hatena Bookmark', 'url' => "javascript:(function(){window.open('http://b.hatena.ne.jp/add?mode=confirm&amp;is_bm=1&amp;title=%TITLE%&amp;url=%PERMALINK%','socialink','width=550,height=600,resizable=1,scrollbars=1');})();"),
 		'yahoojbookmarks' => array('name' => 'Yahoo! JAPAN Bookmarks', 'url' => "javascript:(function(){window.open('http://bookmarks.yahoo.co.jp/bookmarklet/showpopup?t=%TITLE%&amp;u=%PERMALINK%&amp;opener=bm&amp;ei=UTF-8','socialink','width=550px,height=480px,status=1,location=0,resizable=1,scrollbars=0,left=100,top=50',0);})();"),
-		'topicit' => array('name' => 'TopicIT@nifty', 'url' => "javascript:(function(){window.open('http://topic.nifty.com/up/add?mode=2&amp;topic_title=%TITLE%&topic_url=%PERMALINK%');})();"),
+		'topicit' => array('name' => 'TopicIT@nifty', 'url' => "javascript:(function(){window.open('http://topic.nifty.com/up/add?mode=2&amp;topic_title=%TITLE%&amp;topic_url=%PERMALINK%');})();"),
 		'buzzurl' => array('name' => 'Buzzurl', 'url' => 'http://buzzurl.jp/entry/%PERMALINK%'),
 		'choix' => array('name' => 'Choix', 'url' => 'http://www.choix.jp/bloglink/%PERMALINK%'),
-		'newsing' => array('name' => 'newsing', 'url' => 'http://newsing.jp/add?url=%PERMALINK%&title=%TITLE%'),
-		'livedoorclip' => array('name' => 'livedoor Clip', 'url' => 'http://clip.livedoor.com/redirect?link=%PERMALINK%&title=%TITLE%&ie=utf-8'),
-		'pookmark' => array('name' => 'POOKMARK Airlines', 'url' => 'http://pookmark.jp/post?url=%PERMALINK%&title=%TITLE%'),
+		'newsing' => array('name' => 'newsing', 'url' => 'http://newsing.jp/add?url=%PERMALINK%&amp;title=%TITLE%'),
+		'livedoorclip' => array('name' => 'livedoor Clip', 'url' => 'http://clip.livedoor.com/redirect?link=%PERMALINK%&amp;title=%TITLE%&amp;ie=utf-8'),
+		'pookmark' => array('name' => 'POOKMARK Airlines', 'url' => 'http://pookmark.jp/post?url=%PERMALINK%&amp;title=%TITLE%'),
+        'goobookmark' => array('name' => 'goo Bookmark', 'url' => 'http://bookmark.goo.ne.jp/add/detail/?url=%PERMALINK%'),
 		);
 
 	/**
@@ -55,7 +56,7 @@ class Socialink extends Plugin
 			);
 	}
 
-  /*
+	/**
 	 * action: plugin_activation
 	 *
 	 * @access public
@@ -70,7 +71,7 @@ class Socialink extends Plugin
 		Options::set( 'socialink__services', serialize( array( 'digg', 'delicious', 'technorati', 'google', 'yahoo', 'furl', 'reddit', 'magnolia' ) ) );
 	}
 
-	/*
+	/**
 	 * action: update_check
 	 *
 	 * @access public
@@ -81,7 +82,7 @@ class Socialink extends Plugin
 		Update::add( 'Socialink', '58c939f3-26ae-11dd-b5d6-001b210f913f', $this->info->version );
 	}
 
-  /*
+	/**
 	 * action: plugin_ui
 	 *
 	 * @access public
@@ -108,7 +109,7 @@ class Socialink extends Plugin
 		}
 	}
 
-  /*
+	/**
 	 * filter: plugin_config
 	 *
 	 * @access public
@@ -122,7 +123,7 @@ class Socialink extends Plugin
 		return $actions;
 	}
 
-  /*
+	/**
 	 * filter: post_content_out
 	 *
 	 * @access public
