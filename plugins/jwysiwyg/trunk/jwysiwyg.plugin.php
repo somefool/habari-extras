2f1fa8b5f3db2a9997f6736dbb3b953b46cf4cb1
michaeltwofish <michaeltwofish@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-08-20 03:32:22 +0000

    Put the content label on the outside of the edit textarea when the area is empty. Not pretty, but better than it was.
    git-svn-id: http://svn.habariproject.org/habari-extras@775 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/jwysiwyg/trunk/jwysiwyg.plugin.php
+++ plugins/jwysiwyg/trunk/jwysiwyg.plugin.php
@@ -2,52 +2,56 @@
 
 class JWYSIWYG extends Plugin {
 
-  /**
-    * Required Plugin Information
-    **/
-  function info() {
-    return array(
-      'name' => 'JWYSIWYG',
-      'license' => 'Apache License 2.0',
-      'url' => 'http://habariproject.org/',
-      'author' => 'Habari Community',
-      'authorurl' => 'http://habariproject.org/',
-      'version' => '0.2',
-      'description' => 'Publish posts using the JWYSIWYG editor.',
-      'copyright' => '2008'
-    );
-  }
+	/*
+	 * Required Plugin Information
+	 */
+	function info()
+	{
+		return array(
+			'name' => 'JWYSIWYG',
+			'license' => 'Apache License 2.0',
+			'url' => 'http://habariproject.org/',
+			'author' => 'Habari Community',
+			'authorurl' => 'http://habariproject.org/',
+			'version' => '0.2',
+			'description' => 'Publish posts using the JWYSIWYG editor.',
+			'copyright' => '2008'
+		);
+	}
 
-  public function action_admin_header($theme) {
-    if ( $theme->page == 'publish' ) {
-      Stack::add('admin_header_javascript', $this->get_url() . '/jwysiwyg/jquery.wysiwyg.js');
-      Stack::add('admin_stylesheet', array($this->get_url() . '/jwysiwyg/jquery.wysiwyg.css', 'screen'));
-    }
-  }
+	public function action_admin_header($theme)
+	{
+		if ( $theme->page == 'publish' ) {
+			Stack::add('admin_header_javascript', $this->get_url() . '/jwysiwyg/jquery.wysiwyg.js');
+			Stack::add('admin_stylesheet', array($this->get_url() . '/jwysiwyg/jquery.wysiwyg.css', 'screen'));
+		}
+	}
 
-  public function action_admin_footer($theme) {
-    if ( $theme->page == 'publish' ) {
-      echo <<<JWYSIWYG
-      <script type="text/javascript">
-      $(function()
-      {
-        $('#content').wysiwyg();
-      });
-      habari.editor = {
-        insertSelection: function(value) {
-          var instance = $.data($('#content')[0], 'wysiwyg');
-          instance.setContent(instance.getContent() + value);
-        }
-      }
-      </script>
+	public function action_admin_footer($theme)
+	{
+		if ( $theme->page == 'publish' ) {
+			echo <<<JWYSIWYG
+			<script type="text/javascript">
+			$('[@for=content]').removeAttr('for');
+			$(function()
+			{
+				$('#content').wysiwyg();
+			});
+			habari.editor = {
+				insertSelection: function(value) {
+					var instance = $.data($('#content')[0], 'wysiwyg');
+					instance.setContent(instance.getContent() + value);
+				}
+			}
+			</script>
 JWYSIWYG;
-    }
-  }
+		}
+	}
 
-
-  public function action_update_check() {
-    Update::add( 'JWYSIWYG', 'b5f0c17d-22e6-4d6c-8011-c79481d5efc7',  $this->info->version );
-  }
+	public function action_update_check()
+	{
+		Update::add( 'JWYSIWYG', 'b5f0c17d-22e6-4d6c-8011-c79481d5efc7',  $this->info->version );
+	}
 }
 
 ?>
