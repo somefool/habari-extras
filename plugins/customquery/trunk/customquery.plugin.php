7f810b1cba677d394dfe9ae42c533cd1f6f4f9a3
mikelietz <mikelietz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-08-25 12:10:12 +0000

    customquery: FormUI update and added beacon.
    git-svn-id: http://svn.habariproject.org/habari-extras@818 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/customquery/trunk/customquery.plugin.php
+++ plugins/customquery/trunk/customquery.plugin.php
@@ -2,14 +2,14 @@
 
 class CustomQuery extends Plugin
 {
-	const VERSION= '0.1';
+	const VERSION= '0.2';
 	
 	public function info()
 	{
 		return array( 
 			'name' => 'Custom Query',
 			'author' => 'Habari Community',
-			'description' => 'Allows you to set the number of post to display for each page.',
+			'description' => 'Allows you to set the number of posts to display for each page.',
 			'url' => 'http://habariproject.org',
 			'version' => self::VERSION,
 			'license' => 'Apache License 2.0'
@@ -32,21 +32,28 @@ class CustomQuery extends Plugin
 					$ui = new FormUI( 'customquery' );
 					
 					foreach ( RewriteRules::get_active()->getArrayCopy() as $rule ) {
-						if ( strpos( $rule->name, 'display_' ) === 0 ) {
-							$ui->add( 'text', $rule->name, 'Number of Posts for ' . $rule->name );
+						if ( strpos( $rule->name, 'display_' ) === 0 and ( $rule->name <> 'display_404' )) {
+							$ui->append( 'text', $rule->name,'customquery__'. $rule->name,
+								 'Number of Posts for ' . $rule->name );
 						}
 					}
-					
+
+					$ui->append( 'submit', 'save', _t('Save') );					
 					$ui->out();
 					break;
 			}
 		}
 	}
+
+	public function action_update_check()
+	{
+	 	Update::add( 'Custom Query', '1fe407a0-729e-11dd-ad8b-0800200c9a66', $this->info->version );
+	}
 	
 	public function filter_template_where_filters( $where_filters )
 	{
-		if ( Options::get( 'customquery:' . URL::get_matched_rule()->name ) ) {
-			$where_filters['limit']= Options::get( 'customquery:' . URL::get_matched_rule()->name );
+		if ( Options::get( 'customquery__' . URL::get_matched_rule()->name ) ) {
+			$where_filters['limit']= Options::get( 'customquery__' . URL::get_matched_rule()->name );
 		}
 		return $where_filters;
 	}
