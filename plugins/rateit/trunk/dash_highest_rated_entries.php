c7fcaf372e8eb80254fdab269cfb910739ff6de1
ayunyan <ayunyan@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-09-01 17:16:30 +0000

    plugin:rateit round rating in dashboard
    git-svn-id: http://svn.habariproject.org/habari-extras@873 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/rateit/trunk/dash_highest_rated_entries.php
+++ plugins/rateit/trunk/dash_highest_rated_entries.php
@@ -3,7 +3,7 @@
   <li class="item clear">
     <span class="date pct15 minor"><a href="<?php echo URL::get('display_entries_by_date', array('year' => date('Y', strtotime($post->pubdate)), 'month' => date('m', strtotime($post->pubdate)), 'day' => date('d', strtotime($post->pubdate)))); ?>" title="<?php printf(_t('Posted at %1$s'), date('g:m a \o\n F jS, Y', strtotime($post->pubdate))); ?>"><?php echo date('M j', strtotime($post->pubdate)); ?></a></span>
     <span class="title pct75"><a href="<?php echo $post->permalink; ?>"><?php echo $post->title; ?></a> <a class="minor" href="<?php Site::out_url('habari'); ?>/admin/user/<?php echo $post->author->username; ?>"> <?php _e('by'); ?> <?php echo $post->author->displayname; ?></a></span>
-    <span class="comments pct10"><?php echo $post->info->rateit_rating; ?></span>
+    <span class="comments pct10"><?php echo round($post->info->rateit_rating, 2); ?></span>
   </li>
 <?php endwhile; ?>
 </ul>
