89d4fe6f3c3ef6e504821e9c284cab8903a61ea5
rickc <rickc@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-09-02 11:30:12 +0000

    git-svn-id: http://svn.habariproject.org/habari-extras@879 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/xpsf-mini/trunk/xpsf-mini.plugin.php
@@ -0,0 +1,67 @@
+<?php
+/*
+* XSPF Mini Plugin for Habari
+* Embeds an audio player in posts
+ * @version $Id$
+ * @copyright 2008
+ */
+
+Class XSPFMini extends Plugin
+{
+
+	public function info()
+	{
+		return array(
+			'name'=>'XSPF Mini',
+			'version'=>'0.1',
+			'url'=>'http://habariproject.org/',
+			'author'=>'Habari Community',
+			'authorurl'=>'http://habariproject.org/',
+			'license'=>'ASL 2.0',
+			'description'=>'An audio player using the xspf slim player.'
+		);
+	}
+
+	public function action_update_check()
+	{
+		Update::add( 'XSPF Mini', '0DE511F0-7890-11DD-9F6E-3D7356D89593',  $this->info->version );
+	}
+
+	public function filter_post_content_out( $content )
+	{
+		preg_match_all( '/<!-- file:(.*) -->/i', $content, $matches, PREG_PATTERN_ORDER );
+		$matches_obj = new ArrayObject( $matches[1] );
+
+		for( $it = $matches_obj->getIterator(); $it->valid(); $it->next() ){
+			$content = str_ireplace( '<!-- file:' . $it->current() . ' -->', $this->embed_player( $it->current() ), $content );
+		}
+
+		return $content;
+	}
+
+	public function embed_player( $file )
+	{
+		$player = '<p><object width="300" height="20">';
+		$player .= '<param name="movie" value="' . $this->get_url() . '/xspf_player_slim.swf?song_url=' . $file . '&song_title=' . basename( $file, '.mp3' ) . '&player_title=' . htmlspecialchars( Options::get( 'title' ), ENT_COMPAT, 'UTF-8' ) . '" />';
+		$player .= '<param name="wmode" value="transparent" />';
+		$player .= '<embed src="' . $this->get_url() . '/xspf_player_slim.swf?song_url=' . $file . '&song_title=' . basename( $file, '.mp3' ). '&player_title=' . htmlspecialchars( Options::get( 'title' ), ENT_COMPAT, 'UTF-8' ) . '" type="application/x-shockwave-flash" wmode="transparent" width="300" height="20"></embed>';
+		$player .= '</object></p>';
+
+		return $player;
+	}
+
+	public function action_admin_header( $theme ) 
+	{
+		echo <<< XSPFMINI
+
+<script type="text/javascript">
+$.extend(habari.media.output.audio_mpeg3, {
+add_xspfmini_player: function(fileindex, fileobj) {
+habari.editor.insertSelection('<!-- file:'+fileobj.url+' -->');
+}});
+</script>
+
+XSPFMINI;
+	}
+}
+?>
