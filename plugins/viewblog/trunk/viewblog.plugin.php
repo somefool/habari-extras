bce9bbce6899f552d9ecd994daa1684cbec95714
arthus <arthus@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-09-10 00:18:58 +0000

    Plugin: ViewBlog initial commit
    git-svn-id: http://svn.habariproject.org/habari-extras@902 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/viewblog/trunk/viewblog.plugin.php
@@ -0,0 +1,36 @@
+<?php
+
+class ViewBlog extends Plugin
+{
+	const VERSION= '1.0';
+	
+	/**
+	 * Required plugin info() implementation provides info to Habari about this plugin.
+	 */
+	public function info()
+	{
+		return array(
+			'name' => 'View Blog Menu Item',
+			'url' => 'http://blog.voodoolabs.net/',
+			'author' =>'Thomas Coats',
+			'authorurl' => 'http://blog.voodoolabs.net/',
+			'version' => self::VERSION,
+			'description' => 'Adds an extra menu item for viewing the blog',
+			'license' => 'Apache License 2.0',
+		);
+	}
+    
+	public function filter_adminhandler_post_loadplugins_main_menu($mainmenus)
+	{
+        $mainmenus['view'] =
+                array(
+                    'url' => Site::get_url('habari'),
+                    'title' => _t('View blog'),
+                    'text' => _t('View Blog'),
+                    'hotkey' => 'V',
+                    'selected' => '');
+        return $mainmenus;
+	}
+}
+?>
+
