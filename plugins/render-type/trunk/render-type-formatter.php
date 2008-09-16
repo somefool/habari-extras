aaa1c64e9b042c622e2ac5b63e6a2d6402cc61c3
bcse <bcse@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-09-16 23:38:07 +0000

    Plugin: Render Type: Follow the Coding Standards
    git-svn-id: http://svn.habariproject.org/habari-extras@948 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/render-type/trunk/render-type-formatter.php
+++ plugins/render-type/trunk/render-type-formatter.php
@@ -2,21 +2,23 @@
 
 class RenderTypeFormat extends Format
 {
-  public function render_type( $content,
-			       $font_file, 
-			       $font_size = 28,
-			       $font_color = '#000000FF',
-			       $background_color = '#00000000',
-			       $output_format = 'png' )
-  {
-    return Plugins::filter( 'render_type',
-			    $content,
-			    $font_file,
-			    $font_size,
-			    $font_color,
-			    $background_color,
-			    $output_format );
-  }
+	public function render_type(
+		$content,
+		$font_file, 
+		$font_size = 28,
+		$font_color = '#000000FF',
+		$background_color = '#00000000',
+		$output_format = 'png' )
+	{
+		return Plugins::filter(
+			'render_type',
+			$content,
+			$font_file,
+			$font_size,
+			$font_color,
+			$background_color,
+			$output_format );
+	}
 }
 
 ?>
--- plugins/render-type/trunk/render-type.plugin.php
+++ plugins/render-type/trunk/render-type.plugin.php
@@ -1,140 +1,131 @@
 <?php
 
-  /**
-   * RenderType Plugin Class
-   *
-   * This plugin adds a formatter called 'render_type' which templates can use
-   * to render text, such as titles, as images in a chosen font using the data:
-   * URI scheme. It requires ImageMagick and the PHP Imagick library. (It will
-   * not work with GD.)
-   *
-   * Here is a minimal example of how you might use Render Type in
-   * action_init_theme in your theme.php (see the documentation for
-   * filter_render_type below for more details):
-   *
-   * Format::apply( 'render_type', 'post_title_out', '/path/to/font.ttf', 28, 'black' );
-   *
-   * data: URIs work in all modern browsers except Internet Explorer
-   * (which does not support them at all as of version 7). This plugin uses
-   * conditional comments to hide the data: images from all versions of IE
-   * and to show it the text instead, which of course you may style as
-   * desired.
-   *
-   * The image data for a unique combination of input string, font, size, color,
-   * background color, and output format (e.g. PNG, JPEG, etc.) is cached
-   * after the first time it is requested, so performance should be reasonable,
-   * though I've performed no load testing. This cache is currently never
-   * expired -- not even if you deactivate the plugin. This may be
-   * addressed in the future.
-   *
-   * Nota bene: This plugin is not suitable for hiding email addresses from
-   * harvesters -- the original text appears in the formatter's output in the
-   * IE conditional comments and in the alt and title attributes of the img element.
-   *
-   * @package render_type
-   **/
+/**
+ * RenderType Plugin Class
+ *
+ * This plugin adds a formatter called 'render_type' which templates can use
+ * to render text, such as titles, as images in a chosen font. It requires
+ * ImageMagick and the PHP Imagick library.(It will not work with GD.)
+ *
+ * Here is a minimal example of how you might use Render Type in
+ * action_init_theme in your theme.php(see the documentation for
+ * filter_render_type below for more details):
+ *
+ * Format::apply( 'render_type', 'post_title_out', '/path/to/font.ttf' );
+ *
+ * The image data for a unique combination of input string, font, size, color,
+ * background color, and output format(e.g. PNG, JPEG, etc.) is cached
+ * after the first time it is requested, so performance should be reasonable,
+ * though I've performed no load testing. This cache is currently never
+ * expired -- not even if you deactivate the plugin. This may be
+ * addressed in the future.
+ *
+ * Nota bene: This plugin is not suitable for hiding email addresses from
+ * harvesters -- the original text appears in the formatter's output in the
+ * alt and title attributes of the img element.
+ *
+ * @package render_type
+ **/
 
 require_once 'render-type-formatter.php';
 
 class RenderTypePlugin extends Plugin
 {
-  const VERSION='0.5';
+	const VERSION = '0.5';
 
-  /**
-   * function info
-   * Returns information about this plugin
-   * @return array Plugin info array
-   **/
-  public function info()
-  {
-    return array (
-		  'name' => 'Render Type',
-		  'url' => 'http://svn.habariproject.org/habari-extras/plugins/render-type/',
-		  'author' => 'Eli Naeher',
-		  'authorurl' => 'http://flyoverblues.com',
-		  'version' => self::VERSION,
-		  'description' => 'A plugin for rendering text inline as data: PNGs.',
-		  'license' => 'This plugin is in the public domain.',
-		  );
-  }
+	/**
+	 * function info
+	 * Returns information about this plugin
+	 * @return array Plugin info array
+	 **/
+	public function info()
+	{
+		return array(
+			'name' => 'Render Type',
+			'url' => 'http://svn.habariproject.org/habari-extras/plugins/render-type/',
+			'author' => 'Eli Naeher',
+			'authorurl' => 'http://flyoverblues.com',
+			'version' => self::VERSION,
+			'description' => 'A plugin for rendering text inline as data: PNGs.',
+			'license' => 'Public Domain.',
+			);
+	}
 
-  /**
-   * function action_update_check
-   * Check for more recent versions of this plugin.
-   **/
+	/**
+	 * function action_update_check
+	 * Check for more recent versions of this plugin.
+	 **/
 
-  public function action_update_check ( )
-  {
-    Update::add( 'RenderType', 'FEFD6638-838E-11DD-8AFF-17DB55D89593', self::VERSION );
-  }
+	public function action_update_check()
+	{
+		Update::add( 'RenderType', 'FEFD6638-838E-11DD-8AFF-17DB55D89593', self::VERSION );
+	}
 
-  /**
-   * function filter_render_type
-   * Returns HTML markup containing an image with a data: URI
-   * @param string $content The text to be rendered
-   * @param string $font_file The path to a font file in a format ImageMagick can handle
+	/**
+	 * function filter_render_type
+	 * Returns HTML markup containing an image with a data: URI
+	 * @param string $content The text to be rendered
+	 * @param string $font_file The path to a font file in a format ImageMagick can handle
    * @param integer $font_size The font size, in pixels (defaults to 28)
    * @param string $font_color The font color (defaults to 'black')
    * @param string $background_color The background color (defaults to 'transparent')
    * @param string $output_format The image format to use (defaults to 'png')
-   * @return string HTML markup containing the rendered image
-   **/
+	 * @return string HTML markup containing the rendered image
+	 **/
 
-  public function filter_render_type ( $content, $font_file, $font_size, $font_color, $background_color, $output_format)
-  {
-    $cache_group = strtolower( get_class( $this ) );
-    $cache_key = $font_file
-      . $font_size
-      . $font_color
-      . $background_color
-      . $output_format
-      . $content;
+	public function filter_render_type( $content, $font_file, $font_size, $font_color, $background_color, $output_format)
+	{
+		$cache_group = strtolower( get_class( $this ) );
+		$cache_key =
+			$font_file .
+			$font_size .
+			$font_color .
+			$background_color .
+			$output_format .
+			$content;
 
-    if ( Cache::has( array ( $cache_group, md5($cache_key) ) ) ) {
-      return '<img src="' . URL::get('display_rendertype', array('hash' => md5($cache_key), 'format' => $output_format)) . '" title="' . $content . '" alt="' . $content . '" />';
-    } else {
-      $draw = new ImagickDraw();
-      $draw->setFont($font_file);
-      $draw->setFontSize($font_size);
-      $draw->setFillColor(new ImagickPixel($font_color));
-      $draw->setTextEncoding('UTF-8');
-      $draw->annotation(0, $font_size * 2, $content);
-      $canvas = new Imagick();
-      $canvas->newImage(1000, $font_size * 5, new ImagickPixel($background_color));
-      $canvas->setImageFormat($output_format);
-      $canvas->drawImage($draw);
-      $canvas->trimImage(0);
-      Cache::set( array ( $cache_group, md5($cache_key) ), $canvas->getImageBlob() );
+		if ( ! Cache::has( array( $cache_group, md5( $cache_key ) ) ) ) {
+			$draw = new ImagickDraw();
+			$draw->setFont( $font_file );
+			$draw->setFontSize( $font_size );
+			$draw->setFillColor( new ImagickPixel( $font_color ) );
+			$draw->setTextEncoding( 'UTF-8' );
+			$draw->annotation(0, $font_size * 2, $content);
+			$canvas = new Imagick();
+			$canvas->newImage( 1000, $font_size * 5, new ImagickPixel( $background_color ) );
+			$canvas->setImageFormat( $output_format );
+			$canvas->drawImage( $draw );
+			$canvas->trimImage( 0 );
+			Cache::set( array( $cache_group, md5( $cache_key ) ), $canvas->getImageBlob() );
+		}
+		return '<img src="' . URL::get( 'display_rendertype', array( 'hash' => md5( $cache_key ), 'format' => $output_format ) ) . '" title="' . $content . '" alt="' . $content . '" />';
+	}
 
-      return '<img src="' . URL::get('display_rendertype', array('hash' => md5($cache_key), 'format' => $output_format)) . '" title="' . $content . '" alt="' . $content . '" />';
-    }
-  }
+	public function filter_rewrite_rules( $rules )
+	{
+		$rules[] = new RewriteRule(array(
+			'name' => 'display_rendertype',
+			'parse_regex' => '%^rendertype/(?P<hash>[0-9a-f]{32}).(?P<format>png)$%i',
+			'build_str' => 'rendertype/{$hash}.{$format}',
+			'handler' => 'UserThemeHandler',
+			'action' => 'display_rendertype',
+			'rule_class' => RewriteRule::RULE_PLUGIN,
+			'is_active' => 1,
+			'description' => 'display_rendertype'
+		));
+		return $rules;
+	}
 
-  public function filter_rewrite_rules ( $rules )
-  {
-    $rules[] = new RewriteRule(array(
-      'name' => 'display_rendertype',
-      'parse_regex' => '%^rendertype/(?P<hash>[0-9a-f]{32}).(?P<format>png)$%i',
-	  'build_str' => 'rendertype/{$hash}.{$format}',
-      'handler' => 'UserThemeHandler',
-      'action' => 'display_rendertype',
-      'rule_class' => RewriteRule::RULE_PLUGIN,
-      'is_active' => 1,
-      'description' => 'display_rendertype'
-    ));
-    return $rules;
-  }
-
-  public function action_handler_display_rendertype ( $handler_vars )
-  {
-    $cache_group = strtolower( get_class( $this ) );
-    if ( Cache::has( array ( $cache_group, $handler_vars['hash'] ) ) ) {
-      header('Content-type: image/' . $handler_vars['format']);
-      echo Cache::get( array ( $cache_group, $handler_vars['hash'] ) );
-    } else {
-      echo 'Cache not found';
-    }
-  }
+	public function action_handler_display_rendertype( $handler_vars )
+	{
+		$cache_group = strtolower( get_class( $this ) );
+		if( Cache::has( array( $cache_group, $handler_vars['hash'] ) ) ) {
+			header( 'Content-type: image/' . $handler_vars['format'] );
+			echo Cache::get( array( $cache_group, $handler_vars['hash'] ) );
+		} else {
+			echo 'Cache not found';
+		}
+	}
 }
 
 ?>
