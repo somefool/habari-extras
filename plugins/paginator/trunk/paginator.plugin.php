74fb04bda925bf80dabf7bec8ad57d7519c103ef
MattRead <MattRead@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-09-19 04:49:01 +0000

    coding style fixes. mad props to scoates for the tokenizer foo!
    git-svn-id: http://svn.habariproject.org/habari-extras@1002 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/paginator/trunk/paginator.plugin.php
+++ plugins/paginator/trunk/paginator.plugin.php
@@ -33,7 +33,7 @@ class Paginator extends Plugin {
 	{
 		switch ( $style ) {
 			case 'list':
-				$out= '<ul class="page_selector">';
+				$out = '<ul class="page_selector">';
 				foreach ( $pagination['pages'] as $page ) {
 					if ( is_array( $page ) ) {
 						$out.= '<li><a href="' . $page['url'] . '"' . ( ( $page['caption'] == $pagination['current'] ) ? ' class="current"'  : '' ) . '>' . $page['caption'] . '</a></li>';
@@ -44,7 +44,7 @@ class Paginator extends Plugin {
 				$out.= '</ul>';
 				break;
 			case 'span':
-				$out= '<span class="page_selector">';
+				$out = '<span class="page_selector">';
 				foreach ( $pagination['pages'] as $page ) {
 					if ( is_array( $page ) ) {
 					 $out.= '<a href="' . $page['url'] . '"' . ( ( $page['caption'] == $pagination['current'] ) ? ' class="current">[' . $page['caption'] . ']' : '>' . $page['caption'] ) . '</a>';
@@ -73,44 +73,44 @@ class Paginator extends Plugin {
 	 * @param array parameters to render the pagination
 	 * @return array contains a 'current' and 'pages' key
  	 **/
-	public function get( $current, $total, $rr_name= NULL, $settings = array() )
+	public function get( $current, $total, $rr_name = NULL, $settings = array() )
  	{
  		// Extract the style and remove it from the array.
  		if ( isset( $settings['style'] ) ) {
- 			$style= $settings['style'];
+ 			$style = $settings['style'];
  			unset( $settings['style'] );
  		}
  		else {
- 			$style= 'span';
+ 			$style = 'span';
  		}
  		
 		// If RewriteRule name is not supplied, use the current RewriteRule
 		if ( $rr_name == '' ) {
-			$rr= URL::get_matched_rule();
+			$rr = URL::get_matched_rule();
 		}
 		else {
 			list( $rr )= RewriteRules::by_name( $rr_name );
 		}
 		
 		// Retrieve the RewriteRule and aggregate an array of matching arguments
-		$rr_named_args= $rr->named_args;
-		$rr_args= array_merge( $rr_named_args['required'], $rr_named_args['optional']  );
-		$rr_args_values= array();
+		$rr_named_args = $rr->named_args;
+		$rr_args = array_merge( $rr_named_args['required'], $rr_named_args['optional']  );
+		$rr_args_values = array();
 		foreach ( $rr_args as $rr_arg ) {
-			$rr_arg_value= Controller::get_var( $rr_arg );
+			$rr_arg_value = Controller::get_var( $rr_arg );
 			if ( $rr_arg_value != '' ) {
 				$rr_args_values[$rr_arg]= $rr_arg_value;
 			}
 		}
 
-		$settings= array_merge( $settings, $rr_args_values );
+		$settings = array_merge( $settings, $rr_args_values );
 
 		// Current page
 		if ( $current > $total ) {
-			$current= $total;
+			$current = $total;
  		}
 
-		$p= array( 'current' => $current );
+		$p = array( 'current' => $current );
 		
 		// 1 - First page
 		$p['pages'][1]['caption']= 1;
@@ -146,8 +146,8 @@ class Paginator extends Plugin {
 			$p['pages'][]['caption']= $total;
  		}
  
-		$count= count( $p['pages'] );
-		for($z= 1; $z <= $count; $z++) {
+		$count = count( $p['pages'] );
+		for($z = 1; $z <= $count; $z++) {
 			if ( is_array( $p['pages'][$z] ) ) {
 				$p['pages'][$z]['url']= $rr->build( array_merge($settings, array('page'=>$p['pages'][$z]['caption'])), false);
 			}
@@ -156,7 +156,7 @@ class Paginator extends Plugin {
  		return self::format( $p, $style );
  	}
  	
- 	public function out( $current, $total, $rr_name= NULL, $settings = array() ) {
+ 	public function out( $current, $total, $rr_name = NULL, $settings = array() ) {
  		print self::get( $current, $total, $rr_name, $settings );
  	}
 
