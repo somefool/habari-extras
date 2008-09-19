01666d09d39311ebc8adb0127a3ab3e2d5682f06
MattRead <MattRead@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-09-19 04:48:56 +0000

    coding style fixes. mad props to scoates for the tokenizer foo!
    git-svn-id: http://svn.habariproject.org/habari-extras@995 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/opensearch/trunk/opensearch.plugin.php
+++ plugins/opensearch/trunk/opensearch.plugin.php
@@ -72,16 +72,16 @@ class OpenSearch extends Plugin {
 	 * @return string XML OpenSearch results Atom feed
 	 */
 	public function filter_atom_get_collection( $xml, $params ) {
-		$criteria= $params['criteria'];
+		$criteria = $params['criteria'];
 		
-		$totalResults= Posts::get( $params )->count_all();
-		$startIndex= isset( $params['page'] ) ? $params['page'] : 1;
-		$itemsPerPage= isset( $this->handler_vars['count'] ) ? $this->handler_vars['count'] : Options::get( 'pagination' );
+		$totalResults = Posts::get( $params )->count_all();
+		$startIndex = isset( $params['page'] ) ? $params['page'] : 1;
+		$itemsPerPage = isset( $this->handler_vars['count'] ) ? $this->handler_vars['count'] : Options::get( 'pagination' );
 		
 		$xml->addChild( 'opensearch:totalResults', $totalResults);
 		$xml->addChild( 'opensearch:startIndex', $startIndex );
 		$xml->addChild( 'opensearch:itemsPerPage', $itemsPerPage );
-		$xml_os_query= $xml->addChild( 'opensearch:Query' );
+		$xml_os_query = $xml->addChild( 'opensearch:Query' );
 		$xml_os_query->addAttribute( 'role', 'request' );
 		$xml_os_query->addAttribute( 'searchTerms', $criteria );
 		$xml_os_query->addAttribute( 'startPage', 1 );
@@ -106,20 +106,20 @@ class OpenSearch extends Plugin {
 	 * @return string XML OpenSearch description file
 	 */
 	public function osDescription() {
-		$template_url= Site::get_url( 'habari' ) . '/search/{searchTerms}/page/{startPage}/count/{count}/atom';
+		$template_url = Site::get_url( 'habari' ) . '/search/{searchTerms}/page/{startPage}/count/{count}/atom';
 		
-		$xml= '<?xml version="1.0" encoding="UTF-8"?><OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/"></OpenSearchDescription>';
+		$xml = '<?xml version="1.0" encoding="UTF-8"?><OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/"></OpenSearchDescription>';
 
-		$xml= new SimpleXMLElement( $xml );
-		$xml_sn= $xml->addChild( 'ShortName', Options::get('title') );
-		$xml_description= $xml->addChild( 'Description', Options::get('tagline') );
-		$xml_tags= '';
-		$xml_contact= '';
-		$xml_url= $xml->addChild( 'Url' );
+		$xml = new SimpleXMLElement( $xml );
+		$xml_sn = $xml->addChild( 'ShortName', Options::get('title') );
+		$xml_description = $xml->addChild( 'Description', Options::get('tagline') );
+		$xml_tags = '';
+		$xml_contact = '';
+		$xml_url = $xml->addChild( 'Url' );
 		$xml_url->addAttribute( 'type', 'text/html');
 		$xml_url->addAttribute( 'template', $template_url );
 		
-		$xml= $xml->asXML();
+		$xml = $xml->asXML();
  
 		/* Clean the output buffer, so we can output from the header/scratch. */
 		ob_clean();
@@ -132,23 +132,23 @@ class OpenSearch extends Plugin {
 	 * Creates a new Atom feed based on criteria and other parameters (pagination, limit, etc.)
 	 */
 	public function search() {
-		$criteria= $this->handler_vars['criteria'];
-		$page= isset( $this->handler_vars['page'] ) ? $this->handler_vars['page'] : 1;
-		$limit= isset( $this->handler_vars['count'] ) ? $this->handler_vars['count'] : Options::get( 'pagination' );
+		$criteria = $this->handler_vars['criteria'];
+		$page = isset( $this->handler_vars['page'] ) ? $this->handler_vars['page'] : 1;
+		$limit = isset( $this->handler_vars['count'] ) ? $this->handler_vars['count'] : Options::get( 'pagination' );
 		
 		preg_match_all( '/(?<=")(\\w[^"]*)(?=")|(\\w+)/', $criteria, $matches );
-		$words= $matches[0];
+		$words = $matches[0];
 		
-		$where= 'status = ?';
-		$params= array( Post::status( 'published' ) );
+		$where = 'status = ?';
+		$params = array( Post::status( 'published' ) );
 		foreach ( $words as $word ) {
 			$where .= " AND (title LIKE CONCAT('%',?,'%') OR content LIKE CONCAT('%',?,'%'))";
 			$params[] = $word;
 			$params[] = $word;  // Not a typo
 		}
 
-		$user_filters= array( 'where' => $where, 'params' => $params, 'page' => $page, 'criteria' => $criteria, 'limit' => $limit );
-		$atomhandler= new AtomHandler();
+		$user_filters = array( 'where' => $where, 'params' => $params, 'page' => $page, 'criteria' => $criteria, 'limit' => $limit );
+		$atomhandler = new AtomHandler();
 		$atomhandler->handler_vars['index']= $page;
 		$atomhandler->get_collection( $user_filters );
 	}
@@ -159,15 +159,15 @@ class OpenSearch extends Plugin {
 	 * @param object $theme Theme object with all its properties and methods available
 	 */
 	public function theme_header( $theme ) {
-		$search_url= Site::get_url('habari') . '/opensearch.xml';
-		$site_title= Options::get('title');
+		$search_url = Site::get_url('habari') . '/opensearch.xml';
+		$site_title = Options::get('title');
 		
 		echo '<link rel="search" type="application/opensearchdescription+xml" href="' . $search_url . '" title="' . $site_title . '">'."\r\n";
 
 		if ( Controller::get_action() == 'search' ) {
-			$totalResults= $theme->posts->count_all();
-			$startIndex= $theme->page;
-			$itemsPerPage= isset( $this->handler_vars['count'] ) ? $this->handler_vars['count'] : Options::get( 'pagination' );
+			$totalResults = $theme->posts->count_all();
+			$startIndex = $theme->page;
+			$itemsPerPage = isset( $this->handler_vars['count'] ) ? $this->handler_vars['count'] : Options::get( 'pagination' );
 			echo '<meta name="totalResults" content="' . $totalResults . '">'."\r\n";
 			echo '<meta name="startIndex" content="' . $startIndex . '">'."\r\n";
 			echo '<meta name="itemsPerPage" content="' . $itemsPerPage . '">'."\r\n";
