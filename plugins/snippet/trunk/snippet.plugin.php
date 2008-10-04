6ad7145f8bdb2c30b45d9d73616b8ca7c3410478
digitalspaghetti <digitalspaghetti@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-10-04 19:02:18 +0000

    snippet: Started working on view and url stuff
    git-svn-id: http://svn.habariproject.org/habari-extras@1102 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/snippet/trunk/snippet.plugin.php
+++ plugins/snippet/trunk/snippet.plugin.php
@@ -17,7 +17,7 @@ class Snippet extends Plugin {
     'java'=>'Java',
     'javascript'=>'Javascript',
     'php'=>'PHP',
-    'python'=>'Python'
+    'python'=>'Python',
    );
    
    return $output; 
@@ -52,41 +52,11 @@ class Snippet extends Plugin {
   }
 	
 	public function action_init() {		
-		$this->add_template('snippet.single', dirname(__FILE__) . '/snippet.single.php');
-	}
-  
-  public function action_add_template_vars($theme, $vars) {
-		if(isset($vars['slug'])) {
-			$theme->snippet = Snippet::get($vars['slug']);
-			$theme->snippet_out = Snippet::out($vars['slug']);
-		}
+		
 	}
 	
-	public function filter_rewrite_rules( $rules ) {
-		$rules[] = new RewriteRule(array(
-			'name' => 'show_snippet',
-			'parse_regex' => '%snippet/(?P<slug>.*)[\/]?$%i',
-			'build_str' =>  'snippet/{$slug}',
-			'handler' => 'UserThemeHandler',
-			'action' => 'show_snippet',
-			'priority' => 6,
-			'is_active' => 1,
-		));
-		
-		return $rules;
-	}
-	
-	public function action_handler_display_event($vars) {		
-		$post = Post::get(array('slug' => $vars['slug']));
-		
-		$this->theme->assign( 'post', $post);
-		$this->theme->display( 'snippet.single' );
-		
-		exit;
-	}
-	
-		/**
-	* Add fields to the publish page for podcasts
+  /**
+	* Add fields to the publish page for snippets
 	*
 	* @param FormUI $form The publish form
 	* @param Post $post 
@@ -95,7 +65,7 @@ class Snippet extends Plugin {
 	public function action_form_publish( $form, $post )
 	{
 		if( $form->content_type->value == Post::type( 'snippet' ) ) {
-			$postfields = $form->publish_controls->append( 'fieldset', 'language', _t( 'Language') );
+			$postfields = $form->publish_controls->append( 'fieldset', 'snippet', _t( 'Snippet Options') );
 			
 			$snippet_language = $postfields->append( 'select', 'snippet_language', 'null:null', _t('Language') );
 			$snippet_language->options = $this->langSelect();
@@ -104,73 +74,38 @@ class Snippet extends Plugin {
 		}
 	}
 	
-	public function get($slug) {
-		$post = Post::get(array('slug' => $slug));
-		
-		
-		if($post->content_type == Post::type('snippet')) {
-			$return = $post;
-			$return->snippet_language = $post->snippet_language;
-      
-			return $return;
-		} else {
-			return FALSE;
-		}
-		
-	}
-	
-	public function out($slug) {
-		if($event = $this->get($slug)) {
-			return $this->html($event);
-		} else {
-			return FALSE;
-		}
-	}
-	
-	public function html($snippet) { ?>
-	  <div class="snippet">
-	    <h2><?php echo $snippet->title?></h2>
-	    <div class="output">
-	      
-	    </div>
-	    <p>
-	      Language: <?php echo $snippet->snippet_language; ?>
-	    </p>
-	  </div>
-	<?php }
-	
-	public function filter_publish_controls ($controls, $post) {
-		$vars = Controller::get_handler_vars();
-				
-		if($vars['content_type'] == Post::type('snippet')) {
-			$output = '';
-			
-			$output .= '<div class="text container"><p class="column span-5"><label for="snippet_language">Language:</label></p><p class="column span-14 last"><input type="text" id="snippet_language" name="snippet_language" value="';
-			if(strlen($post->info->snippet_language) > 0) {
-				$output .= $post->info->snippet_language;
+	public function action_publish_post($post, $form)
+	{
+	  if( $post->content_type == Post::type( 'snippet' ) ) {
+	    if( strlen( $form->snippet->snippet_language->value ) ) {
+				$post->info->snippet_language = $form->snippet->snippet_language->value;
 			}
-			$output .= '" /></p></div>';		
-			$controls['Details'] = $output;
-		}
-		
-		return $controls;
+			else {
+				$post->info->__unset( 'snippet_language' );
+			}
+    }
+  }
+  
+  public function action_handler_display_snippets( $handler_vars ) {
+    Utils::debug($handler_vars);
+    exit();
+  }
+  
+  public function filter_rewrite_rules( $rules )
+	{
+		$rules[] = new RewriteRule( array(
+			'name' => 'display_snippet',
+			'parse_regex' => '%^snippet/(?P<snippet_language>)/(?P<slug>)/?$%i',
+			'build_str' => 'snippet/{$slug}',
+			'handler' => 'UserThemeHandler',
+			'action' => 'display_snippets',
+			'priority' => 7,
+			'is_active' => 1,
+			'description' => 'Displays snippets of language type',
+		));
+
+		return $rules;
 	}
-	
-	public function action_post_update_status( $post, $new_status ) {
-		$vars = Controller::get_handler_vars();
-		
-		if($post->content_type == Post::type('snippet')) {
-			Snippet::set($post);
-		}
-	}
-	
-	function set($post) {
-		$vars = Controller::get_handler_vars();
-		
-		if(strlen($vars['snippet_language']) > 0) {
-			$post->info->event_location = $vars['snippet_language'];
-		}
-		
-	}
+  	
 }
 ?>
