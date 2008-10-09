5e30b62bde3c25e44875a70da59453e611a8656d
bcse <bcse@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-10-09 05:13:02 +0000

    Plugin: Route 301: Build configuration panel automatically.
    git-svn-id: http://svn.habariproject.org/habari-extras@1113 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/route301/trunk/route301.plugin.php
+++ plugins/route301/trunk/route301.plugin.php
@@ -7,7 +7,7 @@
 		 * You have to use the value `Route301` as the handler for your redirecting rules.
 		 * You have to use the key of your $redirect_rules as the action for your redirecting rules.
 		 */
-		var $custom_rules = array(
+		public $custom_rules = array(
 			'display_entry_by_date_and_slug' => array( // Wordpress date and slug based permalink: 2007/09/17/<slug>
 				'name' => 'display_entry_by_date_and_slug',
 				'parse_regex' => '%^(?P<year>[1,2]\d{3})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[^/]+)(?:/page/(?P<page>\d+)/?)?$%i',
@@ -60,12 +60,12 @@
 				'description' => 'Wordpress comments feed'
 			)
 		);
-	
+
 		/* Information function called by the plugin manager. */
 		public function info() {
 			return array(
 				'name' => 'Route 301',
-				'version' => '0.6',
+				'version' => '0.6.1',
 				'url' => 'http://habariproject.org/',
 				'author' =>	'Habari Community',
 				'authorurl' => 'http://habariproject.org/',
@@ -74,13 +74,13 @@
 				'copyright' => '2007'
 				);
 		}
-	
+
 		/* Filter function called by the plugin hook `rewrite_rules`
 		 * Add a new rewrite rule to the database's rules.
 		 */
 		public function filter_rewrite_rules( $db_rules )
 		{
-			
+
 			$defaults = array(
 				'handler' => 'Route301',
 				'priority' => 1,
@@ -88,23 +88,19 @@
 				'rule_class' => RewriteRule::RULE_CUSTOM,
 				'description' => 'Custom Route 301 rule.'
 				);
-	
+
 			foreach ( $this->custom_rules as $rule_name => $paramarray ) {
-				
 				$option_name = 'route301__' . $rule_name;
-				
+
 				// a turned off option is an empty string. 1 == enabled, null == no actual options value (for backwards compatibility)
 				if ( Options::get( $option_name ) !== '' ) {
-
 					$paramarray = array_merge( $defaults, $paramarray );
 					$db_rules[] = new RewriteRule( $paramarray );
-					
 				}
-				
 			}
 			return $db_rules;
 		}
-	
+
 		/* Act function called by Controller, required for an handler.
 		 * Redirects the request to a user-defined rule with a header 301 (moved permanently).
 		 */
@@ -122,89 +118,75 @@
 			} else {
 				$url = URL::get( $action, $this->handler_vars );
 			}
-	
+
 			header( 'Location: ' . $url, true, 301 );
 		}
-	
-		public function action_update_check ( ) {
-			
+
+		public function action_update_check()
+		{
 			Update::add( 'Route301', 'b4bf3419-518f-27c4-c18d-ab9786936f21', $this->info->version );
-			
 		}
-		
-		public function filter_plugin_config ( $actions, $plugin_id ) {
-			
+
+		public function filter_plugin_config( $actions, $plugin_id )
+		{
 			if ( $plugin_id == $this->plugin_id() ) {
 				$actions[] = _t( 'Configure' );
 			}
-			
+
 			return $actions;
-			
 		}
-		
-		public function action_plugin_ui ( $plugin_id, $action ) {
-			
+
+		public function action_plugin_ui( $plugin_id, $action )
+		{
 			if ( $plugin_id == $this->plugin_id() ) {
-				
 				if ( $action == _t( 'Configure' ) ) {
-					
 					$class_name = strtolower( get_class( $this ) );
-					
+
 					$form = new FormUI( $class_name );
-					
+
 					$form->append( 'fieldset', 'post_options', 'Post Options' );
-					$form->post_options->append( 'checkbox', 'display_entry_by_date_and_slug', 'route301__display_entry_by_date_and_slug', _t( 'Route <code>/&lt;year&gt;/&lt;month&gt;/&lt;day&gt;/&lt;slug&gt;</code> URLs' ) );
-					$form->post_options->append( 'checkbox', 'display_entry_by_month_and_slug', 'route301__display_entry_by_month_and_slug', _t( 'Route <code>/&lt;year&gt;/&lt;month&gt;/&lt;slug&gt;</code> URLs' ) );
-					$form->post_options->append( 'checkbox', 'display_entry_by_id', 'route301__display_entry_by_id', _t( 'Route <code>?p=&lt;id&gt;</code> URLs' ) );
-					$form->post_options->append( 'checkbox', 'display_entry_by_tag_and_slug', 'route301__display_entry_by_tag_and_slug', _t( 'Route <code>/tag/&lt;tag&gt;</code> URLs' ) );
-					
 					$form->append( 'fieldset', 'feed_options', 'Feed Options' );
-					$form->feed_options->append( 'checkbox', 'wordpress_feed', 'route301__wordpress_feed', _t( 'Route <code>/feed</code> URL' ) );
-					$form->feed_options->append( 'checkbox', 'wordpress_tag_feed', 'route301__wordpress_tag_feed', _t( 'Route <code>/tag/&lt;tag&gt;/feed</code> URLs' ) );
-					$form->feed_options->append( 'checkbox', 'wordpress_comments_feed', 'route301__wordpress_comments_feed', _t( 'Route <code>/comments/feed</code> URL' ) );
-					
+
+					foreach ( $this->custom_rules as $rule ) {
+						$rule['build_str'] = preg_replace( '/\([^)]+\)/', '', $rule['build_str'] );
+						$rule['build_str'] = str_replace( array( '{$', '}' ), array( '<', '>' ), $rule['build_str'] );
+						$rule['build_str'] = trim( $rule['build_str'], '/' );
+						$rule['build_str'] = htmlspecialchars( $rule['build_str'] );
+						if ( $rule['action'] === 'display_entry' ) {
+							$form->post_options->append( 'checkbox', $rule['name'], 'route301__' . $rule['name'], sprintf( _t( 'Route <code>%s</code> URLs' ), $rule['build_str'] ) );
+						}
+						else {
+							$form->feed_options->append( 'checkbox', $rule['name'], 'route301__' . $rule['name'], sprintf( _t( 'Route <code>%s</code> URLs' ), $rule['build_str'] ) );
+						}
+					}
 					$form->append( 'submit', 'save', _t( 'Save' ) );
-					
+
 					$form->on_success( array( $this, 'updated_config' ) );
 					$form->out();
-					
 				}
-				
 			}
-			
 		}
-		
-		public function updated_config ( $form ) {
-			
+
+		public function updated_config( $form )
+		{
 			$form->save();
-			
 		}
-		
-		public function action_plugin_activation ( $file ) {
-			
+
+		public function action_plugin_activation( $file )
+		{
 			if ( Plugins::id_from_file( $file ) == Plugins::id_from_file( __FILE__ ) ) {
-				
 				// default options: name => default value
-				$options[ 'route301__display_entry_by_date_and_slug' ] = 1;
-				$options[ 'route301__display_entry_by_month_and_slug' ] = 1;
-				$options[ 'route301__display_entry_by_id' ] = 1;
-				$options[ 'route301__display_entry_by_tag_and_slug' ] = 1;
-				$options[ 'route301__wordpress_feed' ] = 1;
-				$options[ 'route301__wordpress_tag_feed' ] = 1;
-				$options[ 'route301__wordpress_comments_feed' ] = 1;
-				
+				foreach ( $this->custom_rules as $rule ) {
+					$options['route301__' . $rule['name']] = 1;
+				}
+
 				foreach ( $options as $option => $value ) {
-					
 					if ( Options::get( $option ) == null ) {
 						Options::set( $option, $value );
 					}
-					
 				}
-				
 			}
-			
 		}
-	
 	}
-	
+
 ?>
