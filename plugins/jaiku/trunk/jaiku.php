b3b73ef4568e0fa0f217ea83c4f953ab2cf23449
bcse <bcse@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-10-12 08:56:46 +0000

    Plugin: Jaiku: Fix a stupid bug (redundant colon)
    git-svn-id: http://svn.habariproject.org/habari-extras@1135 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/jaiku/trunk/jaiku.php
+++ plugins/jaiku/trunk/jaiku.php
@@ -8,7 +8,7 @@
 		} else
 		if (is_array($presences)) { // Multiple presences
 			foreach ($presences as $presence) {
-				printf('<li class="jaiku-message">%1$s <a href="%2$s"><abbr title="%3$s">%4$s</abbr></a> (<a href="%2$s#comments">%5$s</a>)</li>'), $presence->title, $presence->url, $presence->created_at, $presence->created_at_relative, $presence->comments);
+				printf('<li class="jaiku-message">%1$s <a href="%2$s"><abbr title="%3$s">%4$s</abbr></a> (<a href="%2$s#comments">%5$s</a>)</li>', $presence->title, $presence->url, $presence->created_at, $presence->created_at_relative, $presence->comments);
 			}
 			printf('<li class="jaiku-more"><a href="%s">' . _t('Read more…', $this->class_name) . '</a></li>', $presences[0]->user->url);
 		} else // Exceptions
