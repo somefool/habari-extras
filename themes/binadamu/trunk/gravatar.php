2864dfc39403239f14876c84b4b1b43c7caae394
bcse <bcse@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-10-12 12:04:52 +0000

    Theme: Binadamu: Please use Gravatar 080915-themefunction branch.
    git-svn-id: http://svn.habariproject.org/habari-extras@1137 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ themes/binadamu/trunk/gravatar.php
@@ -0,0 +1,3 @@
+<li class="comment-author-gravator">
+	<img src="<?php echo $gravatar; ?>" class="photo" width="<?php echo $gravatar_size; ?>" height="<?php echo $gravatar_size; ?>" alt="<?php echo $commentor; ?>" />
+</li>
