bd5fd936e9c38aed17e6dc1da8a9350aaf629010
bcse <bcse@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-10-16 15:01:22 +0000

    Theme: Binadamu: It will be better to make <!DOCTYPE> at first line.
    git-svn-id: http://svn.habariproject.org/habari-extras@1163 f4a90942-1786-4aa1-b092-57c31935ddc3

--- themes/binadamu/trunk/header.home.php
+++ themes/binadamu/trunk/header.home.php
@@ -1,4 +1,3 @@
-<!-- header -->
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
 <head profile="http://gmpg.org/xfn/11">
@@ -12,6 +11,7 @@
 
 </head>
 <body<?php $theme->body_class(); ?>>
+<!-- header -->
 <div id="container">
 	<div id="header">
 		<h1 id="site-name"><a href="<?php Site::out_url('habari'); ?>"><?php Options::out('title'); ?></a></h1>
@@ -23,5 +23,5 @@
 <?php } ?>
 		</ul>
 	</div>
-	<hr />
 <!-- /header -->
+	<hr />
