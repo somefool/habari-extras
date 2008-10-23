3eb1cdb4607ce4b5f6b5967ccbef911c86cb2b38
freakerz <freakerz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-10-23 18:58:33 +0000

    Admin Detour: Choose what page appears when you log in. Uses plugin config FormUI because User profile page sucks for now.
    git-svn-id: http://svn.habariproject.org/habari-extras@1181 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/admindetour/admindetour.plugin.php
@@ -0,0 +1,126 @@
+<?php
+class admindetour extends Plugin {
+	
+	public $mainmenus = array();
+	
+	public function info() {
+		return array(
+			'name' => 'Admin Detour',
+			'version' => '0.1',
+			'url' => 'http://habariproject.org/',
+			'author' =>	'Habari Community',
+			'authorurl' => 'http://habariproject.org/',
+			'license' => 'Apache License 2.0',
+			'description' => 'Decide what is your front page when logging in.',
+			);
+	}
+	
+	public function filter_rewrite_rules( $db_rules )
+	{	
+		$db_rules[] = new RewriteRule ( array(
+			'name' => 'admin_dashboard',
+			'parse_regex' => '%^admin/dashboard/?$%i',
+			'build_str' => 'admin/dashboard',
+			'handler' => 'AdminHandler',
+			'action' => 'admin',
+			'priority' => 7,
+			'is_active' => 1,
+			'rule_class' => RewriteRule::RULE_CUSTOM,
+			'description' => 'Add "admin/dashboard" so Admin Detour can hijack "admin".'
+			) );
+		return $db_rules;
+	}
+	
+	public function action_before_act_admin( $that ) {
+		if (!isset($that->handler_vars['page'])) {
+			$that->handler_vars = array_merge($that->handler_vars, User::identify()->info->admindetour_real['args']);
+		}
+	}
+	
+	public function filter_adminhandler_post_loadplugins_main_menu( $mainmenus ) {
+		$mainmenus['dashboard'] = array( 'url' => URL::get( 'admin', 'page=dashboard' ), 'title' => _t( 'View your user dashboard' ), 'text' => _t( 'Dashboard' ), 'hotkey' => 'D', 'selected' => false );
+		
+		$this->mainmenus = $mainmenus;
+		
+		return $mainmenus;
+	}
+	
+	public function filter_plugin_config( $actions, $plugin_id )
+	{
+		if ( $plugin_id == $this->plugin_id() ) {
+			$actions[] = _t('Configure');
+		}
+
+		return $actions;
+	}
+	
+	public function action_plugin_ui( $plugin_id, $action )
+	{
+		if ( $plugin_id == $this->plugin_id() ) {
+			switch ( $action ) {
+				case _t('Configure'):
+					$mainmenus = array();
+					foreach ($this->mainmenus as $mainmenu) {
+						$mainmenus[$mainmenu['url']] = $mainmenu['text'];
+					}
+					
+					$ui = new FormUI( strtolower( get_class( $this ) ) );
+					$ui->append( 'select', 'mainmenus', 'user:admindetour_fake', _t('Select the wanted admin frontpage:') );
+					$ui->mainmenus->options = $mainmenus;
+
+					$ui->append( 'submit', 'save', _t('Save') );
+					$ui->on_success( array( $this, 'save_mainmenu' ) );
+					$ui->out();
+					break;
+			}
+		}
+	}
+	
+	public function save_mainmenu($form)
+	{
+		$base_url = Site::get_url('habari', true);
+		$start_url = $form->mainmenus->value;
+		
+		/* Strip out the base URL from the requested URL */
+		/* but only if the base URL isn't / */
+		if ( '/' != $base_url) {
+			$start_url = str_replace($base_url, '', $start_url);
+		}
+
+		/* Trim off any leading or trailing slashes */
+		$start_url = trim($start_url, '/');
+
+		/* Remove the querystring from the URL */
+		if ( strpos($start_url, '?') !== FALSE ) {
+			list($start_url, $query_string)= explode('?', $start_url);
+		}
+		
+		/* Allow plugins to rewrite the stub before it's passed through the rules */
+		$start_url = Plugins::filter('rewrite_request', $start_url);
+
+		$stub = $start_url;
+
+		/* Grab the URL filtering rules from DB */
+		$matched_rule = URL::parse($stub);
+
+		if ($matched_rule === FALSE) {
+			print 'error, cant find rule';
+			// error!!!!
+		}
+		
+		/* Return $_GET values to their proper place */
+		$args = array();
+		if( !empty($query_string) ) {
+			parse_str($query_string, $args);
+		}
+
+		$rule = $matched_rule->name;
+		$args = array_merge($matched_rule->named_arg_values, $args);
+		User::identify()->info->admindetour_real = array( 'rule' => $rule, 'args' => $args );
+		$_POST[$form->mainmenus->field] = URL::get( $rule, $args );
+
+		$form->save();
+	}
+
+}
+?>
