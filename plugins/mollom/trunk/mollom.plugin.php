d0dcd52962c094157ddf37a26f5141582dd1ccb2
MattRead <MattRead@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-11-19 04:19:26 +0000

    plugin:mollom oops, this ain't defensio heheheheh
    git-svn-id: http://svn.habariproject.org/habari-extras@1309 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/mollom/trunk/mollom.plugin.php
+++ plugins/mollom/trunk/mollom.plugin.php
@@ -371,7 +371,7 @@ class MollomPlugin extends Plugin
 	}
 	
 	public function action_comment_info( $comment ) {
-		if(isset($comment->info->defensio_spaminess)) {
+		if(isset($comment->info->mollom_quality)) {
 			echo '<p class="keyval spam"><span class="label">' . _t('Mollom Quality:', 'quality') . '</span>' . '<strong>' . $comment->info->mollom_quality . '</strong></p>';
 		}
 	}
