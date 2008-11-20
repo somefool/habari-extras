bc1fb82d50e41fe539e305d5f1cac54d2dd7a4e6
freakerz <freakerz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-11-20 08:32:09 +0000

    New plugin: AutoSave, saves your post via AJAX every minute if title or content changed.
    git-svn-id: http://svn.habariproject.org/habari-extras@1316 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/autosave/trunk/autosave.plugin.js
@@ -0,0 +1,132 @@
+var autoSave = {
+	lastSaved: null,
+	lastDiffed: null,
+	saveTimer: null,
+	diffTimer: null,
+	saveClicked: false,
+	
+	// Initiates timers, prepares placeholder and hijacks buttons/form
+	init: function() {
+		// Autosave placeholder to display errors or last save timestamp
+		$('#save').append('<span id="autosave" style="float:right;"><span id="autosave_type"></span> at <span id="autosave_last"></span> (<span id="autosave_diff"></span> ago)</span>');
+		$('#autosave').hide();
+		
+		/* Let's hook everything we need to provide best autosaving */
+		$('#save > input').val('Save now').click(function(){
+			// Save now" button clicked, prevent form from submitting
+			autoSave.saveClicked = true;
+			// Call our own handler via ajax
+			autoSave.save();
+		});
+		$('#publish, #delete').click(function(){
+			// Other button clicked, let form be submitted
+			autoSave.saveClicked = false;
+		});
+		
+		// Reset the "Save now" button if changes happen on any field and unset timers
+		$('#create-content input[type="text"], #create-content input[type="password"], #create-content textarea').keypress(autoSave.formChangeAndUnset);
+		$('#create-content input[type="checkbox"], #create-content input[type="radio"], #create-content select').change(autoSave.formChangeAndUnset);
+		// Save after interval if changes happen upon changing title or content
+		$('#title, #content').unbind('keypress', autoSave.formChangeAndUnset).keypress(autoSave.formChangeAndSet);
+		
+		// Intercept form submission
+		$('#create-content').submit(function() {
+			if (autoSave.saveClicked) {
+				// Prevent "save" button to submit form, we'll handle the saving "ajax style!"
+				return false;
+			}
+		});
+	},
+	
+	// Displays difference in time (X minutes ago)
+	diff: function() {
+		dateSaved = autoSave.lastSaved != null ? autoSave.lastSaved : new Date();
+		dateDiffed = autoSave.lastDiffed != null ? autoSave.lastDiffed : new Date();
+		dateDiff = Math.round(( dateDiffed - dateSaved ) / 60000);
+		diffMinutes = dateDiff == 1 ? ' minute' : ' minutes';
+		$('#autosave_diff').html(dateDiff + diffMinutes);
+		autoSave.lastDiffed = new Date();
+	},
+	
+	// Saves the current form as a revision
+	save: function() {
+		// Time to send form home
+		var publishForm = $('#create-content').serializeArray();
+		$.post('autosave', publishForm, autoSave.handleResponse, 'json');
+		autoSave.saveTimer = null;
+	},
+	
+	handleResponse: function(data) {
+		$('#autosave').show();
+		autoSave.lastSaved = new Date();
+		var lastHours = (autoSave.lastSaved.getHours() < 10 ? '0' : '') + autoSave.lastSaved.getHours();
+		var lastMinutes = (autoSave.lastSaved.getMinutes() < 10 ? '0' : '') + autoSave.lastSaved.getMinutes();
+		if (autoSave.isSet(data.post_id) && autoSave.isSet(data.post_slug)) {
+			// Prevent a new ID to be generated if title is changed
+			$('input[name="id"]').val(data.post_id);
+			$('#newslug').val(data.post_slug);
+			// Advise user we just saved the form
+			$('#save > input').attr('disabled', 'disabled').val('Saved').css('color', '#555');
+			if (autoSave.lastFailed) {
+				autoSave.lastFailed = false;
+				$('#autosave').html('<span id="autosave_type"></span> at <span id="autosave_last"></span> (<span id="autosave_diff"></span> ago)');
+			}
+			$('#autosave_last').html(lastHours + ':' + lastMinutes);
+			$('#autosave_type').html(autoSave.saveClicked ? 'Saved' : 'Autosaved');
+			autoSave.diff();
+			autoSave.diffTimer = setInterval("autoSave.diff()", 60000); // 1 minute
+		}
+		else if (autoSave.isSet(data.error)) {
+			autoSave.lastFailed = true;
+			$('#autosave').html(data.error + ' (' + lastHours + ':' + lastMinutes + ')');
+		}
+		else {
+			$('#autosave').html('Unexpected behavior, post may not have been saved. (' + lastHours + ':' + lastMinutes + ')').css('color', '#A30000');
+		}
+	},
+	
+	formChange: function() {
+		// If "save now" button isn't already enabled,
+		// Reset "save" button if changes happen after autosaving
+		if ($('#save > input').attr('disabled')) {
+			autoSave.saveCicked = false;
+			$('#save > input').attr('disabled', null).val('Save now').css('color', '#333');
+		}
+	},
+	
+	formChangeAndUnset: function() {
+		autoSave.formChange();
+		// Prevent unwanted savings (incomplete tags or new slugs, for example)
+		autoSave.unload();
+	},
+	
+	formChangeAndSet: function() {
+		autoSave.formChange();
+		// Set a timer to save form after 10 seconds after starting to type
+		if (autoSave.saveTimer == null) {
+			autoSave.saveTimer = setTimeout('autoSave.save()', 10000);
+		}
+	},
+	
+	isSet: function (varname) {
+		try {
+			var type = typeof(varname);
+		} catch(e) { return false; }
+
+		if (type !== 'undefined') { return true; }
+		else { return false; }
+	},
+	
+	unload: function() {
+		clearTimeout(autoSave.saveTimer);
+		clearInterval(autoSave.diffTimer);
+	}
+}
+
+$(document).ready(function() {
+	autoSave.init();
+});
+
+$(document).unload(function() {
+	autoSave.unload();
+});
---
+++ plugins/autosave/trunk/autosave.plugin.php
@@ -0,0 +1,109 @@
+<?php
+class AutoSave extends Plugin {
+	
+	public function info() {
+		return array(
+			'name' => 'AutoSave',
+			'version' => '0.1',
+			'author' =>	'Andrew da Silva',
+			'authorurl' => 'http://andrewdasilva.com/',
+			'license' => 'Apache License 2.0',
+			'description' => 'Saves post every minute if changes were made to the title or content. <strong>Do not use with the Revision plugin.</strong>',
+			'copyright' => '2008'
+		);
+	}
+	
+	public function action_admin_theme_get_publish() {
+		Stack::add('admin_header_javascript', array($this->get_url() . '/autosave.plugin.js'), 'autosave-plugin-js');
+	}
+	
+	/**
+	 * Altered copy of AdminHandler::post_publish():
+	 * - Throws exceptions rather than Session notices so we can return errors to AJAX calls;
+	 * - Does not redirect but echo a JSON object with the post's ID and slug
+	 *
+	 * @see AdminHandler::post_publish()
+	 *
+	 * @param AdminHandler $that The AdminHandler instance
+	 * @param Theme $theme The Theme instance
+	 */
+	public function action_admin_theme_post_autosave($that, $theme) {
+		$response = array();
+
+		try {
+			extract( $that->handler_vars );
+			
+			$form = $that->form_publish( new Post(), false );
+			
+			// check to see if we are updating or creating a new post
+			if ( $form->post_id->value != 0 ) {
+				$post = Post::get( array( 'id' => $form->post_id->value, 'status' => Post::status( 'any' ) ) );
+				$post->title = $form->title->value;
+				if ( $form->newslug->value == '' ) {
+					throw new Exception(_t('A post slug cannot be empty. Keeping old slug.'));
+				}
+				elseif ( $form->newslug->value != $form->slug->value ) {
+					$post->slug = $form->newslug->value;
+				}
+				$post->tags = $form->tags->value;
+
+				$post->content = $form->content->value;
+				$post->content_type = $form->content_type->value;
+				// if not previously published and the user wants to publish now, change the pubdate to the current date/time
+				// if the post pubdate is <= the current date/time.
+				if ( ( $post->status != Post::status( 'published' ) )
+					&& ( $form->status->value == Post::status( 'published' ) )
+					&& ( HabariDateTime::date_create( $form->pubdate->value )->int <= HabariDateTime::date_create()->int )
+					) {
+					$post->pubdate = HabariDateTime::date_create();
+				}
+				// else let the user change the publication date.
+				//  If previously published and the new date is in the future, the post will be unpublished and scheduled. Any other status, and the post will just get the new pubdate.
+				// This will result in the post being scheduled for future publication if the date/time is in the future and the new status is published.
+				else {
+					$post->pubdate = HabariDateTime::date_create( $form->pubdate->value );
+				}
+
+				$post->status = $form->status->value;
+			}
+			else {
+				$postdata = array(
+					'slug' => $form->newslug->value,
+					'title' => $form->title->value,
+					'tags' => $form->tags->value,
+					'content' => $form->content->value,
+					'user_id' => User::identify()->id,
+					'pubdate' => HabariDateTime::date_create($form->pubdate->value),
+					'status' => $form->status->value,
+					'content_type' => $form->content_type->value,
+				);
+
+				$post = Post::create( $postdata );
+			}
+
+			if( $post->pubdate->int > HabariDateTime::date_create()->int && $post->status == Post::status( 'published' ) ) {
+				$post->status = Post::status( 'scheduled' );
+			}
+
+			$post->info->comments_disabled = !$form->comments_enabled->value;
+
+			Plugins::act('publish_post', $post, $form);
+
+			$post->update( $form->minor_edit->value );
+			$response['post_id'] = $post->id;
+			$response['post_slug'] = $post->slug;
+			ob_end_clean();
+			echo json_encode($response);
+			// Prevent rest of adminhandler to run, we only wanted to save!
+			exit;
+		} catch(Exception $e) {
+			$response['error'] = $e->getMessage();
+			ob_end_clean();
+			echo json_encode($response);
+			// Prevent rest of adminhandler to run, we only wanted to save!
+			exit;
+		}
+	}
+	
+}
+?>
