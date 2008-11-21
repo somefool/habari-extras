131c4bec777572c7f48edc3303681cbf58a84aba
Massimiliano <Massimiliano@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-11-21 14:49:17 +0000

    Fixing ticket #101. It's my first ever commit, will I b0rk everything?
    git-svn-id: http://svn.habariproject.org/habari-extras@1326 f4a90942-1786-4aa1-b092-57c31935ddc3

--- themes/aligned/trunk/entry.multiple.php
+++ themes/aligned/trunk/entry.multiple.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 <?php foreach ( $posts as $post ) { ?>
 	<div class="post<?php if($post->status == 1) { echo ' draft'; } ?>" id="post-<?php echo $post->id; ?>">
-		<p class="post_date"><span><?php echo $post->pubdate_out; ?> | <strong><?php echo $post->author->info->displayname; ?></strong></span></p>
+		<p class="post_date"><span><?php echo $post->pubdate_out; ?> | <strong><?php echo $post->author->displayname; ?></strong></span></p>
 		<h2 class="post_title"><a href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>"><?php echo $post->title_out; ?></a></h2>
 		<div class="post_content">
 			<?php echo $post->content_out; ?>
