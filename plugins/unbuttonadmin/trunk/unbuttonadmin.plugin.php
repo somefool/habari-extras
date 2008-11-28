39e84fb6f4ab2f632ab1034c4adbc37ae683651b
MattRead <MattRead@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-11-28 17:27:10 +0000

    plugin:unbuttonadmin initial import
    git-svn-id: http://svn.habariproject.org/habari-extras@1373 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/unbuttonadmin/trunk/unbuttonadmin.plugin.php
@@ -0,0 +1,45 @@
+<?php
+
+class unButtonAdmin extends Plugin
+{
+	public function info()
+	{
+		return array (
+			'name' => 'Un-button Admin',
+			'version' => '0.1',
+			'author' => 'Habari Community',
+			'license' => 'Apache License 2.0',
+			'description' => 'Reverts the ugly admin buttons to the default OS widgets',
+		);
+	}
+	
+	public function action_admin_header( $theme )
+	{
+		Stack::add(
+			'admin_stylesheet',
+			array(
+				'
+				#page input[type=button], #page input[type=submit], #page button { 
+					padding: inherit; 
+					cursor: inherit; 
+					-webkit-border-radius: 0;     /* Webkit only supports up to 9px at present */ 
+					-moz-border-radius: 0; 
+					font-size: inherit; 
+					background: inherit; 
+					border: inherit; 
+				} 
+				 
+				#page input[type=button]:hover, #page input[type=submit]:hover, #page button:hover { 
+					background: inherit; 
+					border: inherit; 
+				}
+				',
+				'screen'
+			),
+			'blogroll',
+			'admin'
+		);
+	}
+}
+
+?>
