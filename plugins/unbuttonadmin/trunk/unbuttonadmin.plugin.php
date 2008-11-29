efb4689ee4898172d7e2f93a6810f3f54f0cb779
MattRead <MattRead@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-11-29 06:06:00 +0000

    plugin:unbuttonadmin OMG! MWAHAHAH! the hack of the day right here.
    git-svn-id: http://svn.habariproject.org/habari-extras@1380 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/unbuttonadmin/trunk/unbuttonadmin.plugin.php
+++ plugins/unbuttonadmin/trunk/unbuttonadmin.plugin.php
@@ -15,29 +15,30 @@ class unButtonAdmin extends Plugin
 	
 	public function action_admin_header( $theme )
 	{
+		// This is such a hack it's not even funny
+		// But I am laughing inside. Laughing in a bad way.
+		Stack::remove('admin_stylesheet', 'admin');
+		$css = file_get_contents(Site::get_dir('admin_theme') . '/css/admin.css');
+		$css = preg_replace(
+			'@#page input\[type=button\], #page input\[type=submit\], #page button {([^}]+)}@',
+			'',
+			$css,
+			1
+		);
+		$css = preg_replace(
+			'@#page input\[type=button\]:hover, #page input\[type=submit\]:hover, #page button:hover {([^}]+)}@',
+			'',
+			$css,
+			1
+		);
 		Stack::add(
 			'admin_stylesheet',
 			array(
-				'
-				#page input[type=button], #page input[type=submit], #page button { 
-					padding: inherit; 
-					cursor: inherit; 
-					-webkit-border-radius: 0;     /* Webkit only supports up to 9px at present */ 
-					-moz-border-radius: 0; 
-					font-size: inherit; 
-					background: inherit; 
-					border: inherit; 
-				} 
-				 
-				#page input[type=button]:hover, #page input[type=submit]:hover, #page button:hover { 
-					background: inherit; 
-					border: inherit; 
-				}
-				',
+				$css,
 				'screen'
 			),
-			'unbuttonadmin',
-			'admin'
+			'admin',
+			'jquery'
 		);
 	}
 }
