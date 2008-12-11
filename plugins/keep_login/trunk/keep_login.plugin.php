37b56ed6742e682e5ce6e85a58d429c5e3308b11
ringmaster <ringmaster@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-12-11 02:16:35 +0000

    Use the stack for proper javascript output.
    git-svn-id: http://svn.habariproject.org/habari-extras@1439 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/keep_login/trunk/keep_login.plugin.php
+++ plugins/keep_login/trunk/keep_login.plugin.php
@@ -19,21 +19,20 @@ class KeepLogin extends Plugin
 			'url' => 'http://habariproject.org/',
 			'author' => 'Habari Community',
 			'authorurl' => 'http://habariproject.org/',
-			'version' => '1.0',
+			'version' => '1.1',
 			'description' => 'Uses ajax to keep a session logged in in the admin.',
 			'license' => 'Apache License 2.0',
 		);
 	}
 
 	/**
-	 * Produce output in the admin header
+	 * Add output in the admin header
 	 * Adds the necessary javascript to make periodic ajax calls to the admin.
-	 */	 
-	public function action_admin_header_after( $admintheme )
+	 */
+	public function action_admin_header()
 	{
 		$ajaxurl = URL::get('auth_ajax', array('context'=>'keep_session'));
-		echo <<< HEADER_JS
-<script type="text/javascript">
+		$script = <<< HEADER_JS
 $(document).ready(function(){
 	window.setInterval(
 		function(){
@@ -42,8 +41,8 @@ $(document).ready(function(){
 		1000 * 60 * 5 // 5 minutes
 	);
 });
-</script>
 HEADER_JS;
+		Stack::add( 'admin_header_javascript',  $script, 'keep_login', array('jquery') );
 	}
 
 	public function action_auth_ajax_keep_session()
