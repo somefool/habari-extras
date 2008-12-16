8db8c210eb9d23d5f0d718941db3670a32722f92
somefool <somefool@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-12-16 18:34:27 +0000

    Removed duplicate 'Posts' and 'Pages' links and added 'Plugins' link
    git-svn-id: http://svn.habariproject.org/habari-extras@1467 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/hadminstrip/trunk/hadminstrip.plugin.php
+++ plugins/hadminstrip/trunk/hadminstrip.plugin.php
@@ -89,8 +89,8 @@ class HadminStrip extends Plugin
 				<a href="<?php echo ( URL::get( 'admin', 'page=dashboard' )); ?>" title="Visit the Dashboard.."><span id="admin">DASHBOARD</span></a>
 				 &middot; <a href="<?php echo (URL::get( 'user', 'page=logout')); ?>" title="Log out..">Logout</a> 
 				 &middot; <a href="<?php echo (URL::get( 'admin', 'page=publish')); ?>" title="Write an entry..">Write</a> 
-				 &middot; <a href="<?php echo (URL::get( 'admin', 'page=posts&type=' . Post::type('entry')));?>" title="<?php echo($postnumber) ?> posts"><?php echo($postnumber) ?> posts</a> 
-				&middot; <a href="<?php echo (URL::get( 'admin', 'page=posts&type=' . Post::type('page')));?>" title="<?php echo($pagecount) ?> pages"> <?php echo($pagecount) ?> pages</a> 
+				 &middot; <a href="<?php echo (URL::get( 'admin', 'page=plugins'));?>" title="Plugins">Plugins</a> 
+			
 					 &middot; <a href="<?php echo (URL::get( 'admin', 'page=options' )); ?>" title="Update settings..">Options</a> 
 					
 					<?php	if ( $unapprovedcomments!=0) { ?> &middot; <a id="admincomments" href="<?php echo (URL::get( 'admin', 'page=comments' )); ?>" title="Comments"><?php echo ($unapprovedcomments) ?> comments</a><?php } ?>
