86e34f47f533be67f050309ba94b61aa688400da
somefool <somefool@f4a90942-1786-4aa1-b092-57c31935ddc3>
2008-12-31 02:20:53 +0000

    git-svn-id: http://svn.habariproject.org/habari-extras@1491 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/hadminstrip/trunk/hadminstrip.plugin.php
+++ plugins/hadminstrip/trunk/hadminstrip.plugin.php
@@ -95,7 +95,7 @@ class HadminStrip extends Plugin
 					
 				<?php	if ( $unapprovedcomments!=0) { ?> &middot; <a id="modcomments" href="<?php echo (URL::get( 'admin', 'page=comments' )); ?>" title="Unapproved Comments"><?php echo ($unapprovedcomments) ?> moderate</a><?php } ?>
 				
-				<?php	if ( $spamnumber!=0) { ?> <a id="admincomments" href="<?php echo (URL::get( 'admin', 'page=comments' )); ?>" title="Spam Comments"><?php echo ($spamnumber) ?> spam</a><?php } ?>
+				<?php	if ( $spamnumber!=0) { ?> &middot; <a id="admincomments" href="<?php echo (URL::get( 'admin', 'page=comments' )); ?>" title="Spam Comments"><?php echo ($spamnumber) ?> spam</a><?php } ?>
 				 
 				&middot; There are <a id="striplink" href="<?php echo (URL::get( 'admin', 'page=posts&type=' . Post::type('entry')));?>" title="<?php echo($postnumber) ?> posts"><?php echo($postnumber) ?> posts</a>, <a id="striplink" href="<?php echo (URL::get( 'admin', 'page=posts&type=' . Post::type('page')));?>" title="<?php echo($pagecount) ?> pages"><?php echo($pagecount) ?> pages</a> and <a id="striplink" href="<?php echo (URL::get( 'admin', 'page=comments' )); ?>" title="Comments"><?php echo($commentnumber) ?> comments</a> within <a id="striplink" href="<?php echo (URL::get( 'admin', 'page=tags'));?>" Tags="Tags"><?php echo($tagcount) ?> tags</a></a>
 								
