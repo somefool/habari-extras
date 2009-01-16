f2de70a1152dea2967c51ce019e9b82703957372
Massimiliano <Massimiliano@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-01-16 11:09:47 +0000

    Fixing blogroll in Aligned, Georgia, Habarism.
    git-svn-id: http://svn.habariproject.org/habari-extras@1505 f4a90942-1786-4aa1-b092-57c31935ddc3

--- themes/habarism/trunk/blogroll.php
+++ themes/habarism/trunk/blogroll.php
@@ -2,7 +2,7 @@
 	<h3><?php echo $blogroll_title; ?></h3>
 	<ul>
 	<?php if ( ! empty( $blogs ) ) { foreach( $blogs as $blog ) { ?>
-		<li><a href="<?php echo $blog->url; ?>"><?php echo $blog->name; ?></a></li>
+		<li><a href="<?php echo $blog->info->url; ?>"><?php echo $blog->title; ?></a></li>
 	<?php } } ?>
 	</ul>
 </div>
