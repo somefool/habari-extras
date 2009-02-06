6b3aa89488405877461a33a455b94eb8c3fbe13c
chrismeller <chrismeller@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-02-06 02:25:32 +0000

    Making the Simpler theme valid HTML by removing some XHTML line breaks.
    git-svn-id: http://svn.habariproject.org/habari-extras@1573 f4a90942-1786-4aa1-b092-57c31935ddc3

--- themes/simpler/trunk/links.php
+++ themes/simpler/trunk/links.php
@@ -8,7 +8,7 @@ Template Name: Links
 			<h2 class="title">Links</h2>
 			<div id="entries">
 				<ul>
-					<?php get_links(-1, '<li>', '</li>', '<br />', FALSE, 'id', TRUE, TRUE, -1, TRUE); ?>
+					<?php get_links(-1, '<li>', '</li>', '<br>', FALSE, 'id', TRUE, TRUE, -1, TRUE); ?>
 				</ul>
 			</div>
 		</div>
