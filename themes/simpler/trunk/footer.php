58a184e10a827fa5fb5c53ac760280fa43261c3b
chrismeller <chrismeller@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-02-12 03:11:19 +0000

    theme:simpler Adds an update check and allows you to configure the copyright notice displayed in the footer.
    git-svn-id: http://svn.habariproject.org/habari-extras@1597 f4a90942-1786-4aa1-b092-57c31935ddc3

--- themes/simpler/trunk/footer.php
+++ themes/simpler/trunk/footer.php
@@ -1,30 +1,30 @@
 		<div id="bottom">
-			
+
 			<div class="column">
 				<h3>Recent Comments</h3>
 				<?php $theme->show_recentcomments(); ?>
 			</div>
-			
+
 			<div class="column">
 				<h3>Monthly Archives</h3>
 				<?php $theme->monthly_archives(5, 'N'); ?>
 				<a href="<?php Site::out_url( 'habari' ); ?>/archives">More...</a>
 			</div>
-			
+
 			<div class="column">
 				<h3>Tags</h3>
 				<?php $theme->tag_cloud(5); ?>
 				<a href="<?php Site::out_url( 'habari' ); ?>/tag">More...</a>
 			</div>
-			
+
 			<br>
-			
+
 			<div id="footer">
-				<p>&copy; Copyright <?php echo date('Y');?>. All Rights Reserved. Subscribe to <a href="<?php URL::out( 'atom_feed', array( 'index' => '1' ) ); ?>">Entries</a> or <a href="<?php URL::out( 'atom_feed_comments', array( 'index' => '1' ) ); ?>">Comments</a></p>
+				<p><?php echo $copyright; ?> Subscribe to <a href="<?php URL::out( 'atom_feed', array( 'index' => '1' ) ); ?>">Entries</a> or <a href="<?php URL::out( 'atom_feed_comments', array( 'index' => '1' ) ); ?>">Comments</a></p>
 			</div>
 		</div>
-		
+
 		<?php $theme->footer(); ?>
-		
+
 	</body>
 </html>
--- themes/simpler/trunk/theme.php
+++ themes/simpler/trunk/theme.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
 
 /**
  * MyTheme is a custom Theme class for the Simpler theme.
- * 
+ *
  * @package Habari
- */ 
+ */
 
 /**
  * @todo This stuff needs to move into the custom theme class:
- */ 
+ */
 
-// Apply Format::autop() to post content... 
+// Apply Format::autop() to post content...
 Format::apply( 'autop', 'post_content_out' );
 // Apply Format::autop() to comment content...
 Format::apply( 'autop', 'comment_content_out' );
-// Apply Format::tag_and_list() to post tags... 
+// Apply Format::tag_and_list() to post tags...
 Format::apply( 'tag_and_list', 'post_tags_out' );
 // Apply Format::nice_date() to post date...
 Format::apply( 'nice_date', 'post_pubdate_out', 'F j, Y \a\t g:ia' );
@@ -22,34 +22,34 @@ Format::apply( 'nice_date', 'comment_date_out', 'F j, Y \a\t g:ia' );
 
 //////////////////////Format::apply_with_hook_params( 'more', 'post_content_out', 'more', 100, 1 );
 
-// We must tell Habari to use MyTheme as the custom theme class: 
+// We must tell Habari to use MyTheme as the custom theme class:
 define( 'THEME_CLASS', 'MyTheme' );
 
 /**
  * A custom theme for K2 output
- */ 
+ */
 class MyTheme extends Theme
 {
 
 	/**
 	 * Add additional template variables to the template output.
-	 * 	 
-	 *  You can assign additional output values in the template here, instead of 
-	 *  having the PHP execute directly in the template.  The advantage is that 
+	 *
+	 *  You can assign additional output values in the template here, instead of
+	 *  having the PHP execute directly in the template.  The advantage is that
 	 *  you would easily be able to switch between template types (RawPHP/Smarty)
 	 *  without having to port code from one to the other.
-	 *  
+	 *
 	 *  You could use this area to provide "recent comments" data to the template,
-	 *  for instance.	  	 	 
-	 *  
-	 *  Note that the variables added here should possibly *always* be added, 
+	 *  for instance.
+	 *
+	 *  Note that the variables added here should possibly *always* be added,
 	 *  especially 'user'.
-	 * 	 
+	 *
 	 *  Also, this function gets executed *after* regular data is assigned to the
-	 *  template.  So the values here, unless checked, will overwrite any existing 
-	 *  values.	 	 	 
-	 */	 	 	 	 	
-	public function add_template_vars() 
+	 *  template.  So the values here, unless checked, will overwrite any existing
+	 *  values.
+	 */
+	public function add_template_vars()
 	{
 		if( !$this->template_engine->assigned( 'pages' ) ) {
 			$this->assign('pages', Posts::get( array( 'content_type' => 'page', 'status' => Post::status('published'), 'nolimit' => 1 ) ) );
@@ -61,7 +61,58 @@ class MyTheme extends Theme
 			$page = Controller::get_var( 'page' );
 			$this->assign('page', isset( $page ) ? $page : 1 );
 		}
+
+		$copyright = Options::get( 'simpler__copyright_notice' );
+
+		if ( $copyright == null ) {
+			$copyright = '&copy; Copyright ' . date('Y') . '. All Rights Reserved.';
+		}
+		else {
+			$copyright = str_replace( '%year', date('Y'), $copyright );
+		}
+
+		$this->assign( 'copyright', $copyright );
+
 		parent::add_template_vars();
+	}
+
+
+	public function action_update_check() {
+		Update::add( 'Simpler Theme', 'b903172d-caa2-487a-81ec-e7a41dd76c5e', $this->version );
+	}
+
+	public function filter_theme_config($configurable)
+	{
+		$configurable = true;
+		return $configurable;
+	}
+
+	/**
+	* Respond to the user selecting 'configure' on the themes page
+	*
+	*/
+	public function action_theme_ui()
+	{
+		$form = new FormUI( 'simpler_theme' );
+
+		$copyright_notice = $form->append('text', 'copyright_notice', 'simpler__copyright_notice', _t( 'Copyright Notice' ) );
+		$copyright_notice->template = 'optionscontrol_text';
+		$copyright_notice->class = 'clear';
+		$copyright_notice->raw = true;
+		$copyright_notice->helptext = _t( 'Use %year to substitute in the current year.' );
+
+		$form->append('submit', 'save', 'Save');
+		$form->on_success( array( $this, 'save_config' ) );
+		$form->out();
+	}
+
+	public function save_config( $form ){
+
+		Session::notice( _t( 'Theme Settings Updated' ) );
+
+		$form->save();
+
+		return true;
 	}
 }
 
