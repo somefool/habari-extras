cc1896ada3d2e9d8ce3fd1beaea64508c406fb93
mikelietz <mikelietz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-02-23 16:39:09 +0000

    Thickbox: Added help() and a GUID. There's probably a need to set priority on adding CSS to the stack, but I didn't have any luck with it.
    git-svn-id: http://svn.habariproject.org/habari-extras@1621 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/thickbox/trunk/thickbox.plugin.php
+++ plugins/thickbox/trunk/thickbox.plugin.php
@@ -13,18 +13,36 @@ class Thickbox extends Plugin {
 			'authorurl' => 'http://habariproject.org/',
 			'license' => 'Apache License 2.0',
 			'description' => 'Adds Thickbox functionality to your theme.',
-			'copyright' => '2008'
+			'copyright' => '2009'
 		);
 	}
 	
 	/**
 	 * Adds needed files to the theme stacks (javascript and stylesheet)
 	 */
-	public function action_init() {
+	public function action_init()
+	{
 		Stack::add( 'template_header_javascript', Site::get_url('scripts') . '/jquery.js', 'jquery' );
 		Stack::add( 'template_header_javascript', Site::get_url('user') . '/plugins/thickbox/thickbox-compressed.js', 'thickbox-js' );
 		Stack::add( 'template_stylesheet', array( Site::get_url('user') . '/plugins/thickbox/thickbox.css', 'screen,projector'), 'thickbox-css' );
 	}
 	
+	/**
+	 * Add help text to plugin configuration page
+	 **/
+	public function help()
+	{
+		$help = _t( 'There is no configuration for this plugin. Once this plugin is activated, add <code>class="thickbox"</code> to the link of an image file to open it in a hybrid modal box. More information and advanced instructions are available at the  <a href="http://jquery.com/demo/thickbox/" title="ThickBox 3.1 Documentation and Examples">jQuery Demo page</a>.' 
+	);
+		return $help;
+	}
+
+	/**
+	 * Add update beacon support
+	 **/
+	public function action_update_check()
+	{
+	 	Update::add( 'Thickbox', '5e276e9e-c7ce-4010-a979-42580feefdd9', $this->info->version );
+	}
 }
 ?>
