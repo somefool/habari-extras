0527f10d7527ddb9343dc1cca984db56c35cca75
ringmaster <ringmaster@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-02-26 14:56:55 +0000

    Allow different post types to have different extra fields.  Breaking API with previous version.  Additional API revisions to come.
    git-svn-id: http://svn.habariproject.org/habari-extras@1632 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/postfields/trunk/postfields.plugin.php
+++ plugins/postfields/trunk/postfields.plugin.php
@@ -12,7 +12,7 @@ class postfields extends Plugin
 	{
 		return array(
 			'name' => 'Post Fields',
-			'version' => '1.0',
+			'version' => '1.1',
 			'url' => 'http://habariproject.org',
 			'author' => 'Habari Community',
 			'authorurl' => 'http://habariproject.org',
@@ -32,7 +32,12 @@ class postfields extends Plugin
 	public function filter_plugin_config($actions, $plugin_id)
 	{
 		if ($plugin_id == $this->plugin_id()){
-			$actions[] = 'Configure';
+			$types = Post::list_active_post_types();
+			unset($types['any']);
+			foreach($types as $type => $id) {
+				$actions['config-' . $id] = _t('%s fields', array($type));
+			}
+			$actions['plugin'] = _t('Build Plugin');
 		}
 
 		return $actions;
@@ -46,14 +51,26 @@ class postfields extends Plugin
 	*/
 	public function action_plugin_ui($plugin_id, $action)
 	{
-		if ($plugin_id == $this->plugin_id()){
-			switch ($action){
-				case 'Configure' :
+		if ($plugin_id == $this->plugin_id()) {
+			switch($action) {
+				case 'plugin':
+
 					$ui = new FormUI('postfields');
-					$ui->append('textmulti', 'fields', 'postfields__fields', 'Additional Fields:');
+					$ui->append('static', 'typelabel', _t('Add this code to a plugin to implement the currently configured fields.'));
+					$ui->append('textarea', 'plugincode', 'null:null', _t('Plugin code:'))->value = $this->get_code();
+					$ui->out();
+					
+					break;
+
+				default:
+					$types = array_flip(Post::list_active_post_types());
+					$key = substr($action, 7);
+			
+					$ui = new FormUI('postfields');
+					$ui->append('static', 'typelabel', _t('Adding fields to the "%s" post type.', array($types[$key])));
+					$ui->append('textmulti', 'fields', 'postfields__fields_' . $key, 'Additional Fields:');
 					$ui->append('submit', 'submit', 'Submit');
 					$ui->out();
-					break;
 			}
 		}
 	}
@@ -66,7 +83,10 @@ class postfields extends Plugin
 	**/
 	public function action_form_publish($form, $post)
 	{
-		$fields = Options::get('postfields__fields');
+		$fields = Options::get('postfields__fields_' . $post->content_type);
+		if(!is_array($fields) || count($fields) == 0) {
+			return;
+		}
 		$output = '';
 		$control_id = 0;
 		$postfields = $form->publish_controls->append('fieldset', 'postfields', 'Additional Fields');
@@ -88,7 +108,10 @@ class postfields extends Plugin
 	*/
 	public function action_publish_post($post, $form)
 	{
-		$fields = Options::get('postfields__fields');
+		$fields = Options::get('postfields__fields_' . $post->content_type);
+		if(!is_array($fields) || count($fields) == 0) {
+			return;
+		}
 		foreach($fields as $field) {
 			$control_id = md5($field);
 			$fieldname = "postfield_{$control_id}";
@@ -96,7 +119,78 @@ class postfields extends Plugin
 			$post->info->{$field} = $customfield->value;
 		}
 	}
+
+	public function get_code()
+	{
+		$cases_form = '';
+
+		$types = Post::list_active_post_types();
+		unset($types['any']);
+		foreach($types as $type => $id) {
+			$fields = Options::get('postfields__fields_' . $id);
+			if(!is_array($fields) || count($fields) == 0) {
+				continue;
+			}
+			$fieldlist = array();
+			foreach($fields as $field) {
+				$fieldlist[] = "'" . addslashes($field) . "'";
+			}
+			$fieldlist = implode(', ', $fieldlist);
+			$cases_form .= "\t\t\tcase {$id}:\n\t\t\t\t\$fields = array({$fieldlist});\n\t\t\t\tbreak;\n";
+		}		
+
+		$code = <<< PLUGIN_CODE_1
+
+	/**
+	* Add additional controls to the publish page tab
+	*
+	* @param FormUI \$form The form that is used on the publish page
+	* @param Post \$post The post being edited
+	**/
+	public function action_form_publish(\$form, \$post)
+	{
+		switch(\$post->content_type) {
+			{$cases_form}
+			default:
+				return;
+		}
+		foreach(\$fields as \$field) {
+			\$control_id = md5(\$field);
+			\$fieldname = "postfield_{\$control_id}";
+			\$customfield = \$postfields->append('text', \$fieldname, 'null:null', \$field);
+			\$customfield->value = isset(\$post->info->{\$field}) ? \$post->info->{\$field} : '';
+			\$customfield->template = 'tabcontrol_text';
+		}
+	}
 	
+
+	/**
+	* Modify a post before it is updated
+	*
+	* @param Post \$post The post being saved, by reference
+	* @param FormUI \$form The form that was submitted on the publish page
+	*/
+	public function action_publish_post(\$post, \$form)
+	{
+		switch(\$post->content_type) {
+			{$cases_form}
+			default:
+				return;
+		}
+		foreach(\$fields as \$field) {
+			\$control_id = md5(\$field);
+			\$fieldname = "postfield_{\$control_id}";
+			\$customfield = \$form->\$fieldname;
+			\$post->info->{\$field} = \$customfield->value;
+		}
+	}
+
+PLUGIN_CODE_1;
+
+		return $code;		
+	}
+	
+		
   /**
    * Add update beacon support
    **/
