0a22fad6a3229a3df60f00a879dc07affefe0387
chrismeller <chrismeller@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-02-27 02:55:45 +0000

    theme:simpler Fixes incorrectly indented code that caused a mistaken duplicate closing div tag.
    git-svn-id: http://svn.habariproject.org/habari-extras@1647 f4a90942-1786-4aa1-b092-57c31935ddc3

--- themes/simpler/trunk/page.tag.php
+++ themes/simpler/trunk/page.tag.php
@@ -2,15 +2,14 @@
 		<div id="content">
 			<div class="post">
 				<h2 class="title"><a href="<?php echo $post->permalink; ?>" rel="bookmark" title="Permanent Link to <?php echo $post->title; ?>"><?php echo $post->title_out; ?></a></h2>
-					<div class="entry">
-						<ul>
-							<?php 
-							foreach(Tags::get() as $tag) {
-								echo '<li><a href="' . URL::get('display_entries_by_tag', array('tag'=>$tag->slug)) . '">' . $tag->tag . '</a> ('. $tag->count . ')</li>';
-							}
-							?>
-						</ul>
-					</div>
+				<div class="entry">
+					<ul>
+						<?php 
+						foreach(Tags::get() as $tag) {
+							echo '<li><a href="' . URL::get('display_entries_by_tag', array('tag'=>$tag->slug)) . '">' . $tag->tag . '</a> ('. $tag->count . ')</li>';
+						}
+						?>
+					</ul>
 				</div>
 			</div>
 		</div>
