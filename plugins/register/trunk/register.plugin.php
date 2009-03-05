750a3dcd22d2abbcf2360f8c60e3d4a9912c8b2b
michaeltwofish <michaeltwofish@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-03-05 08:46:55 +0000

    Initial commit of register plugin, which allows users to register with the blog and be added to a group defined in a template, ->registration('group');
    git-svn-id: http://svn.habariproject.org/habari-extras@1679 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/register/trunk/register.plugin.php
@@ -0,0 +1,124 @@
+<?php
+
+class Register extends Plugin
+{
+
+	const VERSION= '0.1';
+
+	/**
+	 * Return plugin metadata for this plugin
+	 *
+	 * @return array Plugin metadata
+	 */
+	public function info()
+	{
+		return array(
+			'url' => 'http://habariproject.org',
+			'name' => 'Register',
+			'license' => 'Apache License 2.0',
+			'author' => 'Habari Community',
+			'version' => self::VERSION,
+			'description' => 'Lets people register to become blog users, and be placed in a group specified by the admin.'
+		);
+	}
+
+	/**
+	 * Add update beacon support
+	 **/
+	public function action_update_check()
+	{
+		Update::add( 'Register', '57281af4-f709-46d0-8089-6bb327bab3e5', $this->info->version );
+	}
+
+	/**
+	 * Add help text to plugin configuration page
+	 **/
+	public function help()
+	{
+		$help = _t( 'Lets people register to become blog users. Administrators can specify which group new users should be placed in. Registration forms can exist on their own page or can be added to an existing theme template using, for example $theme->signup(\'registered\') to add users to the \'registered\' group.');
+		return $help;
+	}
+
+	/**
+	 * Add the registration content type
+	 **/
+	public function action_plugin_activation( $plugin_file )
+	{
+		if ( Plugins::id_from_file(__FILE__) == Plugins::id_from_file($plugin_file) ) {
+			// Store a secret key for hashing group names
+			Options::set('register_secret', UUID::get());
+		}
+	}
+
+	public function get_form( $group )
+	{
+		$form = new FormUI('registration');
+		$form->class[] = 'registration';
+
+		$form->append('text', 'email', 'null:null', _t('Email'), 'formcontrol_text');
+		$form->email->add_validator('validate_email');
+		$form->append('text', 'username', 'null:null', _t('Username'), 'formcontrol_text');
+		$form->username->add_validator('validate_required');
+		$form->username->add_validator('validate_username');
+
+		$form->append('text', 'password', 'null:null', _t('Password'), 'formcontrol_text');
+		$form->password->add_validator('validate_required');
+
+		// Store the group to be added, all secreted up
+		$form->append('hidden', 'group_name', 'group_name');
+		$group_hash = md5($group);
+		$form->group_name->value = $group_hash;
+		$form->append('hidden', 'group_digest', 'group_digest');
+		$form->group_digest->value = $this->hmac($group_hash);
+
+		// Create the Register button
+		$form->append('submit', 'register', _t('Register'), 'formcontrol_submit');
+
+		$form->on_success( array( $this, 'register_user' ) );
+
+		// Return the form object
+		return $form;
+	}
+
+	public function register_user( $form )
+	{
+		$allowed_groups = array();
+
+		foreach ( UserGroups::get_all() as $group ) {
+			$allowed_groups[] = $group->name;
+		}
+
+		$allowed_groups = array_combine(array_map('md5', $allowed_groups), $allowed_groups);
+
+		$group = '';
+		if ($this->hmac($form->group_name->value) == $form->group_digest->value) {
+			$group = UserGroup::get($allowed_groups[$form->group_name]);
+		}
+		else {
+			die('You naughty hacker!');
+		}
+
+		$user = new User( array( 'username' => $form->username, 'email' => $form->email, 'password' => Utils::crypt( $form->password ) ) );
+		if ( $user->insert() ) {
+			$group->add($user);
+			Session::notice( sprintf( _t( "Added user '%s'" ), $form->username ) );
+		}
+		else {
+			$dberror = DB::get_last_error();
+			Session::error( $dberror[2], 'adduser' );
+		}
+	}
+
+	public function theme_registration( $theme, $group )
+	{
+		$this->get_form($group)->out();
+	}
+
+	function hmac($data)
+	{
+		return md5(md5($data) . Options::get( 'title' ) . md5(Options::get('register_secret')));
+	}
+
+}
+
+?>
