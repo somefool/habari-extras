b2be366a37fd011175120952075ec0c3d21b1bd8
sean <sean@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-04-15 01:58:38 +0000

    change geshi params to use pre, and allow multiple slice elements
    git-svn-id: http://svn.habariproject.org/habari-extras@1773 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/highlighter/trunk/highlighter.plugin.php
+++ plugins/highlighter/trunk/highlighter.plugin.php
@@ -21,7 +21,7 @@
  * Note, you'll also need to grab GeSHi from http://qbnz.com/highlighter/ and
  * unpack the archive to: [habari directory]/3rdparty/geshi
  *
- * THIS PLUGIN REQUIRES HABARI 0.6-dev! IT WILL NOT WORK ON 0.5.2
+ * THIS PLUGIN REQUIRES HABARI 0.7-dev! IT WILL NOT WORK ON 0.6 (needs at least r3475)
  */
 
 class HighlightPlugin extends Plugin {
@@ -32,7 +32,7 @@ class HighlightPlugin extends Plugin {
 			'url' => 'http://seancoates.com/habari',
 			'author' => 'Sean Coates',
 			'authorurl' => 'http://seancoates.com/',
-			'version' => '0.1',
+			'version' => '0.1.1',
 			'description' => 'Highlighter',
 			'license' => 'Apache License 2.0',
 		);
@@ -60,7 +60,7 @@ class GeshiHighlighterFormatPlugin extends Format
 		
 		$tokenizer = new HTMLTokenizer( $in, false );
 		$tokens = $tokenizer->parse();
-		$slices = $tokens->slice( 'div', array( 'class' => 'highlight' ) );
+		$slices = $tokens->slice( array('div','pre','code') , array( 'class' => 'highlight' ) );
 		foreach ($slices as $slice) {
 			$classes = array_filter( explode( ' ', trim( str_replace( 'highlight', '', $slice[0]['attrs']['class'] ) ) ) ); // ugly, refactor
 			$slice->trim_container(); // trims off the div
@@ -68,7 +68,7 @@ class GeshiHighlighterFormatPlugin extends Format
 			
 			$sliceCacheName = 'plugin.highlight.' . md5($sliceValue);
 			
-			if ( Cache::has( $sliceCacheName ) ) {
+			if ( false && Cache::has( $sliceCacheName ) ) {
 				$geshiOutput = Cache::get( $sliceCacheName );
 			} else {
 				// capture the first class (not "highlight")
@@ -77,10 +77,10 @@ class GeshiHighlighterFormatPlugin extends Format
 					$sliceValue = substr( $sliceValue, 9, -3 );
 				}
 				$geshi = new Geshi( trim( $sliceValue ), isset( $classes[0] ) ? $classes[0] : 'php' );
-				$geshi->set_header_type( GESHI_HEADER_DIV );
+				$geshi->set_header_type( GESHI_HEADER_PRE );
 				$geshi->set_overall_class( 'geshicode' );
 				$geshiOutput = @$geshi->parse_code(); // @ is slow, but geshi is full of E_NOTICE
-				Cache::set( $sliceCacheName, $geshiOutput );
+				Cache::set( $sliceCacheName, '$geshiOutput' );
 			}
 			$slice->tokenize_replace( $geshiOutput );
 			$tokens->replace_slice( $slice );
