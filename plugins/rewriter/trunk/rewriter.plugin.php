64f5400ac19fa7b3402d1abeaec4619512cc0b28
michaeltwofish <michaeltwofish@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-05-07 12:32:09 +0000

    Add ACL support.
    git-svn-id: http://svn.habariproject.org/habari-extras@1798 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/rewriter/trunk/rewriter.plugin.php
+++ plugins/rewriter/trunk/rewriter.plugin.php
@@ -3,7 +3,7 @@
 class Rewriter extends Plugin
 {
 	var $rules;
-	
+
 	public function info()
 	{
 		return array(
@@ -11,27 +11,27 @@ class Rewriter extends Plugin
 			'author' => 'Habari Community',
 			'description' => 'Allows you to change rewrite rules for custom permalinks.',
 			'url' => 'http://habariproject.org',
-			'version' => '0.1',
+			'version' => '0.6-0.2',
 			'license' => 'Apache License 2.0'
 		);
 	}
-	
+
 	public function action_init()
 	{
 		$this->add_template( 'rules', dirname(__FILE__) . '/rules.php' );
 	}
-	
+
 	public function filter_default_rewrite_rules( $rules ) {
 		$this->rules = $rules;
-		
+
 		return $rules;
 	}
-	
+
 	function add_rule( $name, $params ) {
 		$rule = RewriteRules::by_name( $name );
 		if(count($rule) == 1) {
 			$rule = $rule[0];
-			
+
 			foreach($params as $key => $param) {
 				$rule->$key = $param;
 			}
@@ -40,22 +40,22 @@ class Rewriter extends Plugin
 		} else {
 			$rule = new RewriteRule($params);
 			$rule->insert();
-		}	
-		
+		}
+
 	}
-	
+
 	function action_add_template_vars( $theme ) {
 		$theme->simple = Options::get('rewriter:simpleEntry');
-		
-		$theme->rules = self::get_rules();	
+
+		$theme->rules = self::get_rules();
 	}
-	
+
 	function action_admin_theme_get_rules( $handler, $theme )
-	{	
+	{
 		$handler_vars = $handler->handler_vars;
-		
+
 		if(isset($handler_vars['names'])) {
-			
+
 			foreach($handler_vars['names'] as $key => $name) {
 				$changes = array(
 					'name' => $name,
@@ -64,16 +64,16 @@ class Rewriter extends Plugin
 					'priority' => $handler_vars['priorities'][$key],
 					'description' => $handler_vars['descriptions'][$key]
 				);
-				
+
 				self::add_rule( $key, $changes);
-				
+
 			}
-			
+
 			Session::notice(_t('Rewrite rules updated.'));
-			
+
 			Utils::redirect();
 		}
-		
+
 		$theme->display( 'rules' );
 		exit;
 	}
@@ -88,16 +88,44 @@ class Rewriter extends Plugin
 		$menu['rules']= array( 'url' => URL::get( 'admin', 'page=rules' ), 'title' => _t('Modify rewrite rules'), 'text' => _t('Rewrite Rules'), 'hotkey' => 'R', 'selected' => false );
 		return $menu;
 	}
-	
+
 	function get_rules() {
-		
+
 		$rules = $this->rules;
-		
+
 		$rules = RewriteRules::sort_rules( RewriteRules::get_active());
-		
+
 		$this->rules = $rules;
-		
+
 		return $rules;
+	}
+
+
+/**
+ * add ACL tokens when this plugin is activated
+ **/
+public function action_plugin_activation( $file )
+{
+	if ( Plugins::id_from_file($file) == Plugins::id_from_file(__FILE__) ) {
+		ACL::create_token( 'Rewriter', 'Create custom rewrite rules', 'rewriter' );
+	}
+}
+
+/**
+	* remove ACL tokens when this plugin is deactivated
+**/
+function action_plugin_deactivation( $plugin_file )
+{
+		if( Plugins::id_from_file( __FILE__ ) == Plugins::id_from_file( $plugin_file  ) ) {
+				ACL::destroy_token( 'Rewriter' );
+		}
+}
+
+	public function filter_admin_access_tokens( $require_any, $page, $type )
+	{
+		if ( 'rules' == $page ) {
+			$require_any = array( 'super_user' => true, 'rewriter' => true );
+		} return $require_any;
 	}
 }
 
