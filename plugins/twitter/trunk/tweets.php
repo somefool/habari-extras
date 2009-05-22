567454b67f38899cd7493a4fa6221183fe0aad08
mikelietz <mikelietz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-05-22 19:40:52 +0000

    Twitter: Added option for multiple tweet display, and a new tweets.php template. NOTE: This will break existing tweets.php! If you use a custom one for your theme it will need to be updated.
    git-svn-id: http://svn.habariproject.org/habari-extras@1805 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/twitter/trunk/tweets.php
+++ plugins/twitter/trunk/tweets.php
@@ -1,7 +1,13 @@
 <!-- This file can be copied and modified in a theme directory -->
 
 <div id="twitterbox">
- <img src="<?php echo htmlspecialchars( $tweet_image_url ); ?>" alt="<?php echo urlencode( Options::get( 'twitter__username' )); ?>">
- <p><?php echo $tweet_text . ' @ ' . $tweet_time; ?></p>
+	<ul>
+		<?php foreach ($tweets as $tweet) : ?>
+		<li>
+			<img src="<?php echo htmlspecialchars( $tweet->image_url ); ?>" alt="<?php echo urlencode( Options::get( 'twitter__username' )); ?>">
+			<?php echo $tweet->text . ' @ ' . $tweet->time; ?>
+		</li>
+		<?php endforeach; ?>
+	</ul>
 <p><small>via <a href="http://twitter.com/<?php echo urlencode( Options::get( 'twitter__username' )); ?>">Twitter</a></small></p>
  </div>
