f0c14ac683f453cd1c1167a1ab9e389f180e25f5
mikelietz <mikelietz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-05-22 19:57:01 +0000

    Twitter: reverting last commit, so /dist/plugins/twitter.zip works for 0.6.x users, but not before copying to an 0.7 branch, where new development should happen.
    git-svn-id: http://svn.habariproject.org/habari-extras@1806 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/twitter/trunk/tweets.php
+++ plugins/twitter/trunk/tweets.php
@@ -1,13 +1,7 @@
 <!-- This file can be copied and modified in a theme directory -->
 
 <div id="twitterbox">
-	<ul>
-		<?php foreach ($tweets as $tweet) : ?>
-		<li>
-			<img src="<?php echo htmlspecialchars( $tweet->image_url ); ?>" alt="<?php echo urlencode( Options::get( 'twitter__username' )); ?>">
-			<?php echo $tweet->text . ' @ ' . $tweet->time; ?>
-		</li>
-		<?php endforeach; ?>
-	</ul>
+ <img src="<?php echo htmlspecialchars( $tweet_image_url ); ?>" alt="<?php echo urlencode( Options::get( 'twitter__username' )); ?>">
+ <p><?php echo $tweet_text . ' @ ' . $tweet_time; ?></p>
 <p><small>via <a href="http://twitter.com/<?php echo urlencode( Options::get( 'twitter__username' )); ?>">Twitter</a></small></p>
  </div>
