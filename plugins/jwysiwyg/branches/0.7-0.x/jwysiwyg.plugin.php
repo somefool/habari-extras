fd0de76428d09c1f0793d2b93f704b80aebe5fa8
michaeltwofish <michaeltwofish@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-06-11 04:55:30 +0000

    Branch for 0.7, update XML, remove info()
    git-svn-id: http://svn.habariproject.org/habari-extras@1902 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/jwysiwyg/branches/0.7-0.x/jwysiwyg.plugin.php
@@ -0,0 +1,41 @@
+<?php
+
+class JWYSIWYG extends Plugin
+{
+
+	public function action_admin_header($theme)
+	{
+		if ( $theme->page == 'publish' ) {
+			Stack::add('admin_header_javascript', $this->get_url() . '/jwysiwyg/jquery.wysiwyg.js');
+			Stack::add('admin_stylesheet', array($this->get_url() . '/jwysiwyg/jquery.wysiwyg.css', 'screen'));
+		}
+	}
+
+	public function action_admin_footer($theme)
+	{
+		if ( $theme->page == 'publish' ) {
+			echo <<<JWYSIWYG
+			<script type="text/javascript">
+			$('label[for=content]').text('');
+			$(function()
+			{
+				$('#content').wysiwyg();
+			});
+			habari.editor = {
+				insertSelection: function(value) {
+					var instance = $.data($('#content')[0], 'wysiwyg');
+					instance.setContent(instance.getContent() + value);
+				}
+			}
+			</script>
+JWYSIWYG;
+		}
+	}
+
+	public function action_update_check()
+	{
+		Update::add( 'JWYSIWYG', 'b5f0c17d-22e6-4d6c-8011-c79481d5efc7',  $this->info->version );
+	}
+}
+
+?>
