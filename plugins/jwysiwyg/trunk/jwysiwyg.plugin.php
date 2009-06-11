12bfdb4894b034b61b99e84f183bee9ca8ffe194
michaeltwofish <michaeltwofish@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-06-11 04:33:22 +0000

    Update jwysiwyg external to 0.5 tag. Change version number to new version system and bump plugin version.
    git-svn-id: http://svn.habariproject.org/habari-extras@1900 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/jwysiwyg/trunk/jwysiwyg.plugin.php
+++ plugins/jwysiwyg/trunk/jwysiwyg.plugin.php
@@ -13,7 +13,7 @@ class JWYSIWYG extends Plugin {
 			'url' => 'http://habariproject.org/',
 			'author' => 'Habari Community',
 			'authorurl' => 'http://habariproject.org/',
-			'version' => '0.3',
+			'version' => '0.6-0.4',
 			'description' => 'Publish posts using the JWYSIWYG editor.',
 			'copyright' => '2008'
 		);
