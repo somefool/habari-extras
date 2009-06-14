95b9302ea1bbc0312ff38b0d8c65d1eab98523da
michaeltwofish <michaeltwofish@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-06-14 15:09:18 +0000

    Initial commit of Simple Gallery plugin, which turns a file structure with image files into an image gallery. Todo: some code clean up, a file management interface, help function.
    git-svn-id: http://svn.habariproject.org/habari-extras@1958 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/simplegallery/branches/0.7-0.x/simplegallery.php
@@ -0,0 +1,44 @@
+<?php $theme->display('header'); ?>
+<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $css; ?>">
+<!--begin content-->
+<div id="content">
+
+	<div id='gallery-breadcrumbs'>
+	<?php
+		$links = array(array_pop($breadcrumbs));
+		$base = Site::get_url( 'habari' );
+		while ( !empty($breadcrumbs) ) {
+			$link = $base . '/' . implode('/', $breadcrumbs);
+			$breadcrumb = array_pop($breadcrumbs);
+			$links[] = "<span class='gallery-breadcrumb'><a href='{$link}'>$breadcrumb</a></span>";
+		}
+		echo implode(" &raquo; ", array_reverse($links));
+	?>
+	</div>
+
+	<h2><?php echo $title; ?></h2>
+
+	<?php foreach ($dirs as $dir): ?>
+
+		<div class='gallery-dir'>
+			Gallery: <a href='<?php echo $dir->url; ?>' title='<?php echo $dir->pretty_title; ?>'><?php echo $dir->pretty_title; ?></a>
+		</div>
+
+	<?php endforeach; ?>
+
+	<div class='clear'></div>
+
+	<?php foreach ($images as $image): ?>
+
+		<div class='gallery-img'>
+			<a href='<?php echo $image->url; ?>' title='<?php echo $image->pretty_title; ?>'>
+				<img src='<?php echo $image->thumbnail_url; ?>'>
+			</a>
+			<h3><?php echo $image->pretty_title; ?></h3>
+		</div>
+
+	<?php endforeach; ?>
+
+</div><!--end content-->
+
+<?php $theme->display('footer'); ?>
---
+++ plugins/simplegallery/branches/0.7-0.x/simplegallery.plugin.php
@@ -0,0 +1,178 @@
+<?php
+
+class SimpleGallery extends Plugin
+{
+	function __get( $name )
+	{
+		switch ($name) {
+			case 'base':
+				$base = Options::get('simplegallery_base');
+				if ( null == $base ) {
+					$base = 'gallery';
+				}
+				return $base;
+			default:
+				return NULL;
+		}
+	}
+
+	/**
+	 * Do some checking and setting up.
+	 */
+	public function action_plugin_activation( $file )
+	{
+		// Don't bother loading if the gd library isn't active
+		if ( !function_exists( 'imagecreatefromjpeg' ) ) {
+			Session::error( _t( "Simple Gallery activation failed. PHP has not loaded the gd imaging library." ) );
+			Plugins::deactivate_plugin( __FILE__ );
+		}
+		else {
+			/*
+			$this->silo = new HabariSilo();
+			$this->silo->action_init();
+			$this->silo->mkdir('simplegallery');
+			*/
+		}
+	}
+
+	/**
+	 * Add the necessary template and create the silo to access files and directories
+	 *
+	 **/
+	function action_init()
+	{
+		$this->add_template( 'simplegallery', dirname(__FILE__) . '/simplegallery.php' );
+		$this->silo = new HabariSilo();
+		$this->silo->action_init();
+	}
+
+
+	public function filter_rewrite_rules( $rules )
+	{
+		$rules[]= new RewriteRule( array(
+			'name' => 'simplegallery',
+			'parse_regex' => '%^' . $this->base . '(?:/?$|/(?P<gallerypath>.*))/?$%i',
+			'build_str' => $this->base . '/({$gallerypath})',
+			'handler' => 'PluginHandler',
+			'action' => 'display_gallery',
+			'priority' => 6,
+			'is_active' => 1,
+			'description' => 'Respond to requests for the simple gallery.',
+			'parameters' => serialize( array( 'require_match' => array('SimpleGallery', 'rewrite_match_gallery') ) )
+		) );
+
+		return $rules;
+	 }
+
+	/**
+	 * Check the requested gallery exists
+	 * @param RewriteRule $rule The matched rewrite rule
+	 * @param string The URL stub requested
+	 * @param array $params Some stuff
+	 **/
+	public static function rewrite_match_gallery( $rule, $stub, $params )
+	{
+		// TODO It would be better to use the silo, but there's no way to check if a path is valid
+		// $silo->get_dir() always returns at least an empty array, even for invalid paths
+		$base = Site::get_dir('user') . '/files/simplegallery/';
+		// Strip the base URL from the front of the stub, and add it to the base to get the full path.
+		$sg = new SimpleGallery();
+		$path = $base . substr($stub, strlen($sg->base));
+		return file_exists($path);
+	}
+
+	public function action_plugin_act_display_gallery( $handler )
+	{
+		$gallery_path = $handler->handler_vars['gallerypath'];
+		// Check if it's an image file is being requested
+		$image = $this->silo->silo_get('simplegallery/' . $gallery_path);
+		if ( $image && in_array($image->filetype, array('image_gif', 'image_png', 'image_jpeg') ) ) {
+			header('Content-type: ' . $image->filetype);
+			echo $image->content;
+			exit;
+		}
+
+		// It must be a directory
+		$assets = $this->silo->silo_dir('simplegallery/' . $gallery_path);
+
+		$theme = Controller::get_handler()->theme;
+		$theme->css = $this->get_url() . '/simplegallery.css';
+		$dirs = array();
+		$images = array();
+
+		if ( 0 != count($assets) ) {
+
+			foreach ($assets as $asset) {
+				// Need to decode twice to keep the /, because URL::get() callse RewriteRule::build() which urlencodes.
+				$asset->url = urldecode(urldecode(
+					URL::get( 'simplegallery', array( 'gallerypath' => $gallery_path . '/'. ($asset->title) ) )
+				));
+				$asset->pretty_title = $this->pretty_title($asset->title);
+				if ( $asset->is_dir ) {
+					$dirs[] = $asset;
+				}
+				else if ( in_array($asset->filetype, array('image_gif', 'image_png', 'image_jpeg') ) ) {
+					$images[] = $asset;
+				}
+			}
+
+		}
+		// Make a breadcrumb array
+		// TODO Just horrible. There must be a nicer way to do this.
+		$breadcrumbs = array($this->base);
+		if ( '' != $gallery_path ) {
+			$breadcrumbs = array_merge($breadcrumbs, explode('/', trim($this->pretty_title($gallery_path), '/')));
+		}
+		$theme->breadcrumbs = $breadcrumbs;
+		$theme->title = $breadcrumbs[count($breadcrumbs) - 1];
+
+		$theme->dirs = $dirs;
+		$theme->images = $images;
+
+		return $theme->display('simplegallery');
+
+	}
+
+	public function filter_plugin_config( $actions, $plugin_id )
+	{
+		if ( $plugin_id == $this->plugin_id() ) {
+			$actions[] = _t( 'Configure' );
+		}
+
+		return $actions;
+	}
+
+	/**
+	* Respond to the user selecting an action on the plugin page
+	*
+	* @param string $plugin_id The string id of the acted-upon plugin
+	* @param string $action The action string supplied via the filter_plugin_config hook
+	*/
+	public function action_plugin_ui( $plugin_id, $action )
+	{
+		if ( $plugin_id == $this->plugin_id() ) {
+			switch ( $action ) {
+				case 'Configure' :
+					$form = new FormUI( 'simplegallery' );
+
+					$form->append( 'text', 'base', 'option:simplegallery_base', _t( 'Gallery location:', 'simplegallery' ) );
+					$form->base->value = $this->base;
+					$form->append( 'submit', 'submit', _t( 'Submit' ) );
+
+					$form->out();
+					break;
+			}
+		}
+	}
+
+	private function pretty_title($title)
+	{
+		// Strip any extension
+		$pretty_title = basename($title, '.' . pathinfo($title, PATHINFO_EXTENSION) );
+		// Turn underscores or hyphens into spaces
+		$pretty_title = strtr($pretty_title, '_-', '  ');
+		return $pretty_title;
+	}
+}
+
+?>
