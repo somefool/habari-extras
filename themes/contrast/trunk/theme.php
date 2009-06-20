8a61d26700ca93ab165a46f82de33eb8914912d7
arthus <arthus@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-06-20 05:27:40 +0000

    theme:contrast add xml/php
    git-svn-id: http://svn.habariproject.org/habari-extras@1986 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ themes/contrast/trunk/theme.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * ResurrectionTheme is a custom Theme class for teh Resurrection theme
+ *
+ * @package Habari
+ */
+
+
+// We must tell Habari to use ResurrectionTheme as the custom theme class:
+define( 'THEME_CLASS', 'Contrast' );
+
+/**
+ * A custom theme for Resurrection output
+ */
+class Contrast extends Theme
+{
+
+
+	
+}
+
+?>
