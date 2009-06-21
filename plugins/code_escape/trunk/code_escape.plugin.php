c56540f6c51b32dc2894a3eaa7cefbf4331ef662
chrismeller <chrismeller@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-06-21 23:07:43 +0000

    plugin:code_escape Updating plugin info for XML format, and adding update beacon support.
    git-svn-id: http://svn.habariproject.org/habari-extras@1998 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/code_escape/trunk/code_escape.plugin.php
+++ plugins/code_escape/trunk/code_escape.plugin.php
@@ -2,22 +2,6 @@
 
 	class Code_Escape extends Plugin {
 		
-		const VERSION = '0.1';
-		
-		public function info ( ) {
-			
-			return array (
-					'name' => 'Code Escape',
-					'url' => 'http://habariproject.org',
-					'author' => 'Habari Community',
-					'authorurl' => 'http://habariproject.org',
-					'version' => self::VERSION,
-					'description' => 'Runs htmlspecialchars() on any &lt;code&gt; blocks in your post.',
-					'license' => 'Apache License 2.0'
-			);
-			
-		}
-		
 		public function filter_post_content_out ( $content, $post ) {
 			
 			$content = preg_replace_callback('/<code>(.*?)<\/code>/s', array( 'self', 'escape_code' ), $content);
@@ -36,6 +20,12 @@
 			
 			return $string;
 			
+		}
+		
+		public function action_update_check ( ) {
+
+			Update::add( 'Code Escape', '3ede3d2c-cb4f-4a37-9ac5-c5918fef7257', $this->info->version );
+		
 		}
 		
 	}
