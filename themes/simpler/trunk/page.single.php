05fcbfe23a565c755e2fb61592d77ebfa119eedf
RandyWalker <RandyWalker@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-07-30 09:29:53 +0000

    Theme:Simpler updated meta-info display on pages, no comments on pages
    git-svn-id: http://svn.habariproject.org/habari-extras@2122 f4a90942-1786-4aa1-b092-57c31935ddc3

--- themes/simpler/trunk/page.single.php
+++ themes/simpler/trunk/page.single.php
@@ -5,13 +5,9 @@
 				<div class="entry">
 					<?php echo $post->content_out; ?>
 				</div>
-				<p class="meta">
-					<?php echo $post->pubdate_out; ?> <?php if ( ! $post->info->comments_disabled ) { ?> | <a href="<?php echo $post->permalink; ?>#comments" title="Comments on this post"><?php echo $post->comments->approved->count; ?> <?php echo _n( 'Comment', 'Comments', $post->comments->approved->count ); ?></a><?php } ?><?php if ( $user->loggedin ) { ?> | <a href="<?php URL::out( 'admin', 'page=publish&id=' . $post->id); ?>" title="Edit post">Edit</a><?php } ?><br>
-					<?php if ( $post->tags ) { ?> Tagged: <?php echo $post->tags_out; ?><?php } ?>
-				</p>
-				<div id="comments">
-					<?php include 'comments.php'; ?>
-				</div>
+				
+				<?php if ( $user->loggedin ) { ?><p class="meta"><a href="<?php URL::out( 'admin', 'page=publish&id=' . $post->id); ?>" title="Edit post">Edit</a></p><?php } ?>
+
 			</div>
 		</div>
 <?php include 'footer.php'; ?>
