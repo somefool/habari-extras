412f87cf52583f157e7e63adff6e5be2dd9f73c2
mikelietz <mikelietz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-07-31 20:27:55 +0000

    plugin:simpletaglist Added some comments. Also, props to eighty4 for fluffytag, referenced in the creation of this.
    git-svn-id: http://svn.habariproject.org/habari-extras@2133 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/simpletaglist/trunk/simpletaglist.php
+++ plugins/simpletaglist/trunk/simpletaglist.php
@@ -1,3 +1,4 @@
+<!-- This file can be copied and modified in a theme directory -->
 <ul id="tag-links">
 <?php	foreach( $theme->tag_links as $tag) { ?>
 	<li><a href="<?php URL::out( 'display_entries_by_tag', array( 'tag'=> $tag['slug'] ), false ); ?>"><?php echo $tag['tag']; ?></a></li>
--- plugins/simpletaglist/trunk/simpletaglist.plugin.php
+++ plugins/simpletaglist/trunk/simpletaglist.plugin.php
@@ -1,22 +1,35 @@
 <?php
 class SimpleTagList extends Plugin
 {
+	/**
+	 * Add help text to plugin configuration page
+	 **/
 	public function help() 
 	{
 		$help = _t( 'To use, add <code>&lt;?php $theme->tag_links(); ?&gt;</code> to your theme where you want the list output.' );
 		return $help;
 	}
 
+	/**
+	 * Add update beacon support
+	 **/
 	public function action_update_check()
 	{
 		 Update::add( 'Simple Tag List', 'da67e437-e3b9-4d7a-bad8-5ab55b6d801b', $this->info->version );
 	}
 
+	/**
+	 * On plugin init, add the template included with this plugin to the available templates in the theme
+	 */
 	public function action_init()
 	{
 		$this->add_template( 'simpletaglist', dirname(__FILE__) . '/simpletaglist.php' );
 	}
 
+	/**
+	 * Add simplified tag array to the available template vars
+	 * @param Theme $theme The theme that will display the template
+	 **/
 	public function theme_tag_links( $theme )
 	{
 		$tags = Tag::get();
