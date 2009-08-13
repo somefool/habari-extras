2a03192a557d89cfa98dff84a42184b3e4d15836
arthus <arthus@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-08-13 03:39:05 +0000

    plugin:mpango initial commit
    git-svn-id: http://svn.habariproject.org/habari-extras@2214 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/mpango/trunk/mpango.plugin.php
@@ -0,0 +1,157 @@
+<?php
+
+class Mpango extends Plugin
+{
+	
+	public function action_update_check()
+	{
+		Update::add( $this->info->name, 'e283ba9d-d16d-4932-b9dd-0117e84a3ba8', $this->info->version );
+	}
+	
+	/**
+	 * Set up needed stuff for the plugin
+	 **/
+	public function install()
+	{
+		Post::add_new_type( 'project' );
+		
+		// Give anonymous users access
+		$group = UserGroup::get_by_name('anonymous');
+		$group->grant('post_project', 'read');
+	}
+	
+	/**
+	 * Remove stuff we installed
+	 **/
+	public function uninstall()
+	{
+		Post::deactivate_post_type( 'project' );
+	}
+	
+	/**
+	 * action_plugin_activation
+	 * @param string $file plugin file
+	 */
+	function action_plugin_activation( $file )
+	{
+		if( Plugins::id_from_file($file) == Plugins::id_from_file(__FILE__) ) {
+			self::install();
+		}
+	}
+
+	/**
+	 * action_plugin_deactivation
+	 * @param string $file plugin file
+	 */
+	function action_plugin_deactivation( $file )
+	{
+		if( Plugins::id_from_file($file) == Plugins::id_from_file(__FILE__) ) {
+			self::uninstall();
+		}
+	}
+	
+	/**
+	 * Create name string
+	 **/
+	public function filter_post_type_display($type, $foruse) 
+	{ 
+		$names = array( 
+			'project' => array(
+				'singular' => _t('Project'),
+				'plural' => _t('Projects'),
+			)
+		); 
+ 		return isset($names[$type][$foruse]) ? $names[$type][$foruse] : $type; 
+	}
+	
+	/**
+	 * Modify publish form
+	 */
+	public function action_form_publish($form, $post)
+	{
+		if ( $post->content_type == Post::type('project') ) {
+			
+			$options = $form->publish_controls->append('fieldset', 'options', _t('Project'));
+			
+			$options->append('text', 'repository', 'null:null', _t('Repository URL'), 'tabcontrol_text');
+			$options->repository->value = $post->project->repository->base;
+			
+		}
+	}
+	
+	/**
+	 * Save our data to the database
+	 */
+	public function action_publish_post( $post, $form )
+	{
+		if ($post->content_type == Post::type('project')) {
+			
+			// $this->action_form_publish( $form, $post, 'create');
+			
+			$post->info->repository = $form->repository->value;
+		
+		}
+	}
+	
+	/**
+	 * Creates the Project class for each post
+	 **/
+	public function filter_post_project($project, $post) {
+		if($post->content_type == Post::type('project')) {
+			return new Project( $post );
+		}
+		else {
+			return $permalink;
+		}
+	}
+	
+}
+
+/**
+* Class for projects, is a subclass of post
+*/
+class Project
+{
+	
+	function __construct( $post )
+	{
+		$this->post = $post;
+	}
+	
+	public function __get( $property ) {
+		switch ( $property ) {
+			case 'xml_url':
+				$this->xml_url = $this->repository->trunk . $this->post->slug . '.plugin.xml';
+				return $this->xml_url;
+			case 'repository':
+				$repository = new stdClass;
+				
+				$repository->base = $this->post->info->repository;
+				$repository->trunk = $repository->base . 'trunk/';
+			
+				$this->repository = $repository;
+				return $this->repository;
+			case 'description':
+				$this->description = (string) $this->xml->description;
+				return $this->description;
+			case 'authors':
+				$authors = array();
+				foreach( $this->xml->author as $author) {
+					$authors[] = array(
+						'url' => (string) $author['url'],
+						'name' => (string) $author
+					);
+				}
+								
+				$this->authors = $authors;
+				return $this->authors;
+			case 'xml':
+				$this->xml = simplexml_load_file( $this->xml_url );
+				return $this->xml;
+		}
+	}
+	
+}
+
+
+?>
