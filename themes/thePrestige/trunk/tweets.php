69f653b4b63a6a822c1d95f69d842cb79c1731ff
mikelietz <mikelietz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-09-12 15:51:32 +0000

    theme:theprestige Get tweets.php working. It may look funky if you decide to show more than one, but it works for one.
    git-svn-id: http://svn.habariproject.org/habari-extras@2283 f4a90942-1786-4aa1-b092-57c31935ddc3

--- themes/thePrestige/trunk/tweets.php
+++ themes/thePrestige/trunk/tweets.php
@@ -1,5 +1,7 @@
 <div id="twitter">
 <h2>{hi:"Twittering"}</h2>
-	<p><?php echo $tweet_text ?><br>
+	<?php foreach ($tweets as $tweet) : ?>
+	<p><?php echo $tweet->text; ?><br>
 		<small>{hi:"via"} <a href="http://twitter.com/<?php echo urlencode( Options::get( 'twitter__username' )); ?>" title="<?php echo urlencode( Options::get( 'twitter__username' )); ?>">{hi:"Twitter"}</a></small></p>
+	<?php endforeach; ?>
 </div>
