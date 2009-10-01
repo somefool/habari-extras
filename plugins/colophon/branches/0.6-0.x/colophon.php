f06d4fe23a8722930ccc9a9d252db5a1dd38b133
lildude <lildude@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-10-01 11:58:06 +0000

    Updated 0.6 branch to reflect more recent code in trunk and moved 0.7 code to trunk in the process removing the 0.7 branch
    git-svn-id: http://svn.habariproject.org/habari-extras@2325 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/colophon/branches/0.6-0.x/colophon.php
+++ plugins/colophon/branches/0.6-0.x/colophon.php
@@ -1,4 +1,4 @@
 <!-- This file can be copied and modified in a theme directory -->
 
 <h2><?php echo $theme->colophon_title; ?></h2>
-<p><?php echo $theme->colophon_text; ?></p>
+<p><?php echo nl2br( $theme->colophon_text ); ?></p>
--- plugins/colophon/branches/0.7-0.x/colophon.php
+++
@@ -1,4 +0,0 @@
-<!-- This file can be copied and modified in a theme directory -->
-
-<h2><?php echo $theme->colophon_title; ?></h2>
-<p><?php echo $theme->colophon_text; ?></p>
