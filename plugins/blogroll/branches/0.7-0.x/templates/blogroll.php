fd7eb0e450367b751ca6971ef98f5b024b1f2d29
lildude <lildude@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-10-01 11:51:26 +0000

    plugin:blogroll Removed redundant 0.7 branch as all the same code is in trunk
    git-svn-id: http://svn.habariproject.org/habari-extras@2324 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/blogroll/branches/0.7-0.x/templates/blogroll.php
+++
@@ -1,9 +0,0 @@
-<!-- To customize this template, copy it to your currently active theme directory and edit it -->
-<div id="blogroll">
-	<h2><?php echo $blogroll_title; ?></h2>
-	<ul>
-	<?php if ( ! empty( $blogs ) ) { foreach( $blogs as $blog ) { ?>
-		<li class="vcard"><a href="<?php echo $blog->info->url; ?>" class="url" title="<?php echo $blog->content; ?>" rel="<?php echo $blog->info->relationship; ?> <?php echo $blog->xfn_relationships; ?>"><?php echo $blog->title; ?></a></li>
-	<?php } } ?>
-	</ul>
-</div>
