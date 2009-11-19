104cfcd0734feb4c5a9d39b9ebbe035a70849091
philip <philip@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-11-19 04:48:50 +0000

    created 0.6 tag from current trunk
    git-svn-id: http://svn.habariproject.org/habari-extras@2393 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/randomquotes/tags/0.6-0.1/quote.php
@@ -0,0 +1,6 @@
+<!-- For Random Quotes: This file can be copied and modified in a theme directory -->
+
+<span class="randomquote">
+	<p><q><?php echo htmlspecialchars( $quote_text ); ?></q></p><br /> 
+	<p>&#8211; <cite><?php echo htmlspecialchars( $quote_author ); ?></cite></p> 
+</span>
