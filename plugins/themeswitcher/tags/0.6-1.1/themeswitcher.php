c9ebae47d3465498c8c4b27c0e7635b32cdd08f6
philip <philip@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-11-22 02:19:11 +0000

    Tagged 0.6-1.1
    git-svn-id: http://svn.habariproject.org/habari-extras@2435 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/themeswitcher/tags/0.6-1.1/themeswitcher.php
@@ -0,0 +1,19 @@
+<?php
+$active_theme= Options::get( 'theme_dir' );
+$all_themes= Themes::get_all_data();
+$selected_themes= Options::get( 'themeswitcher__selected_themes' );
+?>
+	<div class="sb-switcher">
+		<h2>Theme Switcher</h2>
+		<form action="" method="post" name="themeswitcher">
+			<select name="theme_dir" onChange="this.form.submit()">
+				<?php
+				foreach( $selected_themes as $selected_theme ) {
+					$theme= $all_themes[$selected_theme];
+				?>
+					<option value="<?php echo $theme['dir']; ?>"<?php echo ($theme['dir'] == $active_theme) ? 'selected' : ''; ?>><?php echo $theme['info']->name; ?> <?php echo $theme['info']->version; ?></option>
+				<?php } ?>
+			</select>
+			<input type="submit" style="display: none;" name="themeswitcher_submit" value="Switch">
+		</form>
+	</div>
