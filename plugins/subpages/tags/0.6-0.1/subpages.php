399a1e70db9f346a26914d7c166932ec1ed0c106
philip <philip@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-11-23 18:40:29 +0000

    Deleted misguided tagging of 0.6, because 0.7+ (taxonomy) is required. Requested by michaeltwofish
    git-svn-id: http://svn.habariproject.org/habari-extras@2452 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/subpages/tags/0.6-0.1/subpages.php
+++
@@ -1,8 +0,0 @@
-<span>Subpages:</span>
-<?php
-$out = array();
-foreach ( $subpages as $subpage ) {
-	$out[] = "<a href='{$subpage->permalink}'>{$subpage->title}</a>";
-}
-echo join(' | ', $out);
-?>
