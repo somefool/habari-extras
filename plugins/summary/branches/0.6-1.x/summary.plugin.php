2ada36341ed83307768e2e8435ef8b7898ffeca6
coldflame <coldflame@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-12-01 05:02:49 +0000

    plugin:summary Fixed formatting, and removed requirement to only go on entries. All content-types can have a summary. in the future, this could probably be set as an admin option, but this plugin is nice and simple, so i'm keeping it this way.
    git-svn-id: http://svn.habariproject.org/habari-extras@2468 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/summary/branches/0.6-1.x/summary.plugin.php
@@ -0,0 +1,77 @@
+<?php
+ 
+/**
+ * Summary Plugin Class
+ * 
+ * This plugin adds a 'Summary' field on the entry publishing form, which is
+ * available to templates and code as $post->info->summary. A summary element
+ * is added to the Atom feed for entries with a non-empty summary.
+ *
+ **/
+ 
+class SummaryPlugin extends Plugin
+{
+	/**
+	 * function info
+	 * Returns information about this plugin
+	 * @return array Plugin info array
+	 **/
+	function info()
+	{
+		return array (
+			'name' => 'Summary',
+			'url' => 'http://scratch.flyoverblues.com/summary.plugin.php',
+			'author' => 'Eli Naeher',
+			'authorurl' => 'http://flyoverblues.com',
+			'version' => '1.0',
+			'description' => 'A plugin for providing post summaries',
+			'license' => 'Apache License 2.0',
+			);
+	}
+ 
+	/**
+	 * Add fields to the publish page for Pages
+	 *
+	 * @param FormUI $form The publish form
+	 * @param Post $post The post being published
+	 */
+	public function action_form_publish($form, $post)
+	{
+		$form->insert('tags', 'text', 'summary', 'null:null', _t('Summary'), 'admincontrol_textArea');
+		$form->summary->raw = true;
+		$form->summary->value = $post->info->summary;
+		$form->summary->tabindex = 3;
+		$form->tags->tabindex = 4;
+		$form->buttons->save->tabindex = 5;
+		$form->summary->template = 'admincontrol_text';
+	}
+
+	/**
+	 * Store summary in postinfo table when the entry is published.
+	 *
+	 * @param FormUI $form The publish form
+	 * @param Post $post The post being published
+	 */
+
+	public function action_publish_post($post, $form)
+	{
+		$post->info->summary = $form->summary->value;
+	}
+
+	/**
+	 * Add summary element to Atom feed, if a summary is present.
+	 *
+	 * @param SimpleXMLElement $feed_entry
+	 * @param Post $post The post corresponding to the feed entry
+	 */
+
+	public function action_atom_add_post($feed_entry, $post)
+	{
+		if ((boolean) $post->info->summary) {
+			$feed_entry->addChild( 'summary', $post->info->summary );
+		}
+	}
+
+}
+ 
+?>
