757df29a5b45a790cde7cbebe65023908ba96701
somefool <somefool@f4a90942-1786-4aa1-b092-57c31935ddc3>
2009-12-16 20:28:05 +0000

    added 404 page
    git-svn-id: http://svn.habariproject.org/habari-extras@2568 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ themes/hmallow2/trunk/404.php
@@ -0,0 +1,52 @@
+<?php // get header ?>
+<?php $theme->display ( 'header'); ?>
+
+
+<body id="single" >
+
+<div id="shadow">
+
+<div id="rap">
+
+<?php // get the nav etc ?>
+<?php include "topbanner.php"; ?>
+
+
+
+
+<div id="content">
+
+
+
+						
+
+<div class="post">
+
+
+	
+<!-- Post title -->
+<h3 class="storytitle">Error 404: Page not found</h3>
+
+
+		
+		<!-- Actual post -->
+<div class="storycontent">
+
+</div>
+
+
+</div>
+
+
+</div>
+
+
+
+<?php // get footer ?>
+<?php $theme->display ( 'footer' ); ?>
+
+</div>
+</div>
+
+</body>
+</html>
