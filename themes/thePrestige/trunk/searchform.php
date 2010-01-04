3baa059ca69dc23a27449d8cc4fe3684dd785403
rickc <rickc@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-01-04 19:49:36 +0000

    Finish hienginifying the searchform.
    git-svn-id: http://svn.habariproject.org/habari-extras@2639 f4a90942-1786-4aa1-b092-57c31935ddc3

--- themes/thePrestige/trunk/searchform.php
+++ themes/thePrestige/trunk/searchform.php
@@ -1,7 +1,7 @@
 <!-- searchform -->
 <?php Plugins::act( 'theme_searchform_before' ); ?>
 	<form method="get" id="searchform" action="{hi:url:display_search}">
-		<p><input type="text" id="s" name="criteria" value="<?php if ( isset( $criteria ) ) { echo htmlentities($criteria, ENT_COMPAT, 'UTF-8'); } else { _e('Search'); } ?>"><br> <input type="submit" id="searchsubmit" value="{hi:"Go!"}"></p>
+	<p><input type="text" id="s" name="criteria" value="{hi:?isset(criteria)}{hi:escape:criteria}{/hi:?}"> <input type="submit" id="searchsubmit" value="{hi:"Go!"}"></p>
 	</form>
 <?php Plugins::act( 'theme_searchform_after' ); ?>
 <!-- searchform -->
