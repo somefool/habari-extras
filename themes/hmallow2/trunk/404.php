1cba3b731002fd7bd251bd94afca22b27625b353
somefool <somefool@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-01-10 12:31:53 +0000

    Various minor tweaks to CSS etc & remove 'in progress' folder.
    git-svn-id: http://svn.habariproject.org/habari-extras@2661 f4a90942-1786-4aa1-b092-57c31935ddc3

--- themes/hmallow2/trunk/404.php
+++ themes/hmallow2/trunk/404.php
@@ -1,7 +1,6 @@
 <?php // get header ?>
 <?php $theme->display ( 'header'); ?>
 
-
 <body id="single" >
 
 <div id="shadow">
@@ -11,42 +10,29 @@
 <?php // get the nav etc ?>
 <?php include "topbanner.php"; ?>
 
-
-
-
 <div id="content">
-
-
-
-						
 
 <div class="post">
 
-
-	
 <!-- Post title -->
 <h3 class="storytitle">Error 404: Page not found</h3>
 
-
-		
-		<!-- Actual post -->
+<!-- Actual post -->
 <div class="storycontent">
 
 </div>
 
-
 </div>
 
-
 </div>
-
-
 
 <?php // get footer ?>
 <?php $theme->display ( 'footer' ); ?>
 
 </div>
+
 </div>
 
 </body>
+
 </html>
--- themes/hmallow2/trunk/commentform.php
+++ themes/hmallow2/trunk/commentform.php
@@ -1,4 +1,5 @@
 <!-- commentsform -->
+
 <?php // Do not delete these lines
 if ( ! defined('HABARI_PATH' ) ) { die( _t('Please do not load this page directly. Thanks!') ); }
 
@@ -21,21 +22,19 @@ else {
 	$commenter_name= '';
 	$commenter_email= '';
 	$commenter_url= '';
-}
-
-
+}  
 ?>
-     <div class="commentform">
-      <h4 id="respond" class="reply"><?php _e('Leave a Reply'); ?></h4>
+
+<div class="commentform">
+<h4 id="respond" class="reply"><?php _e('Leave a Reply'); ?></h4>
 <?php
 if ( Session::has_messages() ) {
 	Session::messages_out();
 }
 ?>
 
-    <?php	$post->comment_form()->out(); ?>
+<?php	$post->comment_form()->out(); ?>
     
-    
-         </div>
+</div>
 
 <!-- /commentsform -->
