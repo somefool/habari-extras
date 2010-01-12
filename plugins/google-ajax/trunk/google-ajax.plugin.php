2f8390b5ee4c5647929354c2bf6ee912ca1dad07
lildude <lildude@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-01-12 17:22:47 +0000

    plugin:google-ajax - Added some stack entry duplication detection and tidied up code
    git-svn-id: http://svn.habariproject.org/habari-extras@2667 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/google-ajax/trunk/google-ajax.plugin.php
+++ plugins/google-ajax/trunk/google-ajax.plugin.php
@@ -2,42 +2,42 @@
 class GoogleAjax extends Plugin
 {
 	/**
-     * Beacon Support for Update checking
-     *
-     * @access public
-     * @return void
-     **/
-    public function action_update_check()
-    {
+	 * Beacon Support for Update checking
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function action_update_check()
+	{
 		Update::add( 'GoogleAjax', 'DDB34CAA-ECBE-11DE-A151-593F56D89593', $this->info->version );
-    }
+	}
 
 	/**
-     * Add the Configure option for the plugin
-     *
-     * @access public
-     * @param array $actions
-     * @param string $plugin_id
-     * @return array
-     */
-    public function filter_plugin_config( $actions, $plugin_id )
-    {
+	 * Add the Configure option for the plugin
+	 *
+	 * @access public
+	 * @param array $actions
+	 * @param string $plugin_id
+	 * @return array
+	 */
+	public function filter_plugin_config( $actions, $plugin_id )
+	{
 		if ( $plugin_id == $this->plugin_id() ) {
 			$actions[]= _t( 'Configure' );
 		}
 		return $actions;
-    }
+	}
 
 	/**
-     * Plugin UI
-     *
-     * @access public
-     * @param string $plugin_id
-     * @param string $action
-     * @return void
-     */
-    public function action_plugin_ui( $plugin_id, $action )
-    {
+	 * Plugin UI
+	 *
+	 * @access public
+	 * @param string $plugin_id
+	 * @param string $action
+	 * @return void
+	 */
+	public function action_plugin_ui( $plugin_id, $action )
+	{
 		if ( $plugin_id == $this->plugin_id() ) {
 			switch ( $action ) {
 				case _t( 'Configure' ):
@@ -74,11 +74,11 @@ class GoogleAjax extends Plugin
 	 * @param $filter
 	 * @return array
 	 */
-    public function filter_stack_out( $stack, $stack_name, $filter )
+	public function filter_stack_out( $stack, $stack_name, $filter )
 	{
 		if ( count( $stack ) == 0 ) {
-            return $stack;
-        }
+			return $stack;
+		}
 
 		// Array of direct links - defaults to latest version
 		// TODO: Google doesn't offer a method to programatically determine the available versions, so these are hard-coded.
@@ -86,19 +86,23 @@ class GoogleAjax extends Plugin
 				'jquery'		=> 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js',
 				'jqueryui'		=> 'http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js',
 				'prototype'		=> 'http://ajax.googleapis.com/ajax/libs/prototype/1/prototype.js',
-				'scriptaculous' => 'http://ajax.googleapis.com/ajax/libs/scriptaculous/1/scriptaculous.js',
+				'scriptaculous'		=> 'http://ajax.googleapis.com/ajax/libs/scriptaculous/1/scriptaculous.js',
 				'mootools'		=> 'http://ajax.googleapis.com/ajax/libs/mootools/1/mootools-yui-compressed.js',
 				'dojo'			=> 'http://ajax.googleapis.com/ajax/libs/dojo/1/dojo/dojo.xd.js',
 				'swfobject'		=> 'http://ajax.googleapis.com/ajax/libs/swfobject/2/swfobject.js',
 				'yui'			=> 'http://ajax.googleapis.com/ajax/libs/yui/2/build/yuiloader/yuiloader-min.js',
 				'ext-core'		=> 'http://ajax.googleapis.com/ajax/libs/ext-core/3/ext-core.js',
-				'chrome-frame'	=> 'http://ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js'
-			);
+				'chrome-frame'		=> 'http://ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js'
+				);
 
 		switch ( $stack_name ) {
-            case 'template_footer_javascript':
-            case 'template_header_javascript':
-				if ( Options::get( __CLASS__ . '__direct_link') ) {
+			case 'template_header_javascript':
+			case 'template_footer_javascript':
+				// First we remove the duplicates that occur in the header stack if we're processing the footer stack - Habari doesn't do this, so we do.  This is all about performance after all.
+				if ( $stack_name == 'template_footer_javascript' ) {
+					$stack = array_diff_key( $stack, Stack::get_named_stack( 'template_header_javascript' ) );
+				}
+				if ( Options::get( __CLASS__ . '__direct_link' ) ) {
 					$int = array_intersect_key( $googleHosts, $stack );
 					return array_merge( $stack, $int );
 				} else {
@@ -109,14 +113,14 @@ class GoogleAjax extends Plugin
 							$ver = explode( '/', $googleHosts[$key] );
 							$newstack['jsapi_load'] .= 'google.load("'.$key.'", "'.$ver[6].'");'; // This assumes Google keeps a consistent URL structure with the version in the 6th field.
 							unset( $stack[$key] );	// Remove replaced key from original stack
-						} 
+						}
 					}
-					return array_merge( $newstack, $stack ); // Merge stacks, putting the Google API calls first.
+					return ( $newstack['jsapi_load'] != '' ) ? array_merge( $newstack, $stack ) : $stack; // Merge stacks, putting the Google API calls first.
 				}
 				break;
-            default:
-                return $stack;
-        }
+			default:
+				return $stack;
+		}
 	}
 }
 ?>
