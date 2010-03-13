7e2ae0f3eebc3c03b30961d5aeef7bbae25669e1
mikelietz <mikelietz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-03-13 03:00:40 +0000

    theme:habarism Not actually using this template, can be removed.
    git-svn-id: http://svn.habariproject.org/habari-extras@3017 f4a90942-1786-4aa1-b092-57c31935ddc3

--- themes/habarism/trunk/forms/formcontrol_submit.php
+++
@@ -1,4 +0,0 @@
-<input type="submit"<?php if(isset($disabled) && $disabled) { ?>disabled <?php } if ( isset( $tabindex ) ) { ?> tabindex="<?php echo $tabindex; ?>"<?php } ?> name="<?php echo $field; ?>" id="<?php echo $id; ?>" value="<?php echo htmlspecialchars($caption); ?>">
-<?php if($message != '') : ?>
-<p class="error"><?php echo $message; ?></p>
-<?php endif; ?>
