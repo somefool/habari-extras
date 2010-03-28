61aa139ac7a62628c76c3166c51e2011a6b73203
ringmaster <ringmaster@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-03-28 05:12:52 +0000

    Allow easy adding of validators using an array.  Opens the potential for a form to add more custom validators.
    git-svn-id: http://svn.habariproject.org/habari-extras@3053 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/commonblocks/trunk/commonblocks.plugin.php
+++ plugins/commonblocks/trunk/commonblocks.plugin.php
@@ -17,6 +17,10 @@ class CommonBlocks extends Plugin
 //		'twitter_updates' => 'Twitter Updates',
 
 	);
+	
+	
+	// See action_init for this initial value:
+	private $validation_urls = array();
 
 	/**
 	 * Register the template.
@@ -26,6 +30,14 @@ class CommonBlocks extends Plugin
 		foreach ( array_keys( $this->allblocks ) as $blockname ) {
 			$this->add_template( "block.$blockname", dirname(__FILE__) . "/block.$blockname.php" );
 		}
+
+		// This is here because you can't init a URL with dynamic values in the declaration
+		$this->validation_urls = array(		
+			'XHTML 1.0 Transitional' => 'http://validator.w3.org/check?uri=referer',
+			'CSS level 3' => 'http://jigsaw.w3.org/css-validator/check/referer?profile=css3',
+			'HTML5' => 'http://html5.validator.nu/?doc=' . Site::get_url('habari'),
+			'Feed Validator' => 'http://beta.feedvalidator.org/check.cgi?url=' . Site::get_url('habari'),
+		);
 	}
 
 	/**
@@ -45,6 +57,12 @@ class CommonBlocks extends Plugin
 	public function action_block_form_recent_comments( $form, $block )
 	{
 		$content = $form->append('text', 'quantity', $block, _t( 'Comments to show:' ) );
+		$form->append('submit', 'save', 'Save');
+	}
+
+	public function action_block_form_w3c_validators( $form, $block )
+	{
+		$content = $form->append('checkboxes', 'links', $block, _t( 'Links to show:' ), array_flip($this->validation_urls) );
 		$form->append('submit', 'save', 'Save');
 	}
 
@@ -69,6 +87,16 @@ class CommonBlocks extends Plugin
 			'type'=>Comment::COMMENT, 
 			'orderby'=>'date DESC',
 		) );
+	}
+	
+	public function action_block_content_w3c_validators($block, $theme)
+	{
+		$list = array();
+		$validation_urls = array_flip($this->validation_urls);
+		foreach($block->links as $link) {
+			$list[$link] = $validation_urls[$link];
+		}
+		$block->list = $list;
 	}
 	
 	/**
