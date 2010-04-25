647f7b001a89a4b8166294febe33b907c1b9e980
mikelietz <mikelietz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-04-25 03:56:17 +0000

    plugin:commonblocks Make the form name more specific, so as to allow for more than one of these to work. Add another tag archives template for a javascripted dropdown of months.
    git-svn-id: http://svn.habariproject.org/habari-extras@3084 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/commonblocks/trunk/block.dropdown.tag_archives.php
+++ plugins/commonblocks/trunk/block.dropdown.tag_archives.php
@@ -1,8 +1,8 @@
-<form id="archiveform" action="">
+<form id="tag_archive_form" action="">
 <fieldset><legend>Browse Archives</legend>
 
 <select name="archive_tags" onchange="window.location =
-(document.forms.archiveform.archive_tags[document.forms.archiveform.archive_tags.selectedIndex].value);">
+(document.forms.tag_archive_form.archive_tags[document.forms.tag_archive_form.archive_tags.selectedIndex].value);">
 
     <option value=''>by tag</option>
     <?php $tags = $content->tags; foreach( $tags as $tag ): ?>
