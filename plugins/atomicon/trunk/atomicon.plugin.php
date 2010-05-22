50ddff3fb153777a7ed2659fbc9a0766d9639af3
coar <coar@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-05-22 05:53:39 +0000

    'Nother plugin, to add an <icon> element to the Atom feed
    git-svn-id: http://svn.habariproject.org/habari-extras@3138 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/atomicon/trunk/atomicon.plugin.php
@@ -0,0 +1,85 @@
+<?php
+class AtomIcon extends Plugin {
+
+    const GUID         = '854af572-1fb6-4e67-bd28-cdbeec4020c1';
+    const PLUGIN_NAME  = 'AtomIcon';
+    const PLUGIN_TOKEN = 'AtomIcon_token';
+    const VERSION      = '1.0';
+
+    /**
+     * Do any wrapper-like things to the Atom feed proper.
+     * @param SimpleXMLElement $xml the Atom feed document
+     * @return SimpleXMLElement the modified Atom feed document
+     */
+    public function action_atom_create_wrapper( $xml )
+    {
+        if ($iconurl = Options::get('atomicon_iconurl')) {
+            $xml->addChild('icon', $iconurl);
+        }
+        return $xml;
+    }
+
+    public function action_update_check() {
+        Update::add(self::PLUGIN_NAME, self::GUID, $this->info->version);
+    }
+
+    /*
+     * Admin-type methods
+     */
+    public function action_plugin_activation($file) {
+        if ($file == str_replace('\\', '/', $this->get_file())) {
+            ACL::create_token(self::PLUGIN_TOKEN,
+                              _t('Allow use of AtomIcon plugin'),
+                              'Category',
+                              false);
+            $group = UserGroup::get_by_name('admin');
+            $group->grant(self::PLUGIN_TOKEN);
+        }
+    }
+
+    public function action_plugin_deactivation($file) {
+        if ($file == str_replace('\\', '/', $this->get_file())) {
+            ACL::destroy_token(self::PLUGIN_TOKEN);
+        }
+    }
+
+    /*
+     * Add a configuration panel for us.
+     */
+    public function filter_plugin_config($actions, $plugin_id) {
+        if ($plugin_id == $this->plugin_id()) {
+            $actions[] = _t('Configure');
+        }
+        return $actions;
+    }
+    /*
+     * And here's the actual configuration panel itself.
+     */
+
+    public function action_plugin_ui($plugin_id, $action) {
+        if ($plugin_id == $this->plugin_id()) {
+            switch ($action) {
+            case _t('Configure'):
+                $ui = new FormUI(strtolower(get_class($this)));
+                $ui->append('text',
+                            'iconurl',
+                            'option:atomicon_iconurl',
+                            _t('Feed icon URL:'));
+                $ui->iconurl->size = 64;
+                $ui->append('submit', 'save', 'Save');
+                $ui->out();
+                break;
+            }
+        }
+    }
+
+}
+/*
+ * Local Variables:
+ * mode: C
+ * c-file-style: "bsd"
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
+?>
