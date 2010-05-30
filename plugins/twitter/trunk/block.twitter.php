0e2013fcf53532a95418c00c6e32617542f4f798
mikelietz <mikelietz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-05-30 14:20:54 +0000

    plugin:twitter The username is in $content, not $block.
    git-svn-id: http://svn.habariproject.org/habari-extras@3143 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/twitter/trunk/block.twitter.php
+++ plugins/twitter/trunk/block.twitter.php
@@ -2,9 +2,9 @@
 	<ul>
 <?php foreach ( $content->tweets as $tweet ) : ?>
 		<li>
-			<img src="<?php echo htmlspecialchars( $tweet->image_url ); ?>" alt="<?php echo urlencode( $block->username ); ?>">
+			<img src="<?php echo htmlspecialchars( $tweet->image_url ); ?>" alt="<?php echo urlencode( $content->username ); ?>">
 			<?php echo $tweet->text . ' @ ' . $tweet->time; ?>
 		</li>
 		<?php endforeach; ?>
 	</ul>
-<p><small>via <a href="http://twitter.com/<?php echo urlencode( $block->username ); ?>">Twitter</a></small></p>
+<p><small>via <a href="http://twitter.com/<?php echo urlencode( $content->username ); ?>">Twitter</a></small></p>
