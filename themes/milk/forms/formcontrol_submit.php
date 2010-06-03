f8647882a3e76951660a3aca9fe15de6ec6b5aab
Barbara <Barbara@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-06-03 20:23:24 +0000

    theme:milk deleting old files.
    git-svn-id: http://svn.habariproject.org/habari-extras@3159 f4a90942-1786-4aa1-b092-57c31935ddc3

--- themes/milk/forms/formcontrol_submit.php
+++
@@ -1,3 +0,0 @@
-<div class="row"><button type="submit">Submit</button></div>
-<div class="clear"></div>
-<?php if ( $message != '' ) : ?><p><?php echo $message; ?></p><?php endif; ?>
--- themes/milk/forms/formcontrol_textarea.php
+++
@@ -1,3 +0,0 @@
-<div class="row"><label for="<?php echo $field; ?>"><?php echo $this->caption; ?></label>
-<textarea name="<?php echo $field; ?>" id="<?php echo $field; ?>" cols="40" rows="8"><?php echo htmlspecialchars( $value ); ?></textarea></div>
-
