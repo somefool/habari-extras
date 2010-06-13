219f633080bb5025cd1b67bc6b8a3ed43fbd3af4
philip <philip@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-06-13 03:30:37 +0000

    Updated for 0.7+: Updated XML to <pluggable>, removed info(), updated to modern configure() UI, and changed plugin URL to habari as it was old/dead. The plugin loads, but not sure if it fully works yet.
    git-svn-id: http://svn.habariproject.org/habari-extras@3187 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/googleads/trunk/googleads.plugin.php
+++ plugins/googleads/trunk/googleads.plugin.php
@@ -1,18 +1,6 @@
 <?php
 class GoogleAds extends Plugin 
 {
-	function info() 
-	{
-		return array(
-			'url' => 'http://iamgraham.net/plugins',
-			'name'=> 'GoogleAds',
-			'license' => 'Apache License 2.0',
-			'author'=> 'Graham Christensen',
-			'authorurl' => 'http://iamgraham.net/',
-			'version' => '0.2'
-		);
-	}
-
 	/**
 	 * Add update beacon support
 	 **/
@@ -20,36 +8,17 @@ class GoogleAds extends Plugin
 	{
 	 	Update::add( 'GoogleAds', 'ad99b200-3ba0-11dd-ae16-0800200c9a66', $this->info->version );
 	}
-
-	public function filter_plugin_config( $actions, $plugin_id ) 
+	
+	public function configure()
 	{
-		if ( $plugin_id == $this->plugin_id() ) {
-			$actions[]= _t( 'Configure' );
-		}
-		return $actions;
-	}
+		$form = new FormUI( 'googleads' );
+		$form->append( 'text', 'clientcode', 'option:googleads__clientcode', 'Ad Client Code: ' );
+		$form->append( 'text', 'adslot',     'option:googleads__adslot',     'Ad Slot ID: ' );
+		$form->append( 'text', 'adwidth',    'option:googleads__adwidth',    'Ad Width: ');
+		$form->append( 'text', 'adheight',   'option:googleads__adheight',   'Ad Height: ');
 
-	public function action_plugin_ui($plugin_id, $action) 
-	{
-		if ( $plugin_id == $this->plugin_id() ) {
-			switch ( $action ) {	
-				case _t( 'Configure' ):
-					$ui = new FormUI( strtolower( get_class( $this ) ) );
-					$clientcode = $ui->append( 'text', 'clientcode', 'googleads__clientcode', _t( 'Ad Client Code' ) );
-					$adslot = $ui->append( 'text', 'adslot', 'googleads__adslot', _t( 'Ad Slot ID' ) );
-					$adwidth = $ui->append( 'text', 'adwidth', 'googleads__adwidth', _t( 'Ad Width' ) );
-					$adheight = $ui->append( 'text', 'adheight', 'googleads__adheight', _t( 'Ad Height' ) );
-
-					$ui->append( 'submit', 'save', _t('Save') );
-					$ui->out();
-				break;
-			}
-		}
-	}
-
-	public function updated_config( $ui )
-	{
-		return true;
+		$form->append( 'submit', 'save', 'Save' );
+		return $form;
 	}
 
 	private static function getvar( $var )
