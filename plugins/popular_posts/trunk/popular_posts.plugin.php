2d928dec05301932cc2417091df12abf9d7cfc47
mikelietz <mikelietz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-06-16 15:26:26 +0000

    plugin:popular_posts Change to a configure(), move help to XML, code cleanup.
    git-svn-id: http://svn.habariproject.org/habari-extras@3204 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/popular_posts/trunk/popular_posts.plugin.php
+++ plugins/popular_posts/trunk/popular_posts.plugin.php
@@ -9,36 +9,19 @@ class PopularPosts extends Plugin
 	public function action_init()
 	{
 		$this->add_template( 'popular_posts', dirname(__FILE__) . '/popular_posts.php' );
-	}
 
-	/**
-	 * Add a configuration action for this plugin
-	 *
-	 **/
-	public function filter_plugin_config($actions, $plugin_id)
-	{
-		if ( $plugin_id == $this->plugin_id() ) {
-			$actions[] = _t('Configure');
-		}
-		return $actions;
 	}
 
 	/**
 	 * Create a configuration form for this plugin
 	 *
 	 **/
-	public function action_plugin_ui($plugin_id, $action)
+	public function configure()
 	{
-		if ( $plugin_id == $this->plugin_id() ) {
-			switch ( $action ) {
-				case _t('Configure'):
-					$form = new FormUI(strtolower(get_class($this)));
-					$form->append('checkbox', 'loggedintoo', 'popular_posts__loggedintoo', _t('Track views of logged-in users too'));
-					$form->append('submit', 'save', 'Save');
-					$form->out();
-				break;
-			}
-		}
+		$form = new FormUI( 'popular_posts' );
+		$form->append( 'checkbox', 'loggedintoo', 'popular_posts__loggedintoo', _t( 'Track views of logged-in users too', 'popular_posts' ) );
+		$form->append( 'submit', 'save', 'Save' );
+		$form->out();
 	}
 
 	/**
@@ -48,13 +31,13 @@ class PopularPosts extends Plugin
 	public function action_add_template_vars( $theme, $handler_vars )
 	{
 		// If there is only one post
-		if ( $theme->post instanceof Post && count($theme->posts) == 1 ) {
+		if ( $theme->post instanceof Post && count( $theme->posts ) == 1 ) {
 
 			// Only track users that aren't logged in, unless specifically overridden
-			if ( !User::identify()->loggedin || Options::get('popular_posts__loggedintoo') ) {
-				$set = Session::get_set('popular_posts', false);
+			if ( !User::identify()->loggedin || Options::get( 'popular_posts__loggedintoo' ) ) {
+				$set = Session::get_set( 'popular_posts', false );
 				$post = $theme->post;
-				if ( !in_array($post->id, $set) ){
+				if ( !in_array( $post->id, $set ) ){
 					$views = $post->info->views;
 					if ( $views == null ) {
 						$views = 0;
@@ -82,21 +65,6 @@ class PopularPosts extends Plugin
 			'limit' => $limit
 		));
 		return $theme->display( 'popular_posts' );
-	}
-
-	public function help()
-	{
-		return <<< END_HELP
-<p>To output a list of popular posts, insert this code where you want them to appear:</p>
-<blockquote><code>&lt;?php \$theme-&gt;popular_posts(); ?&gt;</code></blockquote>
-<p>You can also pass the number of popular posts you want to retrieve:</p>
-<blockquote><code>&lt;?php \$theme-&gt;popular_posts(7); ?&gt;</code></blockquote>
-<p>This will retrieve the seven most popular posts. The default is five.</p>
-<p>The default theme inserts an HTML unordered list of links to the popular
-pages.  If you want to alter this, you should copy the
-<tt>popular_posts.php</tt> template included with this plugin to your current
-theme directory and make changes to it there.</p>
-END_HELP;
 	}
 
 	/**
