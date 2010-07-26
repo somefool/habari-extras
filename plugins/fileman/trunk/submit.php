31018848688fe919407351695f14f5aec3a388ee
Matt-SD <Matt-SD@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-07-26 09:01:37 +0000

    Plugin:FileMan - Delete files.
    git-svn-id: http://svn.habariproject.org/habari-extras@3260 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/fileman/trunk/submit.php
+++
@@ -1,9 +0,0 @@
-<?php
- //Check if file is writable
- if(!is_writable($_POST['file'])){die("<span style=\"color:#B00;font-weight:bold;\">File not writable.</span>");}
- $contents = $_POST['contents'];
- //Write to file
- file_put_contents($_POST['file'],$contents) or die("<span style=\"color:#B00;font-weight:bold;\">Failed to write to file.</span>");
- //Send success signal
- echo "<span style=\"color:#0B0;font-weight:bold;\">File saved Successfully.</span>";
-?>
