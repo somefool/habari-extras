a8824c9d4d638ec1788c653673294a2286deb047
eighty4 <eighty4@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-08-27 20:31:14 +0000

    Some speed improvements
    git-svn-id: http://svn.habariproject.org/habari-extras@3287 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/minification/trunk/minification.plugin.php
+++ plugins/minification/trunk/minification.plugin.php
@@ -1,6 +1,6 @@
 <?php
 
-class mini extends Plugin {
+class minification extends Plugin {
 
 	private static $cache_name = 'minify';
 	private static $stack;
@@ -10,25 +10,59 @@ class mini extends Plugin {
 	 **/
 	public function action_update_check()
 	{
-	 	Update::add( 'Minify', '', $this->info->version );
+	 	Update::add( 'Minification', '', $this->info->version );
+	}
+
+	public function action_plugin_ui( $plugin_id, $action )
+	{
+		if ( $plugin_id == $this->plugin_id() ) {
+			switch ( $action ) {
+				case _t( 'Configure' ) :
+					$ui = new FormUI( get_class( $this ) );
+					// Add a text control for the home page description and textmultis for the home page keywords
+					
+					$ui->append( 'text', 'cache_expire', 'option:' . 'minification__expire', _t( 'Time the cache should save the minified files (sec)' ) );
+
+					$ui->append( 'checkbox', 'extream_cache', 'minification__extreme', _t( 'ONLY reminify if the filenames have change.' ) );
+					
+					$ui->append( 'submit', 'save', _t( 'Save' ) );
+					$ui->out();
+					break;
+			}
+		}
 	}
 
 	public function action_template_header() {
 
-		//Cache::expire( self::$cache_name . '_js' );
-		//Cache::expire( self::$cache_name . '_css' );
+		Cache::expire( self::$cache_name . '_js' );
+		Cache::expire( self::$cache_name . '_css' );
 
 		// try to disable output_compression (may not have an effect)
 		ini_set('zlib.output_compression', '0');
 
-		$modified = Stack::get_sorted_stack('template_header_javascript');
-		foreach( $modified as $key => $value ) {			
+		$modified_js = Stack::get_sorted_stack('template_header_javascript');
+		foreach( $modified_js as $key => $value ) {			
 			Stack::remove('template_header_javascript', $key);
 		}
 		
-		Stack::add('template_header_javascript', Site::get_url('user') . "/files/minified.js", 'Minified');
+		Stack::add('template_header_javascript', Site::get_url('user') . '/files/minified.js', 'Minified');
+
+		$modified_css = Stack::get_sorted_stack('template_stylesheet');
+		$css = array();
+		foreach( $modified_css as $key => $value ) {			
+			$css[] = $value[0];
+			Stack::remove('template_stylesheet', $key);
+		}
+		Stack::add('template_stylesheet', array( Site::get_url('user') . "/files/minified.css", 'screen'), 'style' );
 		
-		if ( !Cache::has( self::$cache_name . '_js' ) || !Cache::has( self::$cache_name . '_css' ) || !file_exists(site::get_dir('user') . '/files/minified.css') || !file_exists(site::get_dir('user') . '/files/minified.js') ) {
+		if ( !file_exists(site::get_dir('user') . '/files/minified.css') || 
+			 !file_exists(site::get_dir('user') . '/files/minified.js') ||
+			
+				( !Cache::has( self::$cache_name . '_js' ) || 
+				  !Cache::has( self::$cache_name . '_css' ) )
+					
+			) {
+		
 			
 			/* Taken from min/index.php */
 			define('MINIFY_MIN_DIR', dirname(__FILE__) . '/min/');
@@ -52,78 +86,54 @@ class mini extends Plugin {
 			} elseif (0 === stripos(PHP_OS, 'win')) {
 			    Minify::setDocRoot(); // IIS may need help
 			}
-			
+
 			$min_serveOptions['minifierOptions']['text/css']['symlinks'] = $min_symlinks;
-			
-			if ($min_allowDebugFlag && isset($_GET['debug'])) {
-			    $min_serveOptions['debug'] = true;
-			}
-			
-			if ($min_errorLogger) {
-			    require_once 'Minify/Logger.php';
-			    if (true === $min_errorLogger) {
-			        require_once 'FirePHP.php';
-			        Minify_Logger::setLogger(FirePHP::getInstance(true));
-			    } else {
-			        Minify_Logger::setLogger($min_errorLogger);
-			    }
-			}
-		}
-		
-		if ( !Cache::has( self::$cache_name . '_js' ) || !file_exists(site::get_dir('user') . '/files/minified.js') ) {	
-			$js_stack = array();
-			foreach( $modified as $js ) {
-				$js_stack[] = Site::get_path('base') . str_replace(Site::get_url('habari') . '/', '', $js);
-			}
-			$options = array(
-			    'files' => $js_stack,
-			    'encodeOutput' => false,
-	   		    'quiet' => true,
-			    'maxAge' => 86400,
-			    
-			);
-			$result = Minify::serve('Files', $options);
-			file_put_contents( site::get_dir('user') . '/files/minified.js', $result['content']);
-			Cache::set( self::$cache_name . '_js', 'true' );
-		}
 
-		/* CSS */
-		$modified = Stack::get_sorted_stack('template_stylesheet');
-		$tmp = array();
-		foreach( $modified as $key => $value ) {			
-			$tmp[] = $value[0];
-			Stack::remove('template_stylesheet', $key);
-		}
-		Stack::add('template_stylesheet', array( Site::get_url('user') . "/files/minified.css", 'screen'), 'style' );
+			// Using jsmin+ 1.3
+			$min_serveOptions['minifiers']['application/x-javascript'] = array('JSMinPlus', 'minify');
 
-		if ( !Cache::has( self::$cache_name . '_css' ) || !file_exists(site::get_dir('user') . '/files/minified.css') ) {
-			$css_stack = array();
-			foreach( $tmp as $css ) {
-				$css_stack[] = Site::get_path('base') . str_replace(Site::get_url('habari') . '/', '', $css);
+			/* Javascript */
+			if ( !Cache::has( self::$cache_name . '_js' ) || !file_exists(site::get_dir('user') . '/files/minified.js') ) {	
+				$js_stack = array();
+				foreach( $modified_js as $js ) {
+					$js_stack[] = Site::get_path('base') . str_replace(Site::get_url('habari') . '/', '', $js);
+				}
+				$options = array(
+				    'files' => $js_stack,
+				    'encodeOutput' => false,
+		   		    'quiet' => true,
+				    
+				);
+				$result = Minify::serve('Files', $options);
+				file_put_contents( site::get_dir('user') . '/files/minified.js', $result['content']);
+				Cache::set( self::$cache_name . '_js', 'true', Options::get( 'fluffytag__expire' ) );
 			}
-			$options = array(
-			    'files' => $css_stack,
-			    'encodeOutput' => false,
-	   		    'quiet' => true,
-			    'maxAge' => 86400,
-			    
-			);
-			// handle request
-			$result = Minify::serve('Files', $options);
-			file_put_contents( site::get_dir('user') . '/files/minified.css', $result['content']);
 
-			Cache::set( self::$cache_name . '_css', 'true' );
+			/* CSS */
+			if ( !Cache::has( self::$cache_name . '_css' ) || !file_exists(site::get_dir('user') . '/files/minified.css') ) {
+				$css_stack = array();
+				foreach( $css as $file ) {
+					$css_stack[] = Site::get_path('base') . str_replace(Site::get_url('habari') . '/', '', $file);
+				}
+				$options = array(
+				    'files' => $css_stack,
+				    'encodeOutput' => false,
+		   		    'quiet' => true,
+				    
+				);
+				// handle request
+				$result = Minify::serve('Files', $options);
+				file_put_contents( site::get_dir('user') . '/files/minified.css', $result['content']);
+	
+				Cache::set( self::$cache_name . '_css', 'true', Options::get( 'fluffytag__expire' ) );
+			}
 		}
 	}
 	
 	public function filter_final_output( $buffer )
 	{
 		set_include_path(dirname(__FILE__). '/min/lib' . PATH_SEPARATOR . get_include_path());
-		require_once 'Minify/Source.php';
 		require_once 'Minify/HTML.php'; 
-		require_once 'Minify/CSS.php'; 
-		require_once 'Minify.php';
-		require_once 'Minify/Cache/File.php';
 		return Minify_HTML::minify( $buffer );
 	}
 }
