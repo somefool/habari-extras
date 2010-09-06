e469ef83d8c6d76f0eac78ce0b36733c1c6014d5
lildude <lildude@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-09-06 16:12:32 +0000

    plugin:habarimarkdown - Plugin now presents markdown contents instead of formatted html to Atom feed when viewed by authenticated user.  This allows for the editing and writing of posts using markdown syntax from an atompub client. Also sorted out plugin directory structure.
    git-svn-id: http://svn.habariproject.org/habari-extras@3291 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/habarimarkdown/trunk/habarimarkdown.plugin.php
+++ plugins/habarimarkdown/trunk/habarimarkdown.plugin.php
@@ -11,79 +11,85 @@ require_once "smartypants.php";
 
 class HabariMarkdown extends Plugin
 {
-	const VERSION = '0.4.3';
-	
-	public function action_init()
-	{
-		// Added by Caius Durling <dev@caius.name> <http://caius.name/>
-		// Escapes unsafe chars in the title
-		Format::apply( 'htmlescape', 'post_title_out' );
-		
-		
-		Format::apply( 'markdown', 'post_content_out' );
-		Format::apply( 'markdown', 'post_content_summary' );
-		Format::apply( 'markdown', 'post_content_more' );
-		Format::apply( 'markdown', 'post_content_excerpt' );
-		Format::apply( 'markdown', 'comment_content_out' );
-	}
+    public function action_init()
+    {
+	// Added by Caius Durling <dev@caius.name> <http://caius.name/>
+	// Escapes unsafe chars in the title
+	Format::apply( 'htmlescape', 'post_title_out' );
 
-	/**
-	 * Adds a Configure action to the plugin
-	 * 
-	 * @param array $actions An array of actions that apply to this plugin
-	 * @param string $plugin_id The id of a plugin
-	 * @return array The array of actions
-	 */
-	public function filter_plugin_config( $actions, $plugin_id )
-	{
-		if ( $this->plugin_id() == $plugin_id ){
-			$actions[]= 'Configure';		
-		}
-		return $actions;
-	}
-	
-	/**
-	 * Creates a UI form to handle the plugin configuration
-	 *
-	 * @param string $plugin_id The id of a plugin
-	 * @param array $actions An array of actions that apply to this plugin
-	 */
-	public function action_plugin_ui( $plugin_id, $action )
-	{
-		if ( $this->plugin_id()==$plugin_id && $action=='Configure' )
-			{
-			$form = new FormUI( strtolower(get_class( $this ) ) );
-			$form->append( 'checkbox', 'enable SmartyPants', 'option:habarimarkdown__smarty', _t('Enable SmartyPants') );
-			$form->append( 'submit', 'save', _t( 'Save' ) );
-			$form->out();
-			}
-	}
+	Format::apply( 'markdown', 'post_content_out' );
+	Format::apply( 'markdown', 'post_content_summary' );
+	Format::apply( 'markdown', 'post_content_more' );
+	Format::apply( 'markdown', 'post_content_excerpt' );
+	Format::apply( 'markdown', 'comment_content_out' );
+    }
 
-	/**
-	 * Filter Atom Feed
-	 * @param SimpleXMLElement $feed_entry the Atom feed entry
-	 * @param Post $post The post
-	 * @return SimpleXMLElement the filtered Atom feed entry
-	 */
-	public function action_atom_add_post( $feed_entry, $post )
-	{
-		$feed_entry->content = markdown( $post->content );
-		return $feed_entry;
+    /**
+     * Adds a Configure action to the plugin
+     *
+     * @param array $actions An array of actions that apply to this plugin
+     * @param string $plugin_id The id of a plugin
+     * @return array The array of actions
+     */
+    public function filter_plugin_config( $actions, $plugin_id )
+    {
+	if ( $this->plugin_id() == $plugin_id ){
+	    $actions[]= 'Configure';
 	}
+	return $actions;
+    }
+
+    /**
+     * Creates a UI form to handle the plugin configuration
+     *
+     * @param string $plugin_id The id of a plugin
+     * @param array $actions An array of actions that apply to this plugin
+     */
+    public function action_plugin_ui( $plugin_id, $action )
+    {
+	if ( $this->plugin_id()==$plugin_id && $action=='Configure' ) {
+	    $form = new FormUI( strtolower(get_class( $this ) ) );
+	    $form->append( 'checkbox', 'enable SmartyPants', 'option:habarimarkdown__smarty', _t( 'Enable SmartyPants' ) );
+	    $form->append( 'submit', 'save', _t( 'Save' ) );
+	    $form->out();
+	}
+    }
+
+    /**
+     * Filter Atom Feed
+     * @param SimpleXMLElement $feed_entry the Atom feed entry
+     * @param Post $post The post
+     * @return SimpleXMLElement the filtered Atom feed entry
+     */
+    public function action_atom_add_post( $feed_entry, $post )
+    {
+	// Only apply changes to unauthenticated viewers.  This allows markdown to be used in atompub clients too.
+	if ( ! User::identify()->loggedin ) {
+	    $feed_entry->content = markdown( $post->content );
+	}
+	return $feed_entry;
+    }
 }
 
 class MarkdownFormat extends Format
 {
-	public static function markdown( $content )
-	{
-		$smarty_enabled = Options::get( 'habarimarkdown__smarty') || false;
-		if ( $smarty_enabled ) {
-			return SmartyPants( Markdown ($content ) );
-		}
-		else {
-			return Markdown( $content );
-		}
+    // try and take over autop to prevent conflicts...
+    // there really should be a "remove" in Format!
+    public static function autop( $content )
+    {
+	return $content;
+    }
+
+    public static function markdown( $content )
+    {
+	$smarty_enabled = Options::get( 'habarimarkdown__smarty' ) || false;
+	if ( $smarty_enabled ) {
+	    return SmartyPants( Markdown ( $content ) );
 	}
+	else {
+	    return Markdown( $content );
+	}
+    }
 }
 
 ?>
