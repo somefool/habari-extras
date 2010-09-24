e5a0e7865fd36fd8e14265f2ebc96b43550c39c5
lifeisfoo <lifeisfoo@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-09-24 14:49:56 +0000

    Fixed wrong plugin version
    git-svn-id: http://svn.habariproject.org/habari-extras@3307 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/download-plugin/tags/0.6-0.1.1/download-plugin.plugin.php
+++ plugins/download-plugin/tags/0.6-0.1.1/download-plugin.plugin.php
@@ -3,7 +3,7 @@
 /**
  * DownloadPlugin Class
  *
- * This class provides functionality for downloading and installing
+ * This class provides  functionality for downloading and installing
  * plugins directly from a url.
  *
  * @todo Document methods
@@ -14,6 +14,19 @@
 class DownloadPlugin extends Plugin
 {
 	private $downloadplugin_pluginsPath;
+
+	function info()
+	{
+		return array (
+			'name' => 'Download Plugin',
+			'url' => 'http://wiki.lulug.org/prog/habari/download-plugin',
+			'author' => 'Alessandro Miliucci',
+			'authorurl' => 'http://forkwait.net/blog',
+			'version' => '0.1.1',
+			'description' => 'Download and install plugins',
+			'license' => 'Apache License 2.0',
+		);
+	}
 
 	public function formui_submit( FormUI $form )
 	{
@@ -61,15 +74,17 @@ class DownloadPlugin extends Plugin
 	public function action_plugin_ui( $plugin_id, $action )
 	{	
 		if ( $this->plugin_id() == $plugin_id ){
-			$this->downloadplugin_pluginsPath = HABARI_PATH . '/user/plugins/';
-	  		$ui = new FormUI( 'Plugin download' );
+			$this->downloadplugin_pluginsPath = HABARI_PATH . '/system/plugins/';
+	  		$ui = new FormUI( 'Download Plugin' );
 			if(is_writable($this->downloadplugin_pluginsPath)){
-		  		$url = $ui->append( 'text', 'pluginurl', 'null:null', _t('Plugin URL:', 'plugin_locale') );
+				/* in 0.6 texts aren't resizable; search 
+				 * "what are you trying to do with formui?" 
+				 * in http://drunkenmonkey.org/irc/habari/2010-09-23
+				 */
+		  		$url = $ui->append( 'textarea', 'pluginurl', 'null:null', _t('Plugin URL:', 'plugin_locale') );
 		  		$ui->append('submit', 'Download', _t('Download', 'plugin_locale'));
 				$url->add_validator('url_validator', _t('The plugin_url field value must be a valid URL'));
 		  		$ui->on_success( array($this, 'formui_submit') );
-				//replace with width 100% when will be possible
-				$ui->pluginurl->size = 300;
 			}else{
 				$ui->append('static','disclaimer', _t( '<p><em><small>Plugins directory is not writable, check permissions and reload this page</small></em></p>') );
 			}
@@ -89,6 +104,7 @@ class DownloadPlugin extends Plugin
 	{
 	  Update::add( 'Download Plugin', '2641A2EE-C7D1-11DF-AB1B-7133DFD72085', $this->info->version ); 
 	}
+
 }
 
 ?>
