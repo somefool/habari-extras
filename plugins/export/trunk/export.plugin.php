2798ff01443de88c8bf9dbe08d8329361843ebd9
chrismeller <chrismeller@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-10-10 00:03:31 +0000

    Add the huge mass of export code, along with cron entry configs. There is a DOM-based implementation (at least the start of it) as well as the SimpleXML version that we're currently using. With SimpleXML you don't get CDATA blocks, which may pose a problem.
    git-svn-id: http://svn.habariproject.org/habari-extras@3349 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/export/trunk/export.plugin.php
+++ plugins/export/trunk/export.plugin.php
@@ -84,7 +84,7 @@
 						
 					case _t('Export'):
 						
-						$this->export();
+						$this->run();
 						
 						Utils::redirect( URL::get( 'admin', 'page=plugins' ) );
 						
@@ -113,19 +113,19 @@
 					break;
 					
 				case 'hourly':
-					CronTab::add_hourly_cron('export', array( $this, 'export' ));
+					CronTab::add_hourly_cron('export', array( $this, 'run' ));
 					break;
 					
 				case 'daily':
-					CronTab::add_daily_cron('export', array( $this, 'export' ));
+					CronTab::add_daily_cron('export', array( $this, 'run' ));
 					break;
 					
 				case 'weekly':
-					CronTab::add_weekly_cron('export', array( $this, 'export' ));
+					CronTab::add_weekly_cron('export', array( $this, 'run' ));
 					break;
 					
 				case 'monthly':
-					CronTab::add_monthly_cron('export', array( $this, 'export' ));
+					CronTab::add_monthly_cron('export', array( $this, 'run' ));
 					break;
 				
 			}
@@ -134,9 +134,213 @@
 			
 		}
 		
-		public function export ( ) {
+		public function run_dom ( ) {
 			
+			ob_end_clean();
 			
+			// test the cache
+			$cache = Cache::set('export_test', 'test');
+			
+			if ( $cache == null ) {
+				// we can't export!
+				EventLog::log( _t( 'Unable to write to the cache, export failed!' ), 'critical', 'export', 'export' );
+				
+				if ( User::identify() ) {
+					Session::error( _t( 'Unable to write to the cache, export failed!' ) );
+				}
+				
+				return false;
+			}
+			
+			$dom = new DOMDocument('1.0', 'utf-8');
+			$dom->formatOutput = true;
+			
+			// create the root blog element
+			$blog = $dom->appendChild( new DOMElement( 'blog' ) );
+			$blog->setAttributeNS( 'http://www.w3.org/2000/xmlns/', 'xmlns', 'http://localhost/blogml.xsd' );
+			$blog->setAttributeNS( 'http://www.w3.org/2000/xmlns/', 'xmlns:xs', 'http://www.w3.org/2001/XMLSchema' );
+			
+			$blog->setAttribute( 'root-url', Site::get_url( 'habari' ) );
+			$blog->setAttribute( 'date-created', HabariDateTime::date_create()->format('c') );
+			
+			$title = $dom->createElement( 'title', '' );
+			$title->setAttribute( 'type', 'text' );		// type attribute is optional
+			$title->appendChild( $dom->createCDATASection( Options::get( 'title' ) ) );
+			
+			$dom->appendChild( $title );
+			
+			$sub_title = $dom->createElement( 'sub-title', '' );
+			$sub_title->appendChild( $dom->createCDATASection( Options::get( 'tagline' ) ) );
+			
+			echo $dom->saveXML();
+			
+			die();
+			
+		}
+		
+		public function run ( ) {
+			
+			if ( !$this->test_cache() ) {
+				return false;
+			}
+			
+			$export = new SimpleXMLElement( '<?xml version="1.0" encoding="utf-8"?><blog xmlns="http://www.blogml.com/2006/09/BlogML" xmlns:xs="http://www.w3.org/2001/XMLSchema" />' );
+			$export->addAttribute( 'root-url', Site::get_url('habari') );
+			
+			$export->addChild( 'title', Options::get('title') )->addAttribute( 'type', 'text' );
+			$export->addChild( 'sub-title', Options::get('tagline') )->addAttribute( 'type', 'text' );
+			
+			// export all the blog's users
+			$this->export_users( $export );
+			
+			// export all the blog's options
+			$this->export_options( $export );
+			
+			// export all the blog's tags
+			$this->export_tags( $export );
+			
+			// export all the blog's posts
+			$this->export_posts( $export );
+			
+			EventLog::log( _t( 'Export completed!' ), 'info', 'export', 'export' );
+			
+			// clear out anything that may have been output before us and disable the buffer
+			ob_end_clean();
+			
+			// output the xml!
+			echo $export->asXML();
+			die();
+			
+		}
+		
+		private function test_cache ( ) {
+			
+			// test the cache
+			$cache = Cache::set('export_test', 'test');
+			
+			if ( $cache == null ) {
+				// we can't export!
+				EventLog::log( _t( 'Unable to write to the cache, export failed!' ), 'critical', 'export', 'export' );
+				
+				if ( User::identify() ) {
+					Session::error( _t( 'Unable to write to the cache, export failed!' ) );
+				}
+				
+				return false;
+			}
+			else {
+				
+				Cache::expire( 'export_test' );
+				return true;
+				
+			}
+			
+		}
+		
+		private function export_users( $export ) {
+			
+			$authors = $export->addChild( 'authors' );
+			
+			$users = Users::get();
+			foreach ( $users as $user ) {
+				
+				$author = $authors->addChild( 'author' );
+				$author->addAttribute( 'id', $user->id );
+				$author->addAttribute( 'approved', 'true' );
+				$author->addAttribute( 'email', $user->email );
+				$author->addChild( 'title', $user->displayname )->addAttribute( 'type', 'text' );
+				
+			}
+			
+		}
+		
+		private function export_options ( $export ) {
+			
+			$properties = $export->addChild( 'extended-properties' );
+			
+			$options = DB::get_results( 'select name, value, type from {options}' );
+			foreach ( $options as $option ) {
+				
+				$property = $properties->addChild( 'property' );
+				$property->addAttribute( 'name', $option->name );
+				$property->addAttribute( 'value', $option->value );
+				
+			}
+			
+		}
+		
+		private function export_tags ( $export ) {
+			
+			$categories = $export->addChild( 'categories' );
+			
+			$tags = Tags::get();
+			foreach ( $tags as $tag ) {
+				
+				$category = $categories->addChild( 'category' );
+				$category->addAttribute( 'id', $tag->id );
+				$category->addChild( 'title', $tag->tag_text )->addAttribute( 'type', 'text' );
+				
+			}
+			
+		}
+		
+		private function export_posts ( $export ) {
+			
+			$ps = $export->addChild( 'Posts' );
+			
+			$posts = Posts::get( array( 'limit' => null ) );
+			foreach ( $posts as $post ) {
+				
+				// create the post object
+				$p = $ps->addChild( 'post' );
+				
+				// add all the basic post info
+				$p->addAttribute( 'id', $post->id );
+				$p->addAttribute( 'date-created', $post->pubdate->format('c') );
+				$p->addAttribute( 'date-modified', $post->pubdate->format('c') );
+				$p->addAttribute( 'approved', $post->status == Post::status('published') ? 'true' : 'false' );
+				$p->addAttribute( 'post-url', $post->permalink );	// @todo this has to be relative!
+				$p->addAttribute( 'type', $post->content_type );
+				$p->addChild( 'title', $post->title );
+				$p->addChild( 'content', $post->content );
+				$p->addChild( 'post-name', $post->slug );
+				
+				// now add the post tags
+				$pt = $p->addChild( 'categories' );
+				
+				$tags = $post->tags;
+				foreach ( $tags as $t_term => $t_slug ) {
+					
+					$tag = Tag::get( $t_term );
+					
+					$pt->addChild( 'category' )->addAttribute( 'ref', $tag->id );
+					
+				}
+				
+				// now add the post comments
+				// @todo manually fetch unapproved comments for each post
+				// @todo add support for trackbacks from the pingback plugin?
+				$pc = $p->addChild( 'comments' );
+				
+				$comments = $post->comments;
+				foreach ( $comments as $comment ) {
+					
+					$c = $pc->addChild( 'comment' );
+					$c->addAttribute( 'id', $comment->id );
+					$c->addAttribute( 'date-created', $comment->date->format('c') );
+					$c->addAttribute( 'date-modified', $comment->date->format('c') );
+					$c->addAttribute( 'approved', $comment->status == Comment::STATUS_APPROVED ? 'true' : 'false' );
+					$c->addAttribute( 'user-name', $comment->name );
+					$c->addAttribute( 'user-url', $comment->url );
+					
+					$c->addChild( 'title' );
+					$c->addChild( 'content', $comment->content )->addAttribute( 'type', 'text' );
+					
+				}
+				
+				$p->addChild( 'authors' )->addChild( 'author' )->addAttribute( 'ref', $post->author->id );
+				
+			}
 			
 		}
 		
