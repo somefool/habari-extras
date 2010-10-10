39f4bdb4d22a6beb59538c93d437917047d9d1a8
chrismeller <chrismeller@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-10-10 00:03:15 +0000

    Add an ACL token to allow users to generate an export file and provide an export option in the plugin config UI.
    git-svn-id: http://svn.habariproject.org/habari-extras@3345 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/export/trunk/export.plugin.php
+++ plugins/export/trunk/export.plugin.php
@@ -12,7 +12,22 @@
 			
 			if ( Plugins::id_from_file( $file ) == Plugins::id_from_file( __FILE__ ) ) {
 				
+				ACL::create_token( 'export', 'Export the Habari database.', 'export' );
 				
+			}
+			
+		}
+		
+		public function filter_plugin_config ( $action, $plugin_id ) {
+			
+			if ( $plugin_id == $this->plugin_id() ) {
+				
+				$actions[] = _t('Configure');
+				
+				// only users with the proper permission should be allowed to export
+				if ( User::identify()->can('export now') ) {
+					$actions[] = _t('Export');
+				}
 				
 			}
 			
