d1a80d4ed000596988a6e80d3cb75c6cbae3a89b
mikelietz <mikelietz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-10-12 15:01:57 +0000

    plugin:polls Tag for 0.6. It's not quite complete, or entirely functional, but by calling $theme->poll() the most recent one will appear. This will need a lot of work in trunk.
    git-svn-id: http://svn.habariproject.org/habari-extras@3371 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/polls/tags/0.6-0.52/polls.plugin.php
@@ -0,0 +1,124 @@
+<?php
+
+class Polls extends Plugin {
+	function info() {
+		return array(
+			'name' => 'Poll',
+			'version' => '0.52',
+			'url' => 'http://www.thebigsqueak.com/pluginspolls',
+			'author' => 'Bigsqueak Studios',
+			'authorurl' => 'http://www.thebigsqueak.com',
+			'licence' => 'Creative Commons Attribution-Share Alike 3.0',
+			'description' => 'A flexible polling plugin for habari'
+		);
+	}
+	
+	//initilization
+	public function action_init() {
+		Post::add_new_type('poll');
+		$this->add_template('widget', dirname(__FILE__) . '/widget.php');
+		$this->add_template('poll.single', dirname(__FILE__) . '/poll.single.php');
+		Stack::add( 'template_header_javascript', Site::get_url('scripts') . '/jquery.js', 'jquery' );
+		Stack::add( 'template_stylesheet', array(URL::get_from_filesystem(__FILE__) . '/widget.css', 'screen'), 'pollwigitcss');
+		
+	} 	
+	
+	//deactivate
+	public function remove_template() {
+		Post::deactivate_post_type('poll');
+		$this->remove_template('widget', dirname(__FILE__) . '/widget.php');
+		Stack::remove( 'template_header_javascript', Site::get_url('scripts') . '/jquery.js', 'jquery' );
+		Stack::remove( 'template_stylesheet', array(URL::get_from_filesystem(__FILE__) . '/widget.css', 'screen'), 'pollwigitcss');
+		$this->remove_template('poll.single', dirname(__FILE__) . '/poll.single.php');
+	}
+	
+	public	function action_ajax_ajaxpoll() {
+		$pollid = $_GET['pollid'];
+		$vote = $_GET['result'];
+		$post = Post::get(array('content_type'=>Post::type('poll'), 'id'=>$pollid));
+		
+		if ($vote != 'null') {
+			$array = $post->info->r;
+			$temp = $post->info->r;
+			$temp[$vote]++;
+			$post->info->r = $temp;
+			
+			Session::add_to_set('votes', $post->id);
+		}
+		$post->update();
+		
+		?>
+	<ul id="poll_results">
+		<?php
+		$length = 200;
+		$post->info->r;
+		$max = max($post->info->r);
+		
+		for($n=1; $n<sizeof($post->info->r); $n++) { ?>
+			<label > <?php echo $post->info->entry[$n] ."(". $post->info->r[$n]. ")"; ?> <li style='width: <?php echo $length*($post->info->r[$n]/$max); ?>px'>  </li> </label>
+		
+		<?php }; ?>
+
+	</ul>
+		<?php
+			
+	}
+	
+	//displaying the poll by this
+	//WIGET
+	public function theme_poll($theme, $pollid = null) {
+		
+		include 'widget.php';
+	}
+	
+			
+	public function action_form_publish($form, $post) {
+	if($post->content_type == Post::type('poll')) {
+		if ($form->silos) 
+		$form-> silos->remove();
+		if ($form->clearbutton)
+		$form-> clearbutton->remove();
+		$form->title->caption = "Poll Name";
+		$form->append('text','entry1', 'null:null', 'entry 1','admincontrol_text');
+		$form->move_after($form->entry1, $form->title);
+		$form->entry1->value = $post->info->entry[1];
+		$form->append('text','entry2', 'null:null', 'entry 2','admincontrol_text');
+		$form->move_after($form->entry2, $form->entry1);
+		$form->entry2->value = $post->info->entry[2];
+		$form->append('text','entry3', 'null:null', 'entry 3','admincontrol_text');
+		$form->move_after($form->entry3, $form->entry2);
+		$form->entry3->value = $post->info->entry[3];
+		$form->append('text','entry4', 'null:null', 'entry 4','admincontrol_text');
+		$form->move_after($form->entry4, $form->entry3);
+		$form->entry4->value = $post->info->entry[4];
+		$form->append('text','entry5', 'null:null', 'entry 5','admincontrol_text');
+		$form->move_after($form->entry5, $form->entry4);
+		$form->entry5->value = $post->info->entry[5];
+	   }
+	}
+	
+	public function action_publish_post($post, $form) {
+		if ($post->content_type == Post::type('poll')) {
+			$this->action_form_publish($form, $post);
+			$entry= array();
+			$entry[1] = $form->entry1->value;
+			$entry[2] = $form->entry2->value;
+			$entry[3] = $form->entry3->value;
+			$entry[4] = $form->entry4->value;
+			$entry[5] = $form->entry5->value;
+			$post->info->entry= $entry;
+			$n = 1;
+			$r = array();
+			if (!$post->info->r) {
+				foreach ( $post->info->entry as $result):
+					$r[$n] = 0;
+					$n++;
+					endforeach;
+					$post->info->r = $r;
+			}			
+		}
+	}
+	
+
+}
+?>
---
+++ plugins/polls/tags/0.6-0.52/widget.php
@@ -0,0 +1,127 @@
+
+
+<?php
+//Getting result of of theme function
+if ($pollid == null) { 
+$poll = Post::get(array('content_type' => Post::type('Poll')));
+} elseif (is_int($pollid) === true) {
+$poll = Post::get(array('content_type'=>Post::type('Poll'), 'id'=>$pollid));
+}
+
+$form = new FormUi(strtolower( get_class( $this ) ) );
+$array = array();
+$form->append('radio', 'entry', 'null:null', 'poll this');
+
+
+	$poll->info->entry;
+	$array = $poll->info->entry;
+	$form->entry->options = $array;
+?> 
+
+
+<div id="main_poll">
+	<span id="polltitle"> <a href="<?php echo $poll->permalink ?>"> <b> <?php echo $poll->title; ?> </b> </a></span>
+
+	<?php if (!Session::get_set('votes', false)) { ?>
+		<div id="vote">
+		<?php
+		$form->out();
+		?>
+
+		</div>
+		<a id="votesubmitt"> Vote </a>
+	<?php } ?>
+	<div id="contentLoading" style=" display: none;"alt="Loading, please wait">
+		Loading...
+	</div>
+	<div id="results" >
+
+	</div>
+
+	<?php if (!Session::get_set('votes', false)) { ?>
+		<a submitt="<?php if (Session::get_set('votes', false)) { echo 'off'; } ?>"> <span id="veiw_results"> Veiw resutls </span> </a>
+	<?php } ?>
+
+<script type="text/javascript">
+
+
+$('#veiw_results').click(function() {
+	if($('#veiw_results').text() == "go back to poll") {
+		$('#votesubmitt').show();
+		$('#veiw_results').text("Veiw resutls");
+		$('#vote').css({display: "block"});
+		$('#results').css({display: "none"});
+	} else {
+		//results
+		$('#votesubmitt').hide();
+		$('#veiw_results').text("go back to poll")
+		$('#vote').css({display: "none"});
+		$('#results').css({display: "block"});
+		
+		$.get('<?php echo URL::get('ajax', array('context' => 'ajaxpoll')); ?>', {result: null, pollid: <?php echo $poll->id?>}, function(data) {
+		
+		$('#results').html(data);
+		
+		})
+	}
+
+}) 
+
+$('#votesubmitt').click(function() {
+	value = "entry"
+	value = $("input[type = 'radio']:checked").val();
+	value = value.replace('entry','')
+	value = parseFloat(value);
+
+	$.get('<?php echo URL::get('ajax', array('context' => 'ajaxpoll')); ?>', {result: value, pollid: <?php echo $poll->id ?> }, function(data) {
+		$('#results').html(data);
+		$('#results').show()
+		$('#vote').css({display: "none"});
+		lockdown()
+		})
+
+})
+	
+
+
+function lockdown() {
+$('#veiw_results').hide();
+$('#votesubmitt').hide();
+$('#vote').hide();
+}
+
+function getresults() {
+	$.get('<?php echo URL::get('ajax', array('context' => 'ajaxpoll')); ?>', {result: null, pollid: <?php echo $poll->id ?>}, function(data) {
+		
+		$('#results').html(data);
+		
+		})
+
+}
+
+
+$().ajaxSend(function(r,s){
+$("#contentLoading").show();
+});
+
+
+
+
+ $().ajaxStop(function(r,s){  
+ $("#contentLoading").fadeOut("fast");  
+ });  
+
+
+
+</script>
+<?php if (Session::get_set('votes', false)) { ?>
+	
+	<script type="text/javascript">
+
+	lockdown()
+	getresults()
+
+	</script>
+<?php } ?>
+
+</div>
