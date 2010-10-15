08669aea3ea3fc1267fdaf6a27b8f6101481dd17
mikelietz <mikelietz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-10-15 18:43:46 +0000

    plugin:draftdashmodule Update to use HabariDateTime::friendly();
    git-svn-id: http://svn.habariproject.org/habari-extras@3383 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/draftdashmodule/trunk/dash_latestdrafts.php
+++ plugins/draftdashmodule/trunk/dash_latestdrafts.php
@@ -2,7 +2,7 @@
 
 		<?php foreach((array)$recent_posts as $post): ?>
 		<li class="item clear">
-			<span class="date pct20 minor"><?php echo DraftDashModule::nice_time( $post->pubdate ); ?></span>
+			<span class="date pct20 minor"><?php echo $post->pubdate->friendly; ?></span>
 			<span class="title pct80"><a href="<?php URL::out('admin', 'page=publish&id=' . $post->id); ?>" title="<?php
 				$post_title = ( $post->title ? $post->title : _t( "[untitled post id: %d]" , array( $post->id ),'draftdashmodule' ) );
 			printf( _t('Edit \'%s\''), $post_title ); ?>"><?php echo $post_title; ?></a></span>
