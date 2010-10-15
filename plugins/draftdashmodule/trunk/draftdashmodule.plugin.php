7bf99fc2c0186fb2bbb32ce1d9bb1e64e574ab60
mikelietz <mikelietz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-10-15 18:55:10 +0000

    plugin:draftdashmodule Took out too much code before.
    git-svn-id: http://svn.habariproject.org/habari-extras@3384 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/draftdashmodule/trunk/draftdashmodule.plugin.php
+++ plugins/draftdashmodule/trunk/draftdashmodule.plugin.php
@@ -42,6 +42,21 @@ class DraftDashModule extends Plugin
 
 		return $modules;
 	}
+	
+	/**
+	 * filter_dash_module_latest_entries
+	 * Gets the latest entries module
+	 * @param string $module_id
+	 * @return string The contents of the module
+	 */
+	public function filter_dash_module_latest_drafts( $module, $module_id, $theme )
+	{
+		$theme->recent_posts = Posts::get( array( 'status' => 'draft', 'limit' => 8, 'type' => Post::type( 'entry' ), 'user_id' => User::identify()->id ) );
+		
+		$module[ 'title' ] = ( User::identify()->can( 'manage_entries' ) ? '<a href="' . Utils::htmlspecialchars( URL::get( 'admin', array( 'page' => 'posts', 'type' => Post::type( 'entry' ), 'status' => Post::status( 'draft' ), 'user_id' => User::identify()->id ) ) ) . '">' . _t( 'Latest Drafts', 'draftdashmodule' ) . '</a>' : _t( 'Latest Drafts', 'draftdashmodule' ) );
+		$module[ 'content' ] = $theme->fetch( 'dash_latestdrafts' );
+		return $module;
+	}
 
 	/**
 	 * action_update_check
