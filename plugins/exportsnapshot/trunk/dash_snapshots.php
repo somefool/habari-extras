fafaf6d7f4fdd532ac1bcd1aac80dc85f470a16d
chrismeller <chrismeller@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-10-18 01:25:14 +0000

    Add the size to the dashboard.
    git-svn-id: http://svn.habariproject.org/habari-extras@3411 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/exportsnapshot/trunk/dash_snapshots.php
+++ plugins/exportsnapshot/trunk/dash_snapshots.php
@@ -8,7 +8,8 @@
 			
 				<li class="item clear">
 				
-					<span class="date pct20 minor"><?php echo $snapshot->date; ?></span>
+					<span class="date pct20 minor"><?php echo $snapshot->date->format('M d'); ?></span>
+					<span class="size pct80"><?php echo Utils::human_size( $snapshot->size ); ?></span>
 				
 				</li>
 			
