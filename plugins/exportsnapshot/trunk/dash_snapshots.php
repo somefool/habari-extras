7b2b9f6d29ea63412ffe42ea229f4c89bb3d7498
chrismeller <chrismeller@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-10-19 01:41:21 +0000

    Slight tweaks to the dashboard widget. Add meaningless non-functional links and tweak the spacing.
    git-svn-id: http://svn.habariproject.org/habari-extras@3422 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/exportsnapshot/trunk/dash_snapshots.php
+++ plugins/exportsnapshot/trunk/dash_snapshots.php
@@ -8,8 +8,10 @@
 			
 				<li class="item clear">
 				
-					<span class="date pct20 minor"><?php echo $snapshot->date->format('M d'); ?></span>
-					<span class="size pct80"><?php echo Utils::human_size( $snapshot->size ); ?></span>
+					<span class="date pct20 minor"><?php echo $snapshot->date->friendly; ?></span>
+					<span class="size pct55 minor"><?php echo Utils::human_size( $snapshot->size ); ?></span>
+					<span class="delete pct10 minor"><a href="#">Delete</a></span>
+					<span class="download pct15 minor"><a href="#">Download</a></span>
 				
 				</li>
 			
