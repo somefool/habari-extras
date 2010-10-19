a8057df5c5e105a5e2c1218967fb15893b87b548
chrismeller <chrismeller@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-10-19 04:10:11 +0000

    Git has screwed up somewhere in here, but this should wrap up the Export Snapshot code with the exception of loading snapshots, which relies upon the as yet unfinished Importer.
    git-svn-id: http://svn.habariproject.org/habari-extras@3428 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/exportsnapshot/trunk/dash_snapshots.php
+++ plugins/exportsnapshot/trunk/dash_snapshots.php
@@ -9,9 +9,29 @@
 				<li class="item clear">
 				
 					<span class="date pct20 minor"><?php echo $snapshot->date->friendly; ?></span>
-					<span class="size pct55 minor"><?php echo Utils::human_size( $snapshot->size ); ?></span>
-					<span class="delete pct10 minor"><a href="#">Delete</a></span>
-					<span class="download pct15 minor"><a href="#">Download</a></span>
+					<span class="size pct55 minor">
+						<?php echo _t( '%s snapshot', array( ucfirst( $snapshot->type ) ) ) . ': ' . Utils::human_size( $snapshot->size ); ?>
+					</span>
+					<span class="delete pct10 minor">
+						<?php 
+						
+							if ( User::identify()->can( 'snapshot', 'delete' ) ) {
+								$url = URL::get( 'snapshot_delete', array( 'ts' => $snapshot->date->int ) );
+								echo '<a href="' . $url . '">' . _t( 'Delete', 'exportsnapshot' ) . '</a>';
+							}
+						
+						?>
+					</span>
+					<span class="download pct15 minor">
+						<?php 
+						
+							if ( User::identify()->can( 'snapshot', 'read' ) ) {
+								$url = URL::get( 'snapshot_download', array( 'ts' => $snapshot->date->int ) );
+								echo '<a href="' . $url . '">' . _t( 'Download', 'exportsnapshot' ) . '</a>';
+							}
+						
+						?>
+					</span>
 				
 				</li>
 			
--- plugins/exportsnapshot/trunk/exportsnapshot.plugin.php
+++ plugins/exportsnapshot/trunk/exportsnapshot.plugin.php
@@ -47,7 +47,7 @@
 			
 			if ( User::identify()->can( 'snapshot', 'read' ) ) {
 				
-				$modules[] = _t('Snapshots');
+				$modules[] = _t('Latest Snapshots');
 				
 				$this->add_template( 'dash_snapshots', dirname( __FILE__ ) . '/dash_snapshots.php' );
 				
@@ -57,7 +57,7 @@
 			
 		}
 		
-		public function filter_dash_module_snapshots ( $module, $module_id, $theme ) {
+		public function filter_dash_module_latest_snapshots ( $module, $module_id, $theme ) {
 			
 			$snapshots = Options::get( 'exportsnapshot__snapshots', array() );
 			
@@ -70,17 +70,18 @@
 			$snapshots = array_slice( $snapshots, 0, $count, true );
 			
 			$s = array();
-			foreach ( $snapshots as $ts => $size ) {
+			foreach ( $snapshots as $ts => $snapshot ) {
 				$t = new stdClass();
 				$t->date = HabariDateTime::date_create( $ts );
-				$t->size = $size;
+				$t->size = $snapshot['size'];
+				$t->type = $snapshot['type'];
 				
 				$s[] = $t;
 			}
 			
 			$theme->snapshots = $s;
 			
-			$module['title'] = _t('Snapshots');
+			$module['title'] = _t('Latest Snapshots');
 			$module['content'] = $theme->fetch('dash_snapshots');
 			
 			return $module;
@@ -133,7 +134,7 @@
 						
 					case _t('Take Snapshot'):
 						
-						self::run();
+						self::run( 'manual' );
 						Session::notice( _t('Snapshot saved!') );
 						
 						//CronTab::add_single_cron('snapshot_single', array( 'ExportSnapshot', 'run' ), HabariDateTime::date_create(), 'Run a single snapshot.' );
@@ -167,19 +168,19 @@
 					break;
 					
 				case 'hourly':
-					CronTab::add_hourly_cron('snapshot', array( 'ExportSnapshot', 'run' ));
+					CronTab::add_hourly_cron('snapshot', array( 'ExportSnapshot', 'run' ), _t('Hourly Export Snapshot'));
 					break;
 					
 				case 'daily':
-					CronTab::add_daily_cron('snapshot', array( 'ExportSnapshot', 'run' ));
+					CronTab::add_daily_cron('snapshot', array( 'ExportSnapshot', 'run' ), _t('Daily Export Snapshot'));
 					break;
 					
 				case 'weekly':
-					CronTab::add_weekly_cron('snapshot', array( 'ExportSnapshot', 'run' ));
+					CronTab::add_weekly_cron('snapshot', array( 'ExportSnapshot', 'run' ), _t('Weekly Export Snapshot'));
 					break;
 					
 				case 'monthly':
-					CronTab::add_monthly_cron('snapshot', array( 'ExportSnapshot', 'run' ));
+					CronTab::add_monthly_cron('snapshot', array( 'ExportSnapshot', 'run' ), _t('Monthly Export Snapshot'));
 					break;
 				
 			}
@@ -195,7 +196,7 @@
 			
 			if ( $cache == null ) {
 				// we can't export!
-				EventLog::log( _t( 'Unable to write to the cache, export failed!' ), 'critical', 'export', 'export' );
+				EventLog::log( _t( 'Unable to write to the cache, export failed!' ), 'critical', 'cache', 'ExportSnapshot' );
 				
 				// if a user is running manually, also give them a session error notice
 				if ( User::identify() ) {
@@ -213,7 +214,7 @@
 			
 		}
 		
-		public static function run ( ) {
+		public static function run ( $type = 'cron' ) {
 			
 			Plugins::act('exportsnapshot_run_before');
 			
@@ -225,14 +226,14 @@
 			$export = new Export();
 			$xml = $export->run();
 			
-			EventLog::log( _t( 'Export Snapshot completed!' ), 'info', 'export', 'snapshot' );
+			EventLog::log( _t( 'Export Snapshot completed!' ), 'info', 'snapshot', 'ExportSnapshot', $type );
 			
 			Plugins::act('exportsnapshot_run_after');
 			
 			$xml = Plugins::filter('exportsnapshot_contents', $xml);
 			
 			// save the snapshot
-			$save_result = self::save( $xml );
+			$save_result = self::save( $xml, $type );
 			
 			// cleanup old snapshots
 			$clean_result = self::cleanup();
@@ -246,7 +247,7 @@
 			
 		}
 		
-		private static function save ( $xml ) {
+		private static function save ( $xml, $type ) {
 			
 			$timestamp = HabariDateTime::date_create('now');
 			
@@ -255,7 +256,11 @@
 			if ( $result ) {
 			
 				$snapshots = Options::get( 'exportsnapshot__snapshots', array() );
-				$snapshots[ $timestamp->int ] = MultiByte::strlen( $xml );
+				$snapshots[ $timestamp->int ] = array(
+					'size' => MultiByte::strlen( $xml ),
+					'type' => $type,
+					'ts' => $timestamp->int,
+				);
 			
 				Options::set( 'exportsnapshot__snapshots', $snapshots );
 				
@@ -269,9 +274,7 @@
 		}
 		
 		private static function cleanup ( ) {
-			
-			return true;
-			
+						
 			// the limit on the number of snapshots to retain
 			$max_snapshots = Options::get( 'exportsnapshot__max_snapshots' );
 			
@@ -280,21 +283,23 @@
 			// they should be in timestamp order, but make sure
 			ksort( $snapshots );
 			
-			if ( $max_snapshots != null ) {
+			if ( $max_snapshots !== null && count( $snapshots ) > $max_snapshots ) {
 				
 				// get the oldest snapshots - we need to dump these
-				$old = array_slice( $snapshots, 0, $max_snapshots, true );
-				
-				// get the newest snapshots - these are the ones we'll save
-				$snapshots = array_slice( $snapshots, $max_snapshots, null, true );
+				$old = array_slice( $snapshots, 0, count( $snapshots ) - $max_snapshots, true );
 				
 				// if we've got things to clean up
 				if ( !empty( $old ) ) {
 					
-					foreach ( $old as $ts => $size ) {
+					foreach ( $old as $ts => $snapshot ) {
 						// expire the entries we don't want to keep any longer
 						Cache::expire( 'exportsnapshot__' . $ts );
+						
+						// unset the snapshot directory entry
+						unset( $snapshots[ $ts ] );
 					}
+					
+					 EventLog::log( _t( 'Purged %d exports.', array( count( $old ) ) ), 'debug', 'cleanup', 'ExportSnapshot' );
 					
 				}
 				
@@ -304,6 +309,112 @@
 			Options::set( 'exportsnapshot__snapshots', $snapshots );
 			
 			return true;
+			
+		}
+		
+		public function filter_rewrite_rules ( $rules ) {
+			
+			$rule = new RewriteRule( array(
+				'name' => 'snapshot_download',
+				'parse_regex' => '#^admin/snapshot/download/(?P<ts>[0-9.]+)/?$#i',
+				'build_str' => 'admin/snapshot/download/{$ts}',
+				'handler' => 'adminhandler',
+				'action' => 'snapshot_download',
+				'priority' => 1,
+				'is_active' => 1,
+				'rule_class' => RewriteRule::RULE_PLUGIN,
+				'description' => 'Download an Export Snapshot',
+			) );
+
+			$rules['snapshot_download'] = $rule;
+			
+			$rule = new RewriteRule( array(
+				'name' => 'snapshot_delete',
+				'parse_regex' => '#^admin/snapshot/delete/(?P<ts>[0-9.]+)/?$#i',
+				'build_str' => 'admin/snapshot/delete/{$ts}',
+				'handler' => 'adminhandler',
+				'action' => 'snapshot_delete',
+				'priority' => 1,
+				'is_active' => 1,
+				'rule_class' => RewriteRule::RULE_PLUGIN,
+				'description' => 'Delete an Export Snapshot',
+			) );
+			
+			$rules['snapshot_delete'] = $rule;
+			
+			return $rules;
+			
+		}
+		
+		public function action_handler_snapshot_download ( $handler_vars ) {
+			
+			if ( !User::identify()->can( 'snapshot', 'read' ) ) {
+				Utils::redirect( URL::get( 'admin', array( 'page' => 'unauthorized' ) ) );
+			}
+			
+			$timestamp = Controller::get_var( 'ts' );
+			
+			$snapshots = Options::get( 'exportsnapshot__snapshots', array() );
+			
+			if ( !isset( $snapshots[ $timestamp ] ) ) {
+				die('Unknown snapshot!');
+			}
+			
+			// fetch the snapshot from the cache
+			$snapshot = Cache::get( 'exportsnapshot__' . $timestamp );
+			
+			if ( $snapshot ) {
+				$this->download( $snapshot );
+			}
+			
+		}
+		
+		public function action_handler_snapshot_delete ( $handler_vars ) {
+			
+			if ( !User::identify()->can( 'snapshot', 'delete' ) ) {
+				Utils::redirect( URL::get( 'admin', array( 'page' => 'unauthorized' ) ) );
+			}
+			
+			$timestamp = Controller::get_var( 'ts' );
+			
+			$snapshots = Options::get( 'exportsnapshot__snapshots', array() );
+			
+			if ( !isset( $snapshots[ $timestamp ] ) ) {
+				die('Unknown snapshot!');
+			}
+			
+			// expire the snapshot in the cache
+			Cache::expire( 'exportsnapshot__' . $timestamp );
+			
+			// remove it from the list
+			unset( $snapshots[ $timestamp ] );
+			
+			// write a log event
+			EventLog::log( _t( 'Export Snapshot deleted!' ), 'info', 'delete', 'ExportSnapshot' );
+			
+			// save the list
+			Options::set( 'exportsnapshot__snapshots', $snapshots );
+			
+			// and redirect back to the dashboard
+			Utils::redirect( URL::get( 'admin' ) );
+			
+		}
+		
+		private function download ( $xml ) {
+			
+			$timestamp = HabariDateTime::date_create('now')->format('YmdHis');
+			
+			$filename = 'habari_' . $timestamp . '.xml';
+			
+			// clear out anything that may have been output before us and disable the buffer
+			ob_end_clean();
+			
+			header('Content-Type: text/xml');
+			header('Content-disposition: attachment; filename=' . $filename);
+			
+			echo $xml;
+			
+			die();
 			
 		}
 		
