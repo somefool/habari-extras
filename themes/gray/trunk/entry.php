ff38715f762d4422c2131f5a206df4e249b281b0
ringmaster <ringmaster@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-10-21 12:20:19 +0000

    Do not display the comment form if the admin setting disallows it.
    git-svn-id: http://svn.habariproject.org/habari-extras@3441 f4a90942-1786-4aa1-b092-57c31935ddc3

--- themes/gray/trunk/entry.php
+++ themes/gray/trunk/entry.php
@@ -2,20 +2,6 @@
 	<div class="entry-head">
 		<h2 class="entry-title"><a href="<?php echo $content->permalink; ?>" title="<?php echo $content->title; ?>"><?php echo $content->title_out; ?></a></h2>
 
-		<?php if($ads): ?>
-<script type="text/javascript"><!--
-google_ad_client = "pub-3016948213890281";
-/* 468x60 resurrection */
-google_ad_slot = "9982410320";
-google_ad_width = 468;
-google_ad_height = 60;
-//-->
-</script>
-<script type="text/javascript"
-src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>
-		<?php $ads = false; endif; ?>
-
 		<div class="entry-meta">
 			<span class="chronodata published"><?php echo $content->pubdate_ago; ?></span> &middot; 
 			<span class="commentslink"><a href="<?php echo $content->permalink; ?>#comments" title="<?php _e('Comments on this post', 'resurrection'); ?>"><?php printf(_n( '%d Comment', '%d Comments', $content->comments->approved->count, 'resurrection' ), $content->comments->approved->count); ?></a></span>
@@ -54,6 +40,7 @@ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 	<li><b><?php echo $othername; ?>:</b> <a href="<?php echo $other->permalink; ?>"><?php echo htmlspecialchars($other->title); ?></a></li>
 	<?php endforeach; ?>
 	</ul>
+	<?php if(!$post->info->comments_disabled || $content->comments->moderated->comments->count > 0): ?>
 	<h3 id="comments">Comments</h3>
 	<ol class="entry-comments">
 	<?php foreach($content->comments->moderated->comments as $comment): ?>
@@ -62,7 +49,10 @@ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 		</li>
 	<?php endforeach; ?>
 	</ol>
+	<?php endif; ?>
+	<?php if(!$post->info->comments_disabled): ?>
 	<?php $post->comment_form()->out();	?>
 	<?php endif; ?>
-	<?php //$theme->comment_form($content/*, 'form.comment'*/); ?>
+	<?php endif; ?>
+
 </div>
