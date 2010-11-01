4f05c0a5f599d862be18ad60e85d8ada3a0873ab
MattRead <MattRead@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-11-01 16:16:45 +0000

    add lp sync script
    git-svn-id: http://svn.habariproject.org/habari-extras@3458 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ locales/lp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# setup tmp files from tar.gz's
+bzr update bzr
+
+# check fo .po
+for d in `ls bzr/system/locale/*.po`;
+do
+    if [[ $d =~ bzr/system/locale/(.*).po ]]
+    then
+        if [ ! -d ${BASH_REMATCH[1]} ]
+        then
+            mkdir  ${BASH_REMATCH[1]}
+            mkdir ${BASH_REMATCH[1]}/trunk
+            mkdir ${BASH_REMATCH[1]}/trunk/LC_MESSAGES
+            mkdir ${BASH_REMATCH[1]}/trunk/dist
+            mkdir ${BASH_REMATCH[1]}/tags
+            mkdir ${BASH_REMATCH[1]}/braches
+        fi
+        echo "moving  ${BASH_REMATCH[1]}.po"
+        mv bzr/system/locale/${BASH_REMATCH[1]}.po ${BASH_REMATCH[1]}/trunk/LC_MESSAGES/habari.po
+        echo "generating mo file"
+        msgfmt -o ${BASH_REMATCH[1]}/trunk/LC_MESSAGES/habari.mo ${BASH_REMATCH[1]}/trunk/LC_MESSAGES/habari.po
+        svn add -q ${BASH_REMATCH[1]} ${BASH_REMATCH[1]}/trunk/LC_MESSAGES/habari.po ${BASH_REMATCH[1]}/trunk/LC_MESSAGES/habari.mo
+    fi
+done
+
+# remove tmp files
+rm -rf tmp
+
+#svn ci -m"launchpad sync"
+
+echo "done!"
+
