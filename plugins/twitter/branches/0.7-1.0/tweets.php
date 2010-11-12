c269d263294e3d1370646f5ac786beecc917f7d5
lildude <lildude@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-11-12 16:04:39 +0000

    Plugin:Twitter - removed unnecessary 0.7 branches.
    git-svn-id: http://svn.habariproject.org/habari-extras@3489 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/twitter/branches/0.7-1.0/tweets.php
+++
@@ -1,7 +0,0 @@
-<!-- This file can be copied and modified in a theme directory -->
-
-<div id="twitterbox">
- <img src="<?php echo htmlspecialchars( $tweet_image_url ); ?>" alt="<?php echo urlencode( Options::get( 'twitter__username' )); ?>">
- <p><?php echo $tweet_text . ' @ ' . $tweet_time; ?></p>
-<p><small>via <a href="http://twitter.com/<?php echo urlencode( Options::get( 'twitter__username' )); ?>">Twitter</a></small></p>
- </div>
