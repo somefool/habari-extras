34283cef6e1fd05d611792ea0629805ec82175ce
chrismeller <chrismeller@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-11-23 05:11:11 +0000

    Update the Beacon Test plugin to register a guid for a test them as well as the test plugin. Bump the version to 0.2.
    git-svn-id: http://svn.habariproject.org/habari-extras@3535 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/beacontest/trunk/beacontest.plugin.php
+++ plugins/beacontest/trunk/beacontest.plugin.php
@@ -2,7 +2,15 @@
 
 	class BeaconTest extends Plugin {
 		
-		
+		public function action_update_check ( ) {
+			
+			// add an extra GUID, in addition to the one in our .xml file to pretend a theme was registered
+			Update::add( 'Test', '10cbaf18-bdd2-48e1-b0d3-e1853a4c649d', '3.0.2' );
+			
+			// add an extra GUID, in addition to the one in our .xml file to pretend a plugin was registered
+			Update::add( 'Test', '7a0313be-d8e3-11d1-8314-0800200c9a66', '0.9' );
+			
+		}
 		
 	}
 	
