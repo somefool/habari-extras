705c8e0fc880c511a77786f297ae9d0f5b4d4bf9
chrismeller <chrismeller@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-11-27 20:44:51 +0000

    Move the GUID into the XML info file.
    git-svn-id: http://svn.habariproject.org/habari-extras@3545 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/export/trunk/export.plugin.php
+++ plugins/export/trunk/export.plugin.php
@@ -2,12 +2,6 @@
 
 	class Export extends Plugin {
 		
-		public function action_update_check ( ) {
-			
-			Update::add( 'Export', '41f7ab69-dddd-4308-b31e-92f3d1270123', $this->info->version );
-			
-		}
-		
 		public function action_plugin_activation ( $file = '' ) {
 			
 			if ( Plugins::id_from_file( $file ) == Plugins::id_from_file( __FILE__ ) ) {
