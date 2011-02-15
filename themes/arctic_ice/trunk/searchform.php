ea31c887ba9a5e8749ef2d11b54d1dfe922693c5
rickc <rickc@f4a90942-1786-4aa1-b092-57c31935ddc3>
2011-02-15 14:32:52 +0000

    Theme: Arctic Ice. Get rid of what I think is the last bit of non-HiEngine code in the templates.
    git-svn-id: http://svn.habariproject.org/habari-extras@3734 f4a90942-1786-4aa1-b092-57c31935ddc3

--- themes/arctic_ice/trunk/searchform.php
+++ themes/arctic_ice/trunk/searchform.php
@@ -1,7 +1,7 @@
 <!-- searchform -->
 <?php Plugins::act( 'theme_searchform_before' ); ?>
-<form method="get" id="searchform" action="{hi:url:display_search}" accept-charset="UTF-8" >
-	<input type="text" id="s" name="criteria" value="<?php if ( isset( $criteria ) ) { echo htmlentities($criteria, ENT_COMPAT, 'UTF-8'); } ?>"> <input type="submit" id="searchsubmit" value="{hi:"Go!"}">
-</form>
+	<form method="get" id="searchform" action="{hi:url:display_search}">
+	<input type="text" id="s" name="criteria" value="{hi:?isset(criteria)}{hi:criteria}{/hi:?}"> <input type="submit" id="searchsubmit" value="{hi:"Go!"}">
+	</form>
 <?php Plugins::act( 'theme_searchform_after' ); ?>
 <!-- /searchform -->
