f6b6851151bbdf9f3206a551c6f8ec4becc6cea7
mikelietz <mikelietz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2011-02-26 03:36:28 +0000

    plugin:solvemedia Initial commit, sort of. It's not configurable (there are custom color schemes) and right now comments that don't pass captcha still get into moderation. Putting it here for people to check out and help improve.
    git-svn-id: http://svn.habariproject.org/habari-extras@3771 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/solvemedia/trunk/solvemedia.plugin.php
@@ -0,0 +1,69 @@
+<?php 
+
+/**
+ * http://portal.solvemedia.com/portal/help/pub/php/
+ **/
+class SolveMedia extends Plugin {
+
+	public function configure()
+	{
+		$ui = new FormUI( 'solvemedia' );
+	
+		$ckey = $ui->append( 'text', 'ckey', 'solvemedia__ckey', _t( 'Challenge Key (C-key)', 'solvemedia' ) );
+		$ckey->add_validator( 'validate_required' );
+		$vkey = $ui->append( 'text', 'vkey', 'solvemedia__vkey', _t( 'Verification Key (V-key)', 'solvemedia' ) );
+		$vkey->add_validator( 'validate_required' );
+		$hkey = $ui->append( 'text', 'hkey', 'solvemedia__hkey', _t( 'Authentication Hash Key (H-key)', 'solvemedia' ) );
+		$hkey->add_validator( 'validate_required' );
+							
+		$ui->append( 'submit', 'save', _t( 'Save', 'solvemedia' ) );
+		return $ui;
+	}
+
+	/**
+	 * Add captcha
+	 * ...
+	 * @return the form
+	 */
+	public function action_form_comment( $form, $context = 'public' ) {
+		
+		$form->append( 'static','solvemedia_captcha', solvemedia_get_html( Options::get( 'solvemedia__ckey' ) ) );
+		$form->move_before( $form->solvemedia_captcha, $form->cf_submit);
+		$form->cf_submit->tabindex = $form->cf_submit->tabindex + 1; // ideally we get the captcha tabindex between content and this.
+		return $form;
+	}
+
+	/**
+	 * Assess allowability of comment based on captcha success 
+	 * ...
+	 * @return boolean $allow Whether the comment should be allowed or not.
+	 **/
+	public function filter_comment_insert_allow( $allow, $comment ) {
+
+		if ( $allow ) {
+
+			$solvemedia_response = solvemedia_check_answer( 
+						Options::get( 'solvemedia__vkey' ), 
+						$_SERVER[ "REMOTE_ADDR" ],
+						$_POST[ "adcopy_challenge" ],
+						$_POST[ "adcopy_response" ],
+						Options::get( 'solvemedia__hkey' ) ); 
+			if ( $solvemedia_response->is_valid ) {
+				$comment->status = Comment::STATUS_APPROVED;
+				// rewrite this more like an sprintf
+				EventLog::log( _t( 'Comment by ', 'solvemedia' ) . $comment->name . _t( ' approved by SolveMedia captcha.', 'solvemedia' ), 'info', 'comment', 'SolveMedia' );
+			} else {
+				Session::add_to_set( 'comment', $comment->name, 'name' );
+				Session::add_to_set( 'comment', $comment->email, 'email' );
+				Session::add_to_set( 'comment', $comment->url, 'url' );
+				Session::add_to_set( 'comment', $comment->content, 'content' );
+				Session::error( _t( 'Your CAPTCHA attempt did not succeed: ', 'solvemedia' ) . $solvemedia_response->error );
+			}
+		}
+		return $allow;
+	}
+
+}
+require_once( "solvemedialib.php" ); //include the Solve Media library
+
+?>
