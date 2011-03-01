ad752484fc6064b0be42714d406c6c73ba4947eb
bcse <bcse@f4a90942-1786-4aa1-b092-57c31935ddc3>
2011-03-01 05:47:27 +0000

    plugin:flickrfeed 1. Update to Plugin Block. Fixed #266. 2. It's make more sense to put config in block instead of in theme, so Theme Function methods are removed. 3. Bump version for major API changes. 4. Refactoring: Extract methods from load_feed() 5. Coding Standards: Whitespace changes.
    git-svn-id: http://svn.habariproject.org/habari-extras@3788 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/flickrfeed/trunk/block.flickrfeed.php
+++ plugins/flickrfeed/trunk/block.flickrfeed.php
@@ -1,13 +1,9 @@
-<!-- To customize this template, copy it to your currently active theme directory and edit it -->
-<div id="flickrfeed">
-	<h3>Photos</h3>
-	<ul>
-	<?php	if (isset($content->error)): ?>
-		<li class="flickr-error"><?php echo $content->error; ?></li>
+<ul id="flickrfeed">
+	<?php if ( !isset( $content->error ) ): ?>
+	<?php foreach ( $content->images as $image ): ?>
+	<li class="flickr-image"><a href="<?php echo $image[ 'url' ]; ?>"><img src="<?php echo $image[ 'image_url' ]; ?>" alt="<?php echo Utils::htmlspecialchars( $image[ 'title' ] ); ?>" /></a></li>
+	<?php endforeach; ?>
 	<?php else: ?>
-	<?php foreach ($content->images as $flickrimage): ?>
-		<li class="flickr-image"><a href="<?php echo $flickrimage['url']; ?>"><img src="<?php echo $flickrimage['image_url']; ?>" alt="<?php echo htmlspecialchars($flickrimage['title']); ?>" /></a></li>
-	<?php endforeach; ?>	
+	<li class="flickr-error"><?php echo $content->error; ?></li>
 	<?php endif; ?>
-	</ul>
-</div>
+</ul>
