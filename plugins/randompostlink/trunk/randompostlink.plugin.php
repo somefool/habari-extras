d56024456a00ae4f2956b1d0561474bf8d288f2a
ringmaster <ringmaster@f4a90942-1786-4aa1-b092-57c31935ddc3>
2011-03-03 05:49:42 +0000

    Initial commit.
    git-svn-id: http://svn.habariproject.org/habari-extras@3798 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ plugins/randompostlink/trunk/randompostlink.plugin.php
@@ -0,0 +1,23 @@
+<?php
+
+class RandomPostLink extends Plugin
+{
+	function filter_rewrite_rules($rules)
+	{
+		$rules[] = RewriteRule::create_url_rule('"randompost"', 'PluginHandler', 'randompost');
+		return $rules;
+	}
+	
+	function action_plugin_act_randompost($handler)
+	{
+		$criteria = array(
+			'status' => Post::status('published'), 
+			'orderby' => 'RAND()',
+			'limit' => 1,
+		);
+		$post = Post::get($criteria);
+		
+		Utils::redirect($post->permalink);
+	}
+}
+?>
