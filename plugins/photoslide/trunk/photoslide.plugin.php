fa18e9b3c05933fc1ab07c3c0f44e4297b652791
ringmaster <ringmaster@f4a90942-1786-4aa1-b092-57c31935ddc3>
2011-03-23 02:49:48 +0000

    Allow photoslide to insert multiple photos, and rearrange them.
    git-svn-id: http://svn.habariproject.org/habari-extras@3820 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/photoslide/trunk/photoslide.plugin.php
+++ plugins/photoslide/trunk/photoslide.plugin.php
@@ -4,12 +4,12 @@ class PhotoSlide extends Plugin
 {
 	public function action_plugin_activation( $plugin_file )
 	{
-		Post::add_new_type( 'photo' );
+		Post::add_new_type( 'photoset' );
 	}
 
 	public function action_plugin_deactivation( $plugin_file )
 	{
-		Post::deactivate_post_type( 'photo' );
+		Post::deactivate_post_type( 'photoset' );
 	}
 	
 
@@ -17,8 +17,8 @@ class PhotoSlide extends Plugin
 	{ 
 		$names = array( 
 			'photo' => array(
-				'singular' => _t( 'Photo', 'photoslide' ),
-				'plural' => _t( 'Photos', 'photoslide' ),
+				'singular' => _t( 'Photoset', 'photoslide' ),
+				'plural' => _t( 'Photosets', 'photoslide' ),
 			)
 		); 
 		return isset($names[$type][$foruse]) ? $names[$type][$foruse] : $type; 
@@ -26,37 +26,54 @@ class PhotoSlide extends Plugin
 	
 	public function action_form_publish( $form, $post )
 	{
-		if( $form->content_type->value == Post::type( 'photo' ) ) {
+		if( $form->content_type->value == Post::type( 'photoset' ) ) {
 			
-			$src = '';
+			$assetdivs = '';
 			if($post->content != '') {
-				$asset = Media::get($post->content);
-				$src = $asset->thumbnail_url;
+				$assetdivs =  $this->assetlist_to_divs($post->content, $post, '<div class="setphoto"><img src="{asset_thumbnail}" style="width: 50px;"><a href="#delete">delete</a><input class="setphoto_caption" type="text" value="{caption}"><input class="setphoto_path" type="hidden" value="{asset_path}"></div>');
 			}
 
 			$imagepreview = $form->insert('content', 'static', 'imagepreview', '');
 			$imagepreview->caption = <<< CAPTION_SCRIPT
 <div class="container transparent">
 	<fieldset>
-		<legend>Preview</legend>
-		<img id="imagepreview" src="{$src}" style="width:100px;">
+		<legend>Photos</legend>
+		<div id="photoset">
+{$assetdivs}
+		</div>
 	</fieldset>
 </div>
 <script type="text/javascript">
+function add_photo_to_set(fileindex, fileobj) {
+	$('#photoset').append('<div class="setphoto"><img src="' + fileobj.thumbnail_url + '" style="width: 50px;"><a href="#delete">delete</a><input class="setphoto_caption" type="text" value="' + fileobj.basename + '"><input class="setphoto_path" type="hidden" value="' + fileobj.path + '"></div>');
+	photoset_recompute();
+}
+function photoset_recompute() {
+	var photos = [], captions = [];
+	$('.setphoto').each(function(){
+		photos.push($('.setphoto_path', this).val());
+		captions.push($('.setphoto_caption', this).val());
+	});
+	$('#content').val(photos.join("\\n") + "\\n\\n" + captions.join("\\n"));
+	$('#photoset').sortable({
+		axis: 'y',
+		opacity: 0.6,
+		containment: $('#photoset').closest('.container'),
+		stop: photoset_recompute
+	});
+}
 $(function(){
 	$.extend(habari.media.output.image_jpeg, {
-		insert_image: function(fileindex, fileobj) {
-			$('#content').val(fileobj.path);
-			$('#imagepreview').attr('src', fileobj.thumbnail_url);
-		}
+		insert_image: add_photo_to_set
 	});
 	$.extend(habari.media.output.flickr, {
-		embed_photo: function(fileindex, fileobj) {
-			$('#content').val(fileobj.path);
-			$('#imagepreview').attr('src', fileobj.url);
-			console.log(fileindex, fileobj);
-		}
+		embed_photo: add_photo_to_set
 	});
+	$('.setphoto a[href=#delete]').live('click', function(){
+		$(this).closest('.setphoto').fadeOut(function(){\$(this).remove();photoset_recompute();});
+		return false;
+	});
+	photoset_recompute();
 });
 </script>
 CAPTION_SCRIPT;
@@ -75,7 +92,44 @@ CAPTION_SCRIPT;
 	
 	public function filter_post_content_media($content, $post)
 	{
-		return '<img src="' . Media::get($content)->url . '">';
+		return $this->assetlist_to_divs($content, $post, '<div class="setphoto" id="sp_{post_slug}_{index}"><img src="{asset_url}" alt="{caption}"></div>');
+	}
+	
+	public function filter_post_content_thumbnails($content, $post)
+	{
+		return $this->assetlist_to_divs($content, $post, '<div class="setphoto" id="spthumb_{post_slug}_{index}"><img src="{asset_thumbnail}" alt="{caption}"></div>');
+	}
+	
+	public function assetlist_to_divs($content, $post, $subst)
+	{
+		$assetdivs = '';
+		if($content != '') {
+			$assetdivs = array();
+			$content = preg_split("#(\r\n|\n\r|\n){2,}#", $content);
+			list($assets, $captions) = $content;
+			$assets = explode("\n", $assets);
+			$captions = explode("\n", $captions);
+			reset($captions);
+			$index = 0;
+			foreach($assets as $asset) {
+				$asset = Media::get(trim($asset));
+				$caption = trim(current($captions));
+				$repl = array(
+					'{post_id}' => $post->id,
+					'{post_slug}' => $post->slug,
+					'{asset_url}' => $asset->url,
+					'{asset_path}' => $asset->path,
+					'{asset_thumbnail}' => $asset->thumbnail_url,
+					'{caption}' => $caption,
+					'{index}' => $index,
+				);
+				$assetdivs[] = str_replace(array_keys($repl), $repl, $subst);
+				next($captions);
+				$index++;
+			}
+			$assetdivs = implode("\n", $assetdivs);
+		}
+		return $assetdivs;
 	}
 
 	public function filter_post_content_mediaurl($content, $post)
