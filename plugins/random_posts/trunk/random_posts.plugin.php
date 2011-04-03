4bd0d5ee434207832d88377d2e4c1ed8cbfab09b
mikelietz <mikelietz@f4a90942-1786-4aa1-b092-57c31935ddc3>
2011-04-03 01:27:52 +0000

    plugin:random_posts A lot of changes to get up to 0.7. Will need to fix the 'tag' and 'not_tag' parameters before tagging for 0.7.
    git-svn-id: http://svn.habariproject.org/habari-extras@3830 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/random_posts/trunk/random_posts.plugin.php
+++ plugins/random_posts/trunk/random_posts.plugin.php
@@ -9,60 +9,36 @@
 
 class RandomPosts extends Plugin
 {
-	const VERSION = '0.4';
-
 	private $config = array();
 	private $random_posts = '';
  
-	private $hard_limit = 5; // in case this is not configured, so that all posts are not returned
-
 	public function help() 
 	{
 		$help = _t( 'To use, add <code>&lt;?php $theme->random_posts(); ?&gt;</code> to your theme where you want the list output.' );
 		return $help;
 	}
 
-	public function action_update_check()
-	{
-		 Update::add( 'Random Posts', 'e3dc9780-4229-11dd-ae16-0800200c9a66', $this->info->version );
-	}
-
 	public function action_init()
 	{
 		$class_name = strtolower( get_class( $this ) );
-		$this->config['num_posts'] = Options::get( $class_name . '__num_posts' );
+		$this->config['num_posts'] = Options::get( $class_name . '__num_posts', 5 );
 		$this->config['tags_exclude'] = Options::get( $class_name . '__tags_exclude' );
 		$this->config['tags_include'] = Options::get( $class_name . '__tags_include' );
 		$this->config['before_title'] = Options::get( $class_name . '__before_title' );
 		$this->config['after_title'] = Options::get( $class_name . '__after_title' );
 	}
 
-	public function filter_plugin_config( $actions, $plugin_id )
+	public function configure()
 	{
-		if ( $plugin_id == $this->plugin_id() ) {
-		$actions[] = _t( 'Configure' );
-	}
-	return $actions;
-	}
-
-	public function action_plugin_ui( $plugin_id, $action )
-	{
-		if ( $plugin_id == $this->plugin_id() ) {
-			switch ( $action ) {
-				case _t( 'Configure' ):
-				$class_name = strtolower( get_class( $this ) );
-				$ui = new FormUI( $class_name );
-				$num_posts = $ui->append( 'text', 'num_posts', 'randomposts__num_posts', _t( 'Default number of links to show' ) );
-				$tags_include = $ui->append( 'text','tags_include', 'randomposts__tags_include', _t( 'Comma-delimited list of tags to include' ) );
-				$tags_exclude = $ui->append( 'text','tags_exclude', 'randomposts__tags_exclude', _t( 'Comma-delimited list of tags to exclude' ) );
-				$before_title = $ui->append( 'text','before_title', 'randomposts__before_title', _t( 'HTML before title' ) );
-				$after_title = $ui->append( 'text','after_title', 'randomposts__after_title', _t( 'HTML after title' ) );
+		$ui = new FormUI( 'randomposts' );
+		$num_posts = $ui->append( 'text', 'num_posts', 'randomposts__num_posts', _t( 'Default number of links to show', 'randomposts' ) );
+		$tags_include = $ui->append( 'text','tags_include', 'randomposts__tags_include', _t( 'Comma-delimited list of tags to include', 'randomposts' ) );
+		$tags_exclude = $ui->append( 'text','tags_exclude', 'randomposts__tags_exclude', _t( 'Comma-delimited list of tags to exclude', 'randomposts' ) );
+		$before_title = $ui->append( 'text','before_title', 'randomposts__before_title', _t( 'HTML before title', 'randomposts' ) );
+		$after_title = $ui->append( 'text','after_title', 'randomposts__after_title', _t( 'HTML after title', 'randomposts' ) );
  
-				$ui->append( 'submit', 'save', 'save' );
-				$ui->out();
-				break;
-			}
-		}
+		$ui->append( 'submit', 'save', 'save' );
+		return $ui;
 	}
 
 	public function theme_random_posts()
@@ -74,8 +50,7 @@ class RandomPosts extends Plugin
 			'status' => Post::status( 'published' ),
 		);
 
-		$params[ 'limit' ] = ( empty( $this->config['num_posts']) ? $this->hard_limit : 
-		$this->config[ 'num_posts' ] );
+		$params[ 'limit' ] = $this->config[ 'num_posts' ];
 
 		if ($this->config[ 'tags_include' ] != '' ) {
 			$params[ 'tag' ] = explode( ',', $this->config[ 'tags_include' ] );
