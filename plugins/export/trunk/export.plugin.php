7a260296e1944d894754194309bf3f2620f65007
chrismeller <chrismeller@f4a90942-1786-4aa1-b092-57c31935ddc3>
2011-05-14 15:50:05 +0000

    Bring SVN in line with my Github version. Git-SVN failed, you'll have to look over there for details on changes.
    git-svn-id: http://svn.habariproject.org/habari-extras@3900 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/export/trunk/export.plugin.php
+++ plugins/export/trunk/export.plugin.php
@@ -29,6 +29,7 @@
 				// only users with the proper permission should be allowed to export
 				if ( User::identify()->can('export now') ) {
 					$actions[] = _t('Export');
+					$actions[] = _t('Export as WXR');
 				}
 				
 			}
@@ -43,9 +44,17 @@
 				
 				switch ( $action ) {
 						
-					case _t('Export'):
+					case _t('Export as BlogML'):
 						
-						$this->run(true);
+						$this->run(true, 'blogml');
+						
+						Utils::redirect( URL::get( 'admin', 'page=plugins' ) );
+						
+						break;
+						
+					case _t('Export as WXR'):
+						
+						$this->run(true, 'wxr');
 						
 						Utils::redirect( URL::get( 'admin', 'page=plugins' ) );
 						
@@ -107,27 +116,53 @@
 			
 		}
 		
-		public function run ( $download = false ) {
+		public function run ( $download = false, $format = 'blogml' ) {
 			
 			Plugins::act('export_run_before');
 			
-			$export = new SimpleXMLElement( '<?xml version="1.0" encoding="utf-8"?><blog xmlns="http://schemas.habariproject.org/BlogML.xsd" xmlns:xs="http://www.w3.org/2001/XMLSchema" />' );
-			$export->addAttribute( 'root-url', Site::get_url('habari') );
+			if ( $format == 'blogml' ) {
 			
-			$export->addChild( 'title', Options::get('title') )->addAttribute( 'type', 'text' );
-			$export->addChild( 'sub-title', Options::get('tagline') )->addAttribute( 'type', 'text' );
+				$export = new SimpleXMLElement( '<?xml version="1.0" encoding="utf-8"?><blog xmlns="http://schemas.habariproject.org/BlogML.xsd" xmlns:xs="http://www.w3.org/2001/XMLSchema" />' );
+				$export->addAttribute( 'root-url', Site::get_path('habari', true) );
+				$export->addAttribute( 'date-created', HabariDateTime::date_create()->format( DateTime::W3C ) );
+				
+				$export->addChild( 'title', Options::get('title') )->addAttribute( 'type', 'text' );
+				$export->addChild( 'sub-title', Options::get('tagline') )->addAttribute( 'type', 'text' );
+				
+				// export all the blog's users
+				$this->export_users( $export );
+				
+				// export all the blog's options
+				$this->export_options( $export );
+				
+				// export all the blog's tags
+				$this->export_tags( $export );
+				
+				// export all the blog's posts and pages
+				$this->export_posts( $export, array( 'entry', 'page' ) );
 			
-			// export all the blog's users
-			$this->export_users( $export );
-			
-			// export all the blog's options
-			$this->export_options( $export );
-			
-			// export all the blog's tags
-			$this->export_tags( $export );
-			
-			// export all the blog's posts and pages
-			$this->export_posts( $export, array( 'entry', 'page' ) );
+			}
+			else if ( $format == 'wxr' ) {
+				
+				$export = new SimpleXMLElement( '<?xml version="1.0" encoding="utf-8"?><rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:wp="http://wordpress.org/export/1.0/" />');
+				$channel = $export->addChild( 'channel' );
+				
+				$channel->title = Options::get('title');
+				$channel->link = Site::get_url( 'habari' );
+				$channel->description = Options::get( 'tagline' );
+				$channel->pubDate = HabariDateTime::date_create()->format( DateTime::RSS );
+				$channel->generator = 'Habari/' . Version::get_habariversion() . '-Export/' . $this->info->version;
+				$channel->{'wp:wxr_version'} = '1.0';
+				$channel->{'wp_base_site_url'} = Site::get_url( 'host' );
+				$channel->{'wp_base_blog_url'} = Site::get_url( 'habari' );
+				
+				// export all the blog's tags
+				$this->export_tags_wxr( $export );
+				
+				// export all the blog's posts and pages
+				$this->export_posts_wxr( $export, array( 'entry', 'page' ) );
+				
+			}
 			
 			EventLog::log( _t( 'Export completed!' ), 'info', 'export', 'export' );
 			
@@ -227,8 +262,23 @@
 				
 				$category = $categories->addChild( 'category' );
 				$category->addAttribute( 'id', $tag->id );
-				$category->addAttribute( 'description', $tag->term_display );
-				$category->addChild( 'title', $tag->term )->addAttribute( 'type', 'text' );
+				$category['description'] = $tag->term_display;		// overloading because it needs escaping
+				
+				$category->title = $tag->term;
+				$category->title['type'] = 'text';
+				
+			}
+			
+		}
+		
+		private function export_tags_wxr ( $export ) {
+			
+			$tags = Tags::vocabulary()->get_tree();
+			foreach ( $tags as $tag ) {
+				
+				$t = $export->addChild( 'wp:tag' );
+				$t->{'wp:tag_slug'} = $tag->term;
+				$t->{'wp:tag_name'} = $tag->term_display;
 				
 			}
 			
@@ -263,16 +313,29 @@
 				// create the post object
 				$p = $ps->addChild( 'post' );
 				
+				// figure out the type of the post
+				if ( $post->content_type == Post::type( 'entry' ) ) {
+					$type = 'normal';		// blog posts are normal
+				}
+				else {
+					$type = 'article';		// articles are pages, i think
+				}
+				
+				
 				// add all the basic post info
 				$p->addAttribute( 'id', $post->id );
 				$p->addAttribute( 'date-created', $post->pubdate->format('c') );
 				$p->addAttribute( 'date-modified', $post->modified->format('c') );
 				$p->addAttribute( 'approved', $post->status == Post::status('published') ? 'true' : 'false' );
 				$p->addAttribute( 'post-url', $this->format_permalink( $post->permalink ) );
-				$p->addAttribute( 'type', Post::type_name( $post->content_type ) );
+				$p->addAttribute( 'type', $type );
+				
+				// the post title is already being escaped somewhere, so don't use overloading to escape it again
 				$p->addChild( 'title', $post->title );
-				$p->addChild( 'content', $post->content );
-				$p->addChild( 'post-name', $post->slug );
+				
+				// we use attribute overloading so they get escaped properly
+				$p->content = $post->content;
+				$p->{'post-name'} = $post->slug;
 				
 				// now add the post tags
 				$pt = $p->addChild( 'categories' );
@@ -300,11 +363,83 @@
 					$c->addAttribute( 'user-url', $comment->url );
 					
 					$c->addChild( 'title' );
-					$c->addChild( 'content', $comment->content )->addAttribute( 'type', 'text' );
+					
+					$content = $c->content = $comment->content;
+					$content['type'] = 'text';
 					
 				}
 				
 				$p->addChild( 'authors' )->addChild( 'author' )->addAttribute( 'ref', $post->author->id );
+				
+			}
+			
+		}
+		
+		private function export_posts_wxr ( $export, $content_type = array( 'entry', 'page' ) ) {
+			
+			$posts = Posts::get( array( 'limit' => null, 'content_type' => $content_type ) );
+			foreach ( $posts as $post ) {
+				
+				// create the item object
+				$item = $export->addChild( 'item' );
+				
+				// add all the basic post info
+				$item->id = $post->id;
+				$item->pubDate = $post->pubdate->format( DateTime::RSS );
+				$item->{'dc:creator'} = $post->author->username;
+				$item->guid = $post->permalink;
+				$item->guid['isPermalink'] = 'true';
+				$item->description = '';
+				$item->{'content:encoded'} = $post->content;
+				
+				// the post title is already being escaped somewhere, so don't use overloading to escape it again
+				$item->addChild( 'title', $post->title );
+				
+				$item->{'wp:post_id'} = $post->id;
+				$item->{'wp:post_date'} = $post->pubdate->format( DateTime::ISO8601 );
+				$item->{'wp:post_date_gmt'} = $post->pubdate->set_timezone( 'UTC' )->format( DateTime::ISO8601 );
+				$item->{'wp:comment_status'} = $post->info->comments_disabled ? 'closed' : 'open';
+				$item->{'wp:ping_status'} = $post->info->comments_disabled ? 'closed' : 'open';
+				$item->{'wp:post_name'} = $post->slug;
+				$item->{'wp:status'} = $post->status == Post::status('published') ? 'published' : 'draft';
+				$item->{'wp:post_parent'} = 0;
+				$item->{'wp:menu_order'} = 0;
+				$item->{'wp:post_type'} = $post->typename;
+				$item->{'wp:post_password'} = '';
+				
+				$tags = $post->tags;
+				foreach ( $tags as $tag ) {
+					
+					$category = $item->addChild( 'category', $tag->term );
+					$category['domain'] = 'tag';
+					$category['nicename'] = $tag->term_display;
+					
+				}
+				
+				// now add the post comments
+				// @todo add support for trackbacks from the pingback plugin?
+				foreach ( $post->comments as $comment ) {
+					
+					$c = $item->addChild( 'wp:comment' );
+					$c->{'wp:comment_id'} = $comment->id;
+					$c->{'wp:comment_author'} = $comment->name;
+					$c->{'wp:comment_author_email'} = $comment->email;
+					$c->{'wp:comment_author_url'} = $comment->url;
+					$c->{'wp:comment_author_IP'} = $comment->ip;
+					$c->{'wp:comment_date'} = $comment->date->format( DateTime::RSS );
+					$c->{'wp:comment_date_gmt'} = $comment->date->set_timezone( 'UTC' )->format( DateTime::RSS );
+					$c->{'wp:comment_content'} = $comment->content;
+					$c->{'wp:comment_approved'} = $comment->status == Comment::STATUS_APPROVED ? 'true' : 'false';
+					$c->{'wp:comment_type'} = Comment::type_name( $comment->type );
+					$c->{'wp:comment_parent'} = 0;
+					
+					$user = User::get_by_email( $comment->email );
+					
+					if ( $user ) {
+						$c->{'wp:comment_user_id'} = $user->id;
+					}
+					
+				}
 				
 			}
 			
