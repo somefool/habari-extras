5d6aa4e35c1fb09692a1982c18631b68b96e2441
lapsedtheorist <lapsedtheorist@f4a90942-1786-4aa1-b092-57c31935ddc3>
2011-05-17 20:24:03 +0000

    Plugin: Relatively Popular - removed block title as it now appears via the blockwrapper mechanism
    git-svn-id: http://svn.habariproject.org/habari-extras@3914 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/relativelypopular/trunk/block.relativelypopular.php
+++ plugins/relativelypopular/trunk/block.relativelypopular.php
@@ -2,13 +2,6 @@
 if(is_object($content->relativelypopular)) {
 	?>
 <div id="relativelypopular_plugin">
-<?php
-	if($content->_show_title) {
-?>
-	<h4><?php _e($content->title) ?></h4>
-<?php
-	}
-?>
 <ul>
   <?php foreach( $content->relativelypopular as $p): ?>
     <li>
