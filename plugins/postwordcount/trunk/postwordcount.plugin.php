afb3c3f123294f73effd13b9be33199a42e2178d
lildude <lildude@f4a90942-1786-4aa1-b092-57c31935ddc3>
2011-08-20 12:30:15 +0000

    plugin:postwordcount Moved help() to the xml file
    git-svn-id: http://svn.habariproject.org/habari-extras@3959 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/postwordcount/trunk/postwordcount.plugin.php
+++ plugins/postwordcount/trunk/postwordcount.plugin.php
@@ -10,12 +10,6 @@
 
 class PostWordCount extends Plugin
 {
-	public function help()
-	{
-		$help = _t( 'To use, <code>&lt;?php echo $post->word_count; ?&gt;</code>.' );
-		return $help;
-	}
-
 	public function action_post_update_after( $post )
 	{
 		$allcharacters = 'ÁÀÂÄǍĂĀÃÅǺĄƁĆĊĈČÇĎḌƊÉÈĖÊËĚĔĒĘẸƎƏƐĠĜǦĞĢƔĤḤĦIÍÌİÎÏǏĬĪĨĮỊĴĶƘĹĻŁĽĿŃŇÑŅÓÒÔÖǑŎŌÕŐỌØǾƠŔŘŖŚŜŠŞȘṢŤŢṬÚÙÛÜǓŬŪŨŰŮŲỤƯẂẀŴẄǷÝỲŶŸȲỸƳŹŻŽẒáàâäǎăāãåǻąɓćċĉčçďḍɗéèėêëěĕēęẹǝəɛġĝǧğģɣĥḥħıíìiîïǐĭīĩįịĵķƙĸĺļłľŀŉńňñņóòôöǒŏōõőọøǿơŕřŗśŝšşșṣſťţṭúùûüǔŭūũűůųụưẃẁŵẅƿýỳŷÿȳỹƴźżžẓΑΆΒΓΔΕΈΖΗΉΘΙΊΪΚΛΜΝΞΟΌΠΡΣΤΥΎΫΦΧΨΩΏαάβγδεέζηήθιίϊΐκλμνξοόπρσςτυύϋΰφχψωώÆǼǢÐĐĲŊŒÞŦæǽǣðđĳŋœßþŧ';
@@ -24,21 +18,19 @@ class PostWordCount extends Plugin
 	}
 	
 	public function configure()
-        {
-                                        $class_name= strtolower( get_class( $this ) );
-                                        $ui= new FormUI( $class_name );
-
-                                        $add_title= $ui->append( 'checkbox', 'add_title',
-                                                $class_name . '__add_title', _t( 'Include title words in count?' ) );
-
-                                        $ui->append( 'submit', 'save', 'save' );
-                                        return $ui;
-        }
+    {
+		$class_name= strtolower( get_class( $this ) );
+		$ui= new FormUI( $class_name );
+		$add_title= $ui->append( 'checkbox', 'add_title',
+        $class_name . '__add_title', _t( 'Include title words in count?' ) );
+		$ui->append( 'submit', 'save', 'save' );
+		return $ui;
+    }
 
 	public function action_init()
 	{
 		$class_name= strtolower( get_class( $this ) );
-                $this->config[ 'add_title' ]= Options::get( $class_name . '__add_title' );
+		$this->config[ 'add_title' ]= Options::get( $class_name . '__add_title' );
 	}
 	
 	public function filter_post_word_count( $word_count, $post ) 
