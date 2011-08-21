0ca63a9a92a4444164c1549123c6695c6aba5813
lildude <lildude@f4a90942-1786-4aa1-b092-57c31935ddc3>
2011-08-21 11:48:27 +0000

    plugin:sitemaps Added support for gzipped sitemap.xml. Fixed #204. Thanks to the_angry_angel for the patch
    git-svn-id: http://svn.habariproject.org/habari-extras@3982 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/sitemaps/trunk/sitemaps.plugin.php
+++ plugins/sitemaps/trunk/sitemaps.plugin.php
@@ -11,8 +11,11 @@ class Sitemaps extends Plugin {
      */
     public function filter_rewrite_rules( $db_rules )
     {
-	$db_rules[]= RewriteRule::create_url_rule( '"sitemap.xml"', 'Sitemaps', 'Sitemap' );
-	return $db_rules;
+		$db_rules[]= RewriteRule::create_url_rule( '"sitemap.xml"', 'Sitemaps', 'Sitemap' );
+		if ( function_exists( 'gzencode' ) ) {
+		 	$db_rules[]= RewriteRule::create_url_rule( '"sitemap.xml.gz"', 'Sitemaps', 'SitemapGz' );
+		}
+		return $db_rules;
     }
 
     /**
@@ -23,12 +26,15 @@ class Sitemaps extends Plugin {
      */
     public function act( $action )
     {
-	switch ( $action )
-	{
-	    case 'Sitemap':
-		self::Sitemap();
-		break;
-	}
+		switch ( $action )
+		{
+		    case 'Sitemap':
+				self::Sitemap();
+				break;
+			case 'SitemapGz': 
+			 	self::SitemapGz(); 
+			 	break;
+		}
     }
 
     /**
@@ -37,41 +43,52 @@ class Sitemaps extends Plugin {
      */
     public function Sitemap()
     {
-	//return cached sitemap if exsist
-	if ( Cache::has( 'sitemap' ) ){
-	    $xml = Cache::get( 'sitemap' );
-	}
-	else {
-	    //..or generate a new one
-	    $xml = '<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="'.$this->get_url() .'/sitemap.xsl"?><urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>';
-
-	    $xml = new SimpleXMLElement( $xml );
-
-	    // Retreive all published posts and pages from the database
-	    $content['posts']= Posts::get( array( 'content_type' => 'entry', 'status' => 'published', 'nolimit' => 1 ) );
-	    $content['pages']= Posts::get( array( 'content_type' => 'page', 'status' => 'published', 'nolimit' => 1 ) );
-
-	    // Add the index page first
-	    $url = $xml->addChild( 'url' );
-	    $url_loc = $url->addChild( 'loc', Site::get_url( 'habari' ) );
-
-	    // Generate the `<url>`, `<loc>`, `<lastmod>` markup for each post and page.
-	    foreach ( $content as $entries ) {
-		foreach ( $entries as $entry ) {
-		    $url = $xml->addChild( 'url' );
-		    $url_loc = $url->addChild( 'loc', $entry->permalink );
-		    $url_lastmod = $url->addChild( 'lastmod', $entry->updated->get( 'c' ) );
-		}
-	    }
-	    $xml = $xml->asXML();
-	    Cache::set( 'sitemap', $xml );
-	}
-
-	/* Clean the output buffer, so we can output from the header/scratch. */
-	ob_clean();
-	header( 'Content-Type: application/xml' );
-	print $xml;
+		/* Clean the output buffer, so we can output from the header/scratch. */
+		ob_clean();
+		header( 'Content-Type: application/xml' );
+		print self::SitemapBuild();
     }
+
+	public function SitemapGz() 
+	{ 
+	 	ob_clean(); 
+	 	header( 'Content-Type: application/x-gzip' ); 
+	 	print gzencode( self::SitemapBuild() ); 
+	}
+	
+	public function SitemapBuild() 
+	{
+		//return cached sitemap if exsist
+		if ( Cache::has( 'sitemap' ) ){
+		    $xml = Cache::get( 'sitemap' );
+		}
+		else {
+		    //..or generate a new one
+		    $xml = '<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="'.$this->get_url() .'/sitemap.xsl"?><urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>';
+
+		    $xml = new SimpleXMLElement( $xml );
+
+		    // Retreive all published posts and pages from the database
+		    $content['posts']= Posts::get( array( 'content_type' => 'entry', 'status' => 'published', 'nolimit' => 1 ) );
+		    $content['pages']= Posts::get( array( 'content_type' => 'page', 'status' => 'published', 'nolimit' => 1 ) );
+
+		    // Add the index page first
+		    $url = $xml->addChild( 'url' );
+		    $url_loc = $url->addChild( 'loc', Site::get_url( 'habari' ) );
+
+		    // Generate the `<url>`, `<loc>`, `<lastmod>` markup for each post and page.
+		    foreach ( $content as $entries ) {
+			foreach ( $entries as $entry ) {
+			    $url = $xml->addChild( 'url' );
+			    $url_loc = $url->addChild( 'loc', $entry->permalink );
+			    $url_lastmod = $url->addChild( 'lastmod', $entry->updated->get( 'c' ) );
+			}
+		    }
+		    $xml = $xml->asXML();
+		    Cache::set( 'sitemap', $xml );
+		}
+		return $xml;
+	}
 
     /**
      * post_update_status action called when the post status field is updated.
@@ -82,9 +99,9 @@ class Sitemaps extends Plugin {
      */
     public function action_post_update_status( $post, $new_status )
     {
-	if ( $post->status != $new_status ){
-	    Cache::expire( 'sitemap' );
-	}
+		if ( $post->status != $new_status ){
+		    Cache::expire( 'sitemap' );
+		}
     }
 }
 ?>
