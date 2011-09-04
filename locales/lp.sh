efafe2ac19a4f8b96c1d6122ccbc3a5b851873c1
MattRead <MattRead@f4a90942-1786-4aa1-b092-57c31935ddc3>
2011-09-04 14:01:19 +0000

    launchpad sync
    git-svn-id: http://svn.habariproject.org/habari-extras@3995 f4a90942-1786-4aa1-b092-57c31935ddc3

--- locales/lp.sh
+++ locales/lp.sh
@@ -3,12 +3,14 @@
 # setup tmp files from tar.gz's
 cd /home/matt/habari-locales/bzr/
 bzr merge
+bzr add locale/*
+bzr commit -m "lp sync"
 cd ../
 
 # check fo .po
-for d in `ls bzr/system/locale/*.po`;
+for d in `ls bzr/locale/*.po`;
 do
-    if [[ $d =~ bzr/system/locale/(.*).po ]]
+    if [[ $d =~ bzr/locale/(.*).po ]]
     then
         if [ ! -d ${BASH_REMATCH[1]} ]
         then
@@ -16,11 +18,9 @@ do
             mkdir ${BASH_REMATCH[1]}/trunk
             mkdir ${BASH_REMATCH[1]}/trunk/LC_MESSAGES
             mkdir ${BASH_REMATCH[1]}/trunk/dist
-            mkdir ${BASH_REMATCH[1]}/tags
-            mkdir ${BASH_REMATCH[1]}/braches
         fi
         echo "moving  ${BASH_REMATCH[1]}.po"
-        mv bzr/system/locale/${BASH_REMATCH[1]}.po ${BASH_REMATCH[1]}/trunk/LC_MESSAGES/habari.po
+        cp bzr/locale/${BASH_REMATCH[1]}.po ${BASH_REMATCH[1]}/trunk/LC_MESSAGES/habari.po
         echo "generating mo file"
         msgfmt -o ${BASH_REMATCH[1]}/trunk/LC_MESSAGES/habari.mo ${BASH_REMATCH[1]}/trunk/LC_MESSAGES/habari.po
         svn add -q ${BASH_REMATCH[1]} ${BASH_REMATCH[1]}/trunk/LC_MESSAGES/habari.po ${BASH_REMATCH[1]}/trunk/LC_MESSAGES/habari.mo
