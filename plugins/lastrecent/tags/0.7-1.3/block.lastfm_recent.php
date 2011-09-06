2e0c65135cd3e829cc9b65dec5ea390a93c1f0ad
lildude <lildude@f4a90942-1786-4aa1-b092-57c31935ddc3>
2011-09-06 17:03:45 +0000

    plugin:lastrecent Significantly updated and improved plugin.
    Changes include:
    - Added 0.7 tag
    - Improved help
    - Moved beacon to xml file
    - Added block support without removing "legacy" support
    - Coding standards
    
    This plugin could do with renaming to lastfm_recent or lastfmrecent as lastrecent isn't really clear.  Maybe another day.
    
    
    
    git-svn-id: http://svn.habariproject.org/habari-extras@3999 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugins/lastrecent/tags/0.7-1.3/block.lastfm_recent.php
+++ plugins/lastrecent/tags/0.7-1.3/block.lastfm_recent.php
@@ -1,11 +1,11 @@
 <div id="lastfm">
 	<ul id="lastrecent">
-		<?php if ( isset($lastfm_track['error'] ) ) : ?>
+		<?php if ( isset( $content->lastfm_recent['error'] ) ) : ?>
 		<li>
-			<?php echo $lastfm_track['error']; ?>
+			<?php echo $content->lastfm_recent['error']; ?>
 		</li>
 		<?php else : 
-			foreach ( $lastfm_tracks as $track ) : ?>
+			foreach ( $content->lastfm_recent as $track ) : ?>
 		<li>
 			<a href="<?php echo $track['url']; ?>"><?php if ( $track['image'] != '' ) : ?><img src="<?php echo $track['image'];?>" /><?php endif; ?><?php echo $track['name']; ?></a> by <?php echo $track['artist'];?>
 		</li>		
